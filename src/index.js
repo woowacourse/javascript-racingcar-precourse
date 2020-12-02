57c19ce5aa0a1b7802805ef7d1c5d9c556edd7b6
hyperflow <hyperflow@kakao.com>
2020-12-03 01:07:50 +0900

    feat(index): Complete index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-export default function RacingCarGame() {}
-// export default class RacingCarGame {
-// }
+import { handleCountSubmit, handleNamesSubmit } from "./handler.js";
+import { resetGame } from "./utils.js";
+
+export default function RacingCarGame() {
+  resetGame();
+
+  document
+    .getElementById("car-names-submit")
+    .addEventListener("click", handleNamesSubmit.bind(this));
+
+  document
+    .getElementById("racing-count-submit")
+    .addEventListener("click", handleCountSubmit.bind(this));
+}
 
 new RacingCarGame();
