577b8da66613b491a47e8d8fc34032b7f4ef6982
yujo <yujo@student.42seoul.kr>
2020-12-02 20:58:10 +0900

    feat: 사용자의 자동차 입력이 올바른지 검사하는 기능 구현 완료

--- src/input/getCarName.js
+++ src/input/getCarName.js
@@ -2,6 +2,24 @@
 //   this.name = name;
 // }
 
+function checkLength(carName) {
+  if (carName.length === 0) {
+    return false;
+  }
+  if (carName.length > 5) {
+    return false;
+  }
+  return true;
+}
+
+function checkValidCarName(carNamesArray) {
+  console.log(carNamesArray);
+  if (!carNamesArray.every(checkLength)) {
+    return false;
+  }
+  return true;
+}
+
 export default function getCarName() {
   const $carNamesInput = document.querySelector('#car-names-input');
   const carNamesArray = [...new Set($carNamesInput.value.split(','))];
