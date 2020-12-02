16ba525f42628a566147ca8b81e1c37198f0254d
unknown <dudtjr913@naver.com>
2020-12-02 22:17:35 +0900

    feat : 실행 결과를 보여주는 메소드 구현 <description> 게임을 플레이할 때, 플레이하는 횟수에 맞춰 결과를 보여주는 showOnGameProgress 메소드를 호출하게 하였고, 이 메소드는 HTML 요소인 ul, li를 만드는 메소드인 createResultList메소드를 실행한다. 이 createResultList메소드는 게임에 참여한 자동차 배열을 돌면서 결과를 text로 만드는 createResultText를 호출하는데, createResultText에서 return된 각각의 resultDiv가 모두 li 요소의 자식으로 할당된다.

--- src/index.js
+++ src/index.js
@@ -14,6 +14,33 @@ export default class RacingCarGame {
   gamePlay(times) {
     for (let i = 0; i < times; i++) {
       this.carsArray.forEach((car) => car.plusLength());
+      this.showOnGameProgress();
     }
+  }
+
+  showOnGameProgress() {
+    this.createResultList();
+  }
+
+  createResultList() {
+    const $resultContainer = document.body.querySelector('#result-container');
+    const $resultUl = document.createElement('ul');
+    const $resultLi = document.createElement('li');
+    for (const car of this.carsArray) {
+      $resultLi.appendChild(this.createResultText(car));
+    }
+
+    $resultUl.style.padding = '0px';
+    $resultUl.appendChild($resultLi);
+    $resultContainer.appendChild($resultUl);
+  }
+
+  createResultText(car) {
+    const $resultText = document.createElement('div');
+    $resultText.textContent += `
+    ${car.name}: ${car.length}
+    `;
+
+    return $resultText;
   }
 }
