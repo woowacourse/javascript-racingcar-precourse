630e25d681f8e031659825afcbab3265e5cffad7
jehong <jehong@student.42seoul.com>
2020-12-02 21:46:11 +0900

    feat: get name input and parse it to get a list of car names

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+export default class RacingCarGame {
+  constructor() {
+    this.selectDOM();
+    this.addEventListener();
+  }
 
+  selectDOM() {
+    this.$nameInput = document.querySelector("input[type=text]");
+    this.$nameButton = document.querySelectorAll("button")[0];
+    this.$numberInput = document.querySelector("input[type=number]");
+    this.$numberButton = document.querySelectorAll("button")[1];
+  }
+
+  addEventListener() {
+    this.$nameButton.addEventListener('click', () => this.getInput());
+    this.$nameInput.addEventListener('keypress', (e) => {
+      if (e.keyCode === 13)
+        this.getInput();
+    })
+  }
+
+  getCarNames() {
+    const userNameInput = this.$nameInput.value;
+    const carNames = userNameInput.split(',');
+    console.log(carNames);
+  }
+  
+}
 
 new RacingCarGame();
-
-
-function Car(name) {
-  this.name = name;
-}
-
-class Car {
-  constructor(name) {
-    this.name = name;
-  }
-}
