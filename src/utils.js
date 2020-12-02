33a84e291816d81a6f555d02f829cde274aff572
hyperflow <hyperflow@kakao.com>
2020-12-03 00:58:16 +0900

    feat(utils): Add method 'isCarForward'

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,8 @@ function Car(name, count) {
   this.name = name;
   this.count = count;
 }
+
+export const isCarForward = () => {
+  const crit = Math.floor(Math.random() * 10);
+  return crit >= 4 ? true : false;
+};
