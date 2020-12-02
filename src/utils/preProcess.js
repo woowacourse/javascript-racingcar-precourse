89381336424aa1a728cc661728223a2fc20e678d
swon3210 <swon3210@gmail.com>
2020-12-03 00:49:31 +0900

    refactor: adjust NAME_SEPERATOR constant
    this is added to give more flexibility when the racingCarGame needs to change
    its seperator to seperate car names input

--- src/utils/preProcess.js
+++ src/utils/preProcess.js
@@ -1,8 +1,10 @@
+import { NAME_SEPERATOR } from '../constants/configuration.js';
+
 export const preProcessCarNames = (carNames) => {
-	let carNameList = carNames.split(',');
+	let carNameList = carNames.split(NAME_SEPERATOR);
 	carNameList = carNameList.map((name) => name.trim());
 	carNameList = carNameList.filter((name) => name !== '');
-	const preProcessedCardNames = carNameList.join(',');
+	const preProcessedCardNames = carNameList.join(NAME_SEPERATOR);
 
 	return preProcessedCardNames;
 };
