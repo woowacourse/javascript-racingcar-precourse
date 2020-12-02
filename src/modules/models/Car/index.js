895cab4d910626e53ddbdc2ca91b5570e361647a
Daniel <elrion018@gmail.com>
2020-12-02 17:52:27 +0900

    feat : add randomNumber function

--- src/modules/models/Car/index.js
+++ src/modules/models/Car/index.js
@@ -1,5 +1,16 @@
 export default class Car {
   constructor(name) {
     this.name = name;
+    this.moveForwardDistance = 0;
+    this.MAXVAL = 9;
+    this.MINVAL = 0;
+  }
+
+  randomNumber() {
+    const number = Math.floor(
+      Math.random() * (this.MAXVAL - this.MINVAL + 1) + this.MINVAL,
+    );
+
+    return number;
   }
 }
