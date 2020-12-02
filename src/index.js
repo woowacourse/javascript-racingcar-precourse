c88862f4c3f468abb029a2994335618d4cdb03ae
ChayanRin <k852012@naver.com>
2020-12-02 17:25:42 +0900

    feat: implement getCarNameInput
    feat: 자동차 입력란에 적힌 자동차 이름 배열형태로 돌려주는 메서드 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 export default class RacingCarGame {
   constructor() {
     this.handleCarNameSubmit();
+    this.handleCountSubmit();
   }
+
+  getCarNameInput = () => {
+    const carNameInput = document.getElementById("car-names-input");
+    const carNameValue = carNameInput.value;
+    const carNames = carNameValue.split(",");
+
+    return carNames;
+  };
 
   handleCarNameSubmit = () => {
     const carNameButton = document.getElementById("car-names-submit");
