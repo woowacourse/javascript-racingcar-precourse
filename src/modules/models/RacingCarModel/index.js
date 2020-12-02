d6bb37f5e3b5db9bd32a2269d1d9d3d40ab6ee86
Daniel <elrion018@gmail.com>
2020-12-02 18:21:46 +0900

    feat : add progress function

--- src/modules/models/RacingCarModel/index.js
+++ src/modules/models/RacingCarModel/index.js
@@ -2,4 +2,12 @@ export default class RacingCarModel {
   constructor(cars) {
     this._cars = cars;
   }
+
+  progress() {
+    const progressedCars = this._cars.map(car => {
+      car.moveForward();
+    });
+
+    this._cars = progressedCars;
+  }
 }
