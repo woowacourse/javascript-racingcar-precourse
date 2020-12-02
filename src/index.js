513071b9990df5f874f83462d9038e1d8f709eaa
rjwu95 <yugun000@gmail.com>
2020-12-03 00:17:55 +0900

    1회 마다 전진한 만큼 -로 출력 및 화면에 전진현황 출력
     - 각 car의 distance를 이용하여 전진 현황 출력
     - ` template literal 사용하여 html에 출력

--- src/index.js
+++ src/index.js
@@ -1,22 +1,52 @@
 import RacingCar from "./RacingCar.js";
 
 export default class RacingCarGame {
+  cars = [];
   constructor() {
     this.allClickEventListener();
   }
-  allClickEventListener() {
-    this.addEventListenerById("car-names-submit", this.nameSubmitClickEvent);
-  }
+
   nameSubmitClickEvent() {
     const carNames = document
       .getElementById("car-names-input")
       .value.split(",");
-    carNames.map((carName) => new RacingCar(carName));
+    this.cars = carNames.map((carName) => new RacingCar(carName));
     const racingCountContainer = document.getElementById(
       "racing-count-container"
     );
     racingCountContainer.style.display = "block";
   }
+
+  countSubmitClickEvent() {
+    const count = document.getElementById("racing-count-input").value;
+    let i;
+    for (i = 0; i < count; i++) {
+      this.cars.forEach((car) => car.move());
+      this.printMoveStatus();
+    }
+    const resultContainer = document.getElementById("result-container");
+    resultContainer.style.display = "block";
+  }
+
+  printMoveStatus() {
+    const moveStatusEl = document.createElement("div");
+    moveStatusEl.innerHTML = this.cars
+      .map((car) => `${car.name}: ${"-".repeat(car.distance)}`)
+      .join("<br />");
+    moveStatusEl.style.marginBottom = "30px";
+    const resultContainer = document.getElementById("result-container");
+    resultContainer.appendChild(moveStatusEl);
+  }
+
+  allClickEventListener() {
+    this.addEventListenerById("car-names-submit", () =>
+      this.nameSubmitClickEvent()
+    );
+    this.addEventListenerById("racing-count-submit", () =>
+      this.countSubmitClickEvent()
+    );
+  }
+
   addEventListenerById(id, event) {
     const element = document.getElementById(id);
     element.addEventListener("click", event);
