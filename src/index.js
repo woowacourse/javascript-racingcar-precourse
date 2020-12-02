c3f8f7aa5b00a2d665fce6b5f282bb3794dbf8c1
Teo_Hwang <taeung2008@gmail.com>
2020-12-02 23:29:37 +0900

    feat: 확인버튼 클릭시  carNames 가져오는 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+export default class RacingCarGame {
+  constructor() {
+    this.getCarName();
+  }
 
+  getCarName() {
+    const carNamesSubmitButton = document.querySelector('#car-names-submit');
+
+    carNamesSubmitButton.addEventListener('click', () => {
+      const carNames = this.carNames();
+
+      console.log(this.isCarNames(carNames));
+    });
+  }
+
+  carNames() {
+    const carNamesInput = document.querySelector('#car-names-input');
+
+    return carNamesInput.value;
+  }
+
+  isCarNames(_carNames) {
+    if (_carNames === '') {
+      alert('하나 이상 입력해주세요.');
+
+      return null;
+    } else {
+      // 콤마로 구분해서 배열로 저장하는 기능 넣기
+
+      return _carNames;
+    }
+  }
+}
 
 new RacingCarGame();
