5f1f4969c578f9dd6f8caeba3365e8a6866962d8
hyperflow <hyperflow@kakao.com>
2020-12-03 00:58:53 +0900

    feat(utils): Add method 'inputStringToArray'

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,15 @@ export const isCarForward = () => {
   const crit = Math.floor(Math.random() * 10);
   return crit >= 4 ? true : false;
 };
+
+export const inputStringToArray = (inputString) => {
+  const inputArray = inputString.split(",");
+  const carArray = [];
+
+  // Initialize the object
+  for (let i = 0; i < inputArray.length; i++) {
+    carArray.push(new Car(inputArray[i], 0));
+  }
+
+  return carArray;
+};
