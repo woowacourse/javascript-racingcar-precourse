88acdcb1567c86718b629d9e3acf9c200ba60f69
yujo <yujo@student.42seoul.kr>
2020-12-02 18:41:53 +0900

    feat: 자동차 입력을 받기 전까지 경주 횟수 입력받는 화면과 결과화면을 숨기는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
+// import getCarName from './input/getCarName.js';
+
+// function Car(name) {
+//   this.name = name;
 // }
 
+//   class Car {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
+
+function hideScreen() {
+  const $inputRaceNumberScreen = document.querySelector(
+    '.car-game-container > div:last-child',
+  );
+  const $resultScreen = document.querySelector('#app > div:last-child');
+
+  $inputRaceNumberScreen.style.display = 'none';
+  $resultScreen.style.display = 'none';
+}
+
+export default function RacingCarGame() {
+  hideScreen();
+}
 
 new RacingCarGame();
