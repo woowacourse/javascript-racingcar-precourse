41971343a4a96b64fd341d296221d67c4f0beed6
yujo <yujo@student.42seoul.kr>
2020-12-02 20:19:21 +0900

    feat: 자동차의 입력을 받는 태그들과 레이싱 횟수를 입력하는 태그들에 id값을 부여하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import getUserInput from './input/getUserInput.js';
 
+function addTagID() {
+  const $carGameContainer = document.querySelector('.car-game-container');
+
+  $carGameContainer.querySelector('div > input').id = 'car-names-input';
+  $carGameContainer.querySelector('div > button').id = 'car-names-submit';
+  $carGameContainer.querySelector('div:last-child > input').id =
+    'racing-count-input';
+  $carGameContainer.querySelector('div:last-child > button').id =
+    'racing-count-submit';
+}
+
 function hideScreen() {
   const $inputRaceNumberScreen = document.querySelector(
     '.car-game-container > div:last-child',
@@ -11,6 +22,7 @@ function hideScreen() {
 }
 
 export default function RacingCarGame() {
+  addTagID();
   hideScreen();
   getUserInput();
 }
