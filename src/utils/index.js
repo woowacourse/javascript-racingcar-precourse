43e01ce1bfa68d04b86d549f2bfeb5b6c822c978
Daniel <elrion018@gmail.com>
2020-12-02 20:28:23 +0900

    feat : add splitNamesString function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,13 @@
-function isFourHigher(number) {
+export function isFourHigher(number) {
   if (number >= 4) {
     return true;
   }
   return false;
 }
 
-export { isFourHigher };
+export function splitNamesString(names) {
+  const splitedNames = names.split(',');
+  return splitedNames;
+}
+
+export { isFourHigher, splitNamesString };
