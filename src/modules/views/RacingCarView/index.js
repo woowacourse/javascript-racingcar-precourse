3ecac9b9676c797007e92c99ec4753d62557497b
Daniel <elrion018@gmail.com>
2020-12-02 20:11:39 +0900

    feat : add handleCarNamesSubmit function

--- src/modules/views/RacingCarView/index.js
+++ src/modules/views/RacingCarView/index.js
@@ -1,5 +1,10 @@
 export default class RacingCarView {
   constructor(RacingCarModel, carNamesInput) {
     this._RacingCarModel = RacingCarModel;
+    this.carNamesInput = carNamesInput;
+  }
+
+  handleCarNamesSubmit() {
+    const names = this.carNamesInput.value;
   }
 }
