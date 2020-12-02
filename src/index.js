ee9ba965d5bfafad85a71de44b1b085dbef0aba7
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-03 03:42:41 +0900

    Feat: Element에 id를 추가해주는 메소드 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import Car from "src/core/car.js"
 
 export default class RacingCarGame {
-	
-}
+	constructor() {
+		this.carNamesInput = document.getElementsByTagName("input")[0];
+		this.carNamesSubmit = document.getElementsByTagName("button")[0];
+		this.racingCountInput = document.getElementsByTagName("input")[1];
+		this.racingCountSubmit = document.getElementsByTagName("button")[1];
 
+		this.setAttributes();
+		this.initGame();
+	}
+
+	setAttributes = () => {
+		this.carNamesInput.addAttribute("id", "car-names-input");
+		this.carNamesSubmit.addAttribute("id", "car-names-submit");
+		this.racingCountInput.addAttribute("id", "racing-count-input");
+		this.racingCountSubmit.addAttribute("id", "racing-count-submit");
+	}
+
+	initGame = () => {
+
+	}
+}
 new RacingCarGame();
