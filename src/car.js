d052580ab4df9409eaae8755a22ff6584b349b2c
unknown <dudtjr913@naver.com>
2020-12-02 21:27:12 +0900

    feat : 거리 증가 메소드 구현 <description> 최대한 함수를 잘게 쪼개서 구현했다. 거리를 증가시켜라 -> 만약 증가되는 조건이 만족한다면 -> 증가되는 조건은 숫자가 4 이상일 경우

--- src/car.js
+++ src/car.js
@@ -3,4 +3,25 @@ export default class Car {
     this.name = name;
     this.length = '';
   }
+
+  plusLength() {
+    if (this.isPlusLength()) {
+      return (this.length += '-');
+    }
+  }
+
+  isPlusLength() {
+    if (this.createRandomNumber() >= 4) {
+      return true;
+    }
+
+    return false;
+  }
+
+  createRandomNumber() {
+    const MAX_NUMBER = 9;
+    const randomNumber = Math.floor(Math.random() * MAX_NUMBER + 1);
+
+    return randomNumber;
+  }
 }
