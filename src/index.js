9a2e852ed5a0a3d6c1fb26d766645832774470ba
loquemedalagana <nadamasquetu012@gmail.com>
2020-12-02 15:56:33 +0900

    module import test and update readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-export default function RacingCarGame() {
+import Car from '/src/lib/car.js';
+import {
+  testMessage,
+} from '/src/lib/check.js';
+
+export default class RacingCarGame {
+
 }
-// export default class RacingCarGame {
-// }
 
+let car = new Car('hello');
+console.log(car);
+console.log(testMessage);
 
-new RacingCarGame();
+let game = new RacingCarGame();
---
+++ src/lib/check.js
@@ -0,0 +1,1 @@
+export const testMessage = "hello world";
