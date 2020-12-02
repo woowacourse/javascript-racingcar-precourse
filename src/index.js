659e8693d77060ef0e1d7c8fa90aa8b043a8e3a8
loquemedalagana <nadamasquetu012@gmail.com>
2020-12-02 22:37:18 +0900

    undo modularization - delete gameFunctions.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
+import { GO_AHEAD_MAX_VALUE } from '/src/lib/variables/constantNumbers.js';
+import Car from '/src/lib/classes/car.js';
 import InputStatus from '/src/lib/classes/inputControl.js';
 import addIdsToNodes from '/src/lib/config/addIdsToNodes.js';
 
-import {  
-  getInitCarInfo,
-} from '/src/lib/functions/gameFunctions.js';
-
-
+const getInitCarInfo = carNames => carNames.reduce((acc, carName) => {
+  acc.push(new Car(carName));
+  return acc;
+}, []);
 
 export default class RacingCarGame {
   constructor(carNames) {
 		this.cars = getInitCarInfo(carNames);
   }
 
+  _createRandomNumber() {
+    return Math.floor(Math.random()*(GO_AHEAD_MAX_VALUE+1));
+  }
+
   _moveCars(curTurn) {
     //cars를 순회하면서 숫자랑 현재 턴 집어넣는다.
-    //example -> car._runOrPause(생성한 렌덤 숫자, 현재 turn숫자)
+    //example -> cars._runOrPause(생성한 렌덤 숫자, 현재 turn숫자)
   }
 
   _getResult() {
@@ -35,7 +40,7 @@ const runGame = carNames => {
 
 const startGame = () => {
   addIdsToNodes();
-  
+
   //input
 
 }
