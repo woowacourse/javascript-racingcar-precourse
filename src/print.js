0d5ff86ff6c279f7d343814959dda34c5bc6e727
hyperflow <hyperflow@kakao.com>
2020-12-03 01:02:09 +0900

    feat(print): Add method 'printRoundResult' & 'printRaceResult'

--- src/print.js
+++ src/print.js
@@ -1,5 +1,35 @@
 import { resetGame } from "./utils.js";
 
+export const printRoundResult = (resultArray) => {
+  const resultNode = document.createElement("p");
+
+  for (let i = 0; i < resultArray.length; i++) {
+    const resultLine = document.createElement("div");
+    resultLine.innerText = `${resultArray[i].name}: ${printLength(
+      resultArray[i].count
+    )}`;
+    resultNode.appendChild(resultLine);
+  }
+
+  return resultNode;
+};
+
+export const printRaceResult = (resultArray) => {
+  const resultNode = document.createElement("p");
+
+  const maxValue = resultArray
+    .map((car) => car.count)
+    .reduce((pre, val) => (val > pre ? val : pre));
+  const victoryCarArray = resultArray.filter((car) => car.count === maxValue);
+  const resultCarNameArray = victoryCarArray.map((car) => car.name);
+
+  resultNode.innerText = `최종 우승자: ${resultCarNameArray.join(", ")} `;
+
+  resultNode.appendChild(createRestartButton());
+
+  return resultNode;
+};
+
 const createRestartButton = () => {
   const buttonNode = document.createElement("button");
   buttonNode.innerText = "재시작";
