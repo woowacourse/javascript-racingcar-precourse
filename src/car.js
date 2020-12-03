2ddcdc02a0232aa4ac852075d06e37a9c3839edf
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-04 00:21:04 +0900

    fix(car.js) : 하드코딩 제거

--- src/car.js
+++ src/car.js
@@ -1,15 +1,17 @@
 export class Car {
     constructor(name) {
+        this.STOP_LIMIT_NUMBER = 3;
+        this.NUMBER_BOUNDARY = 9;
         this.name = name;
         this.position = "";
     }
 
     makeRandomNumber() {
-        return parseInt(Math.random() * 9);
+        return parseInt(Math.random() * this.NUMBER_BOUNDARY);
     }
 
     makeOneInningGame() {
-        if (this.makeRandomNumber() > 3) {
+        if (this.makeRandomNumber() > this.STOP_LIMIT_NUMBER) {
             return this.position += "-";
         }
     }
