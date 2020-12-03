feaeed66234b1343f62c991df8bb6c235b60b2df
shinsehantan <shinsehantan@users.noreply.github.com>
2020-12-03 23:55:02 +0900

    자동차 이름 제출 버튼에 이벤트 리스너 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 export default function RacingCarGame() {
+  addEventToCarNameSubmitBtn();
 }
-// export default class RacingCarGame {
-// }
-
-
 new RacingCarGame();
+
+function addEventToCarNameSubmitBtn() {
+  const carNameSubmitBtn = document.getElementsByTagName("button")[0];
+  carNameSubmitBtn.addEventListener("click", onCarNameSubmit);
+}
+
+function onCarNameSubmit() {
+  const carNameInput = document.getElementsByTagName("input")[0];
+  let carNamesString = carNameInput.value;
+
+  console.log(carNamesString);
+}
