1cb873349c93cedb449eb19c58622ffbe23e92d2
zereight <zereight@zereightui-MacBookPro.local>
2020-12-04 01:34:37 +0900

    feat: display toggle function by game rule

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const getCarNameInput = () => {
     const carNames = carNameInput.value.split(',');
     if (isValidCarName(carNames)) {
         game.setCars(carNames.map(carName => carName.trim()));
+        toggleDiplayNumOfTryArea();
     } else {
         alert("❌유효하지 않은 자동차 이름입니다.\n다시 입력해주세요.");
         carNameInput.value = '';
@@ -24,17 +25,32 @@ const getNumOfTry = () => {
     const numOfTryInput = document.getElementById("racing-count-input");
     if (isDigits(numOfTryInput.value)) {
         game.setNumOfTry(numOfTryInput.value);
+        toggleDiplayResultArea();
     } else {
         alert("❌유효하지 않은 시도 횟수입니다.\n다시 입력해주세요.");
         numOfTryInput.value = '';
     }
 }
 
+const toggleDiplayNumOfTryArea = () => {
+    const numOfTryArea = document.getElementById("app").querySelectorAll("div + div")[0];
+    numOfTryArea.style.display = numOfTryArea.style.display === "none" ? "block" : "none";
+}
+
+const toggleDiplayResultArea = () => {
+    const resultArea = document.getElementById("app").querySelectorAll("div + div")[1];
+    resultArea.style.display = resultArea.style.display === "none" ? "block" : "none";
+}
+
 const init = () => {
     const carNameSubmitBtn = document.getElementById('car-names-submit');
     const numOfTrySubmitBtn = document.getElementById('racing-count-submit');
+
     carNameSubmitBtn.addEventListener('click', getCarNameInput);
     numOfTrySubmitBtn.addEventListener('click', getNumOfTry);
+
+    toggleDiplayNumOfTryArea();
+    toggleDiplayResultArea();
 };
 
 export default function RacingCarGame() {
