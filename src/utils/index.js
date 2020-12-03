40aa7d2b76391cd44daa2369ea178055c153ad63
Daniel <elrion018@gmail.com>
2020-12-03 19:29:58 +0900

    fix : isNames return false if blank

--- src/utils/index.js
+++ src/utils/index.js
@@ -32,6 +32,10 @@ function isNumber(count) {
 }
 
 function isNames(names) {
+  if (names[0] === '') {
+    return false;
+  }
+
   for (let i = 0; i < names.length; i++) {
     let name = Array.from(new Set(names[i]));
     if (name.length === 1 && name[0] === ' ') {
