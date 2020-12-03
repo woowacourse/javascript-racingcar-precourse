5a7e980d28d1d73aacddc6d2acbb26740eb13934
yungo1846 <yungo1468@naver.com>
2020-12-03 22:13:30 +0900

    버튼 클릭 이벤트 핸들러 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+import InputCheck from "./inputCheck.js";
+const inputCheck = new InputCheck();
 
+export default function RacingCarGame() {
+  let textSubmitBtn = document.getElementById("car-names-submit");
+  let numSubmitBtn = document.getElementById("racing-count-submit");
+  let words;
+  let inputStrings;
+  let inputNum;
+  let isTextSubmitted = false;
+
+  this.onTextSubmit = () => {
+    inputStrings = document.getElementById("car-names-input").value;
+    words = inputCheck.textCheck(inputStrings);
+    isTextSubmitted = true;
+  };
+
+  this.onNumSubmit = () => {
+    inputNum = document.getElementById("racing-count-input").value;
+    if (isTextSubmitted) {
+    }
+  };
+
+  textSubmitBtn.addEventListener("click", this.onTextSubmit);
+  numSubmitBtn.addEventListener("click", this.onNumSubmit);
+}
 
 new RacingCarGame();
---
+++ src/inputCheck.js
@@ -0,0 +1,14 @@
+export default function InputCheck() {
+  this.textCheck = (inputStrings) => {
+    let i;
+    const words = inputStrings.split(",");
+    for (i = 0; i < words.length; i++) {
+      if (words[i].length > 5) {
+        alert(`${words[i]}의 길이가 5를 초과합니다!`);
+        return;
+      } else {
+        return words;
+      }
+    }
+  };
+}
