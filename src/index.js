309cf316e44d9a99e88447d60fb4795e3ecf549f
Sun Jung Kim <blackmilk274@gmail.com>
2020-12-03 22:46:16 +0900

    feat    : 자동차 이름 입력받기
    - 확인 버튼을 누르면 사용자가 입력한 이름을 받아온다
    - 좌우 공백을 제외하고  받아온다 (이름 가운데 있는 공백은 포함)
    - 입력값이 없는 경우는 에러 발생
    - 이름이 가운데 공백을 포함하여 5자를 넘는 경우는 에러 발생
    
    close issue #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
-
+export default class RacingCarGame {}
 
 new RacingCarGame();
+
+const getCarNamesToList = function () {
+  const carNamesInput = document.querySelector('#car-names-input');
+  const regexp = /[^,\s]+[^,]*[^,\s]+/gi; // ,와 양쪽 공백을 제외한 부분만 가져온다
+  const carNamesList = carNamesInput.value.match(regexp);
+
+  if (!carNamesList) {
+    throw new Error('자동차 이름을 입력해주세요');
+  }
+  if (carNamesList.some(arr => arr.length > 5)) {
+    throw new Error('자동차 이름은 5자 이하로 입력해주세요');
+  }
+  return carNamesList;
+};
+
+const handleCarNamesSubmitBtn = function () {
+  try {
+    const carNamesList = getCarNamesToList();
+    // TODO: 이름대로 car instant 생성해서 이름 지정하기
+    // TODO: 숨겨둔 횟수 div 보이기
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+const carNamesSubmitBtn = document.querySelector('#car-names-submit');
+carNamesSubmitBtn.addEventListener('click', handleCarNamesSubmitBtn);
