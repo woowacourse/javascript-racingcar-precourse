b3f2bdb760bdfbbb9f6c1642263f5bdff0cc5ac5
jho2301 <jho2301@gmail.com>
2020-12-03 11:03:17 +0900

    feat: Component 추상클래스 추가

---
+++ src/library/core/component.js
@@ -0,0 +1,15 @@
+class Component {
+  _$target;
+  _props;
+
+  constructor($target, props) {
+    this._$target = $target;
+    this._props = props;
+  }
+
+  initializeEventListener() {}
+
+  render() {}
+}
+
+export default Component;
