38410b56ec1071742b2ce46e8ed453aac3416fdb
jum0 <wnsah052@naver.com>
2020-12-03 20:45:05 +0900

    feat : 자동차 이름을 검증하는 기능 구현

--- src/domain/carfacotory.js
+++ src/domain/carfacotory.js
@@ -1,5 +1,45 @@
 export default class CarFactory {
   constructor() {
-    console.log(document.querySelector("#car-names-input").value);
+    this.carNameArray = this.makeCarNameArray();
+
+    // this.makeCarNameArray();
+  }
+
+  makeCarNameArray() {
+    let _tmpCarNamesArray
+      = document.querySelector("#car-names-input").value.split(",");
+
+    // console.log(_tmpCarNamesArray);
+
+    _tmpCarNamesArray = this.validateName(_tmpCarNamesArray);
+
+    // console.log(_tmpCarNamesArray);
+
+  }
+
+  validateName(tmpCarNamesArray) {
+    let _validCarNamesArray = [];
+    let _carNameCandidate = "";
+
+    for (let i = 0; i < tmpCarNamesArray.length; i++) {
+      _carNameCandidate = tmpCarNamesArray[i].trim();
+
+      if (
+        _carNameCandidate != ""
+        && this.isValidLength(_carNameCandidate)
+      ) {
+        _validCarNamesArray.push(_carNameCandidate);
+      }
+    }
+
+    return _validCarNamesArray;
+  }
+
+  isValidLength(carNameCandiate) {
+    return (carNameCandiate.length >= 1 && carNameCandiate.length <= 5);
+  }
+
+  getCarNameArray() {
+    return this.carNameArray;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import CarFactory from "./domain/carfacotory.js";
 
 export default class RacingCarGame {
   constructor() {
+    this.clickCarNameSubmitBtn();
+  }
+
+  clickCarNameSubmitBtn() {
     const _carNamesSubmitElement = document.querySelector("#car-names-submit");
 
-    _carNamesSubmitElement.addEventListener("click", () => { new CarFactory() });
+    _carNamesSubmitElement.addEventListener("click", () => { this.runGame() });
   }
-}
 
+  runGame() {
+    const _carFactory = new CarFactory();
+    const _carNameArray = _carFactory.getCarNameArray();
+
+    // console.log(_carNameArray);
+  }
+
+}
 
 new RacingCarGame();
