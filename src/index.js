1de224899d59caec74329805d33a534c3a68eb02
JinsuBaek <71721270+junsubaek@users.noreply.github.com>
2020-12-03 21:26:02 +0900

    Add: 차 이름 유효성 확인 함수추가

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@
 
 // new RacingCarGame();
 
-function handleCarNamesSubmitClick() {}
+function validateCarNames(carNamesInputValue) {
+    const isValid = carNamesInputValue.every(
+        (carName) => 0 < carName && carName < 6
+    );
+    return isValid;
+}
+
+function takeCarNames() {
+    const carNamesInput = document.querySelector("#car-names-input");
+    return carNamesInput.value.split(",");
+}
+
+function handleCarNamesSubmitClick() {
+    const carNamesInputValue = takeCarNames();
+    const isValid = validateCarNames(carNamesInputValue);
+}
 
 function init() {
     const carNamesSubmitButton = document.querySelector("#car-names-submit");
