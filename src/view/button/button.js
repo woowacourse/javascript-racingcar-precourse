fe9292a7f8342cf1335defc9a48eb8b8ca7b54a5
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-04 07:46:27 +0900

    Refactor: button element들 모듈로 분리

---
+++ src/view/button/button.js
@@ -0,0 +1,4 @@
+const carNamesButton = document.getElementsByTagName("button")[0];
+const racingCountButton = document.getElementsByTagName("button")[1];
+
+export {carNamesButton, racingCountButton};
