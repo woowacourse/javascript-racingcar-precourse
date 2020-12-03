57a6251f49d9f5c92061b0ca2229b593302f51dc
sunhpark42 <sunhpark42@gmail.com>
2020-12-04 00:01:21 +0900

    feat: get and check UserInputs about Cars, TryCount

--- src/index.js
+++ src/index.js
@@ -1,7 +1,62 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+/**
+ * @author sunhpark42 <sunhpark42@gmail.com>
+ */
+export default class RacingCarGame {
+  /**
+   * variable for racing car game
+   */
+  constructor() {
+    this.winners = [];
+    this.cars = [];
+  };
+  /**
+   * get Car lists
+   * @return {Array}
+   */
+  getCarList() {
+    const carList =
+      document.getElementById('car-names-input').nodeValue.split(',');
+    return carList;
+  };
+  /**
+   * check Validation of UserInput
+   * @param {Array} carList
+   * @return {boolean}
+   */
+  checkCarList(carList) {
+    let isValid = true;
 
+    if (carList.length <= 1) {
+      isValid = false;
+    }
+    carList.forEach((car) => {
+      if (car.length > 5) {
+        isValid = false;
+      }
+    });
+    return isValid;
+  };
+  /**
+   * getTryCount
+   * @return {number}
+   */
+  getTryCount() {
+    const tryCount =
+      Number(document.getElementById('#racing-count-input').nodeValue);
+    return tryCount;
+  };
+  /**
+   * check TryCount Validtion
+   * @param {number} tryCount
+   * @return {boolean}
+   */
+  checkTryCount(tryCount) {
+    const isValid = true;
+    if (tryCount <= 0) {
+      isValid = false;
+    }
+    return isValid;
+  }
+}
 
 new RacingCarGame();
