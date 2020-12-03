e7299ad0a7a4131d0a45ee896c2a25f6c34ceb26
sunhpark42 <sunhpark42@gmail.com>
2020-12-04 00:04:05 +0900

    feat: add Car module

---
+++ src/Car.js
@@ -0,0 +1,27 @@
+/**
+ * @author sunhpark42 <sunhpark42@gmail.com>
+ * Car module with getRandomNumber and Go
+ */
+export default class Car {
+  /**
+   * init Car by name, position.
+   * @param {string} name
+   */
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  };
+  /**
+   * getRandom Number 0 - 9
+   * @return {number}
+   */
+  getNumber() {
+    return Math.floor(Math.random() * Math.floor(10));
+  }
+  /**
+   * go foward 1 step.
+   */
+  go() {
+    this.position += 1;
+  };
+}
