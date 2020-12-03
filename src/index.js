13482cd75bb7f776893d7994ec7d0fac1662795a
kiminseob <kis6473@naver.com>
2020-12-03 17:35:11 +0900

    feat: 자동차 이름 입력값 콤마 분리 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-export default function RacingCarGame() {
+export default class RacingCarGame {
+  constructor(app) {
+    app.onclick = this.onClick.bind(this);
+    this.carNames = [];
+  }
+
+  setCarNames() {
+    const carNameInputValue = document.getElementById('car-names-input').value;
+    const carNames = this.splitCarNamesWithComma(carNameInputValue);
+  }
+
+  splitCarNamesWithComma(carNames) {
+    return carNames.split(',')
+        .filter(carName => carName.trim().length !== 0)
+        .map(carName => carName.trim());
+  }
+
+  onClick(event) {
+    const targetId = event.target.id;
+
+    if (targetId === 'car-names-submit') {
+      this.setCarNames();
+    }
+
+    if (targetId === 'racing-count-submit') {
+
+    }
+  }
 }
-// export default class RacingCarGame {
-// }
 
 
-new RacingCarGame();
+new RacingCarGame(document.getElementById('app'));
