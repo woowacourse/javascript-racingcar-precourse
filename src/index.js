43adb0f4100598428cfd41fe06a144281f7ab2ea
zereight <zereight@zereightui-MacBookPro.local>
2020-12-04 00:28:25 +0900

    feat: input car names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import { getRandomDigit, isOnlySpaceString } from './utils.js';
+import {
+    isUnderFiveDigits,
+    isOnlySpaceString
+} from './utils.js';
+import Car from './Car.js';
+
+const getCarNameInput = () => {
+    const carNameInput = document.getElementById('car-names-input');
+    const carNames = carNameInput.value.split(',');
+
+    game.setCars(carNames);
+};
+
+const init = () => {
+    const carNameSubmitBtn = document.getElementById('car-names-submit');
+    carNameSubmitBtn.addEventListener('click', getCarNameInput);
+};
 
 export default function RacingCarGame() {
-	console.log(isOnlySpaceString('    '));
+    let cars = [];
+    init();
+
+    this.setCars = (carNameArray) => {
+        cars = carNameArray.map(carName => new Car(carName));
+        console.log(cars.map(car => car.getCarName()));
+    }
 }
 
-new RacingCarGame();
+const game = new RacingCarGame();
