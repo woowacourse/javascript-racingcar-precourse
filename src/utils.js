4c7a0ec88a43df8c4cd016ae7e84c8f5721c0da6
zereight <zereight@zereightui-MacBookPro.local>
2020-12-04 00:41:03 +0900

    fix: bug fix (isUnderFiveDigits)
    길이조건 0초과 5이하로 수정

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,13 @@ export const getRandomDigit = () => {
 };
 
 export const isUnderFiveDigits = (string) => {
-    return string.length <= 5 ? true : false;
+    const len = string.length;
+    return (0 < len && len <= 5) ? true : false;
 };
 
 export const isOnlySpaceString = (string) => {
     const nonDuplicatedString = [...new Set(Array.from(string))];
+    console.log(nonDuplicatedString);
     if (nonDuplicatedString.length === 1) {
         if (nonDuplicatedString[0] === ' ') {
             return true;
