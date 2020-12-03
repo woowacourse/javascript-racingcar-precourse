e637558e12c906eb6969681b0eaf9e441a3fae05
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-03 23:47:22 +0900

    feat : 랜덤숫자를 바탕으로 자동차 위치를 변경하는 기능 구현(4이상이면 전진)

--- src/car.js
+++ src/car.js
@@ -1,11 +1,17 @@
 export class Car {
     constructor(name) {
         this.name = name;
+        this.position = "";
     }
 
     makeRandomNumber() {
         return (Math.random() * 9)
     }
 
+    makeOneInningGame() {
+        if (this.makeRandomNumber() > 3) {
+            return this.position += "-"
+        }
+    }
 
 }
