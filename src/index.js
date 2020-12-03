9168208320a5f2ea14f3fe7f1bda7ae17046829b
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 01:26:27 +0900

    feat: 자동차 이름 입력값 받는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+	function getCarNamesInput() {
+		const carNamesSubmit = document.querySelector("#car-names-submit");
+		carNamesSubmit.addEventListener("click", () => {
+			const carNamesInput = document.querySelector("#car-names-input").value;
+			console.log(carNamesInput)
+			return carNamesInput
+		}); 
+	}
 
+	function init() {
+		getCarNamesInput();
+	}
+
+	init();
+}
 
 new RacingCarGame();
