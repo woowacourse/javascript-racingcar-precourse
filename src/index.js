b43434db48d3cf8d53e92dab252acdefbe536514
shinsehantan <shinsehantan@users.noreply.github.com>
2020-12-04 00:00:19 +0900

    입력받은 자동차 이름들로 자동차 그룹 어레이 생성

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ export default function RacingCarGame() {
 }
 new RacingCarGame();
 
+// 자동차 이름 제출 버튼에 이벤트 리스너 달기
 function addEventToCarNameSubmitBtn() {
   const carNameSubmitBtn = document.getElementsByTagName("button")[0];
   carNameSubmitBtn.addEventListener("click", onCarNameSubmit);
 }
 
+// 전체 자동차 이름 string으로 받기
 function onCarNameSubmit() {
   const carNameInput = document.getElementsByTagName("input")[0];
   let carNamesString = carNameInput.value;
 
-  console.log(carNamesString);
+  makeCarNameArray(carNamesString);
+}
+
+// string으로 받은 이름 콤마(,) 기준으로 어레이에 담기
+function makeCarNameArray(carNamesString) {
+  let carNamesArr = carNamesString.split(",");
 }
