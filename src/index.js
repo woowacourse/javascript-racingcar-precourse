f5c08e791017d0b612666578e5a654a99678c346
shinsehantan <shinsehantan@users.noreply.github.com>
2020-12-04 02:55:41 +0900

    실행 결과창에 자동차의 이름과 랜덤 숫자 출력해보기

--- src/index.js
+++ src/index.js
@@ -22,34 +22,53 @@ function onCarNameSubmit() {
 
 // string으로 받은 이름 콤마(,) 기준으로 어레이에 담기
 function makeCarNameArray(carNamesString) {
-  let carNamesArr = carNamesString.split(",");
+  let carNames = carNamesString.split(",");
 
-  makeCarObj(carNamesArr);
+  makeCarObj(carNames);
 }
 
 // 각 자동차에 이름과 인덱스, 랜덤 넘버 부여한 객체 어레이 생성
-function makeCarObj(carNamesArr) {
-  const randoms = makeRandomNumber(carNamesArr);
+function makeCarObj(carNames) {
+  const randoms = makeRandomNumber(carNames);
   let carsArr = [];
 
-  for (let i = 0; i < carNamesArr.length; i++) {
+  for (let i = 0; i < carNames.length; i++) {
     let carObj = {
-      name: `${carNamesArr[i]}`,
+      name: `${carNames[i]}`,
       index: `${i + 1}`,
       randomNum: randoms[i],
     };
     carsArr.push(carObj);
   }
   console.log(carsArr, carsArr.length);
+  makeResultBox(carsArr);
 }
 
 // 랜덤 숫자 생성
-function makeRandomNumber(carNamesArr) {
+function makeRandomNumber(carNames) {
   let randoms = [];
 
-  for (let i = 0; i < carNamesArr.length; i++) {
+  for (let i = 0; i < carNames.length; i++) {
     let randomNumber = Math.floor(Math.random() * 10);
     randoms.push(randomNumber);
   }
   return randoms;
+}
+
+// 실행 결과창에 자동차의 이름, 랜덤숫자 출력해보기
+function makeResultBox(carsArr) {
+  const resultHead = document.getElementsByTagName("h4")[1];
+  resultHead.id = "result-head";
+
+  const resultBox = document.createElement("div");
+  resultBox.id = "result-box";
+  resultHead.append(resultBox);
+
+  printResult(resultBox, carsArr);
+}
+
+function printResult(resultBox, carsArr) {
+  for (let i = 0; i < carsArr.length; i++) {
+    resultBox.innerHTML += `${carsArr[i].name}: ${carsArr[i].randomNum}<br />`;
+  }
 }
