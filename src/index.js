85e221d3b1a4aa3cc05a8f3514417415d01a9ad4
kiminseob <kis6473@naver.com>
2020-12-03 17:41:40 +0900

    feat: 자동차 이름 입력값 검증 기능 구현
    - 올바른 입력 : 5자 이하

--- src/index.js
+++ src/index.js
@@ -7,12 +7,23 @@ export default class RacingCarGame {
   setCarNames() {
     const carNameInputValue = document.getElementById('car-names-input').value;
     const carNames = this.splitCarNamesWithComma(carNameInputValue);
+
+    if (!this.isValidCarNames(carNames)) {
+      return alert('자동차 이름은 5자 이하로 입력해주세요.');
+    }
+
+    this.carNames = carNames;
+    console.log(this.carNames);
   }
 
   splitCarNamesWithComma(carNames) {
     return carNames.split(',')
         .filter(carName => carName.trim().length !== 0)
         .map(carName => carName.trim());
+  }
+
+  isValidCarNames(carNames) {
+    return !carNames.some(carName => carName.length > 5);
   }
 
   onClick(event) {
