c04c58e765416928a73067448bdc71e0a818f898
jho2301 <jho2301@gmail.com>
2020-12-03 12:22:04 +0900

    feat: 자동차 이름 입력 검증하기 구현

--- src/components/car-game-container.js
+++ src/components/car-game-container.js
@@ -34,8 +34,65 @@ class CarGameContainer extends Component {
 
   setCarNames() {
     const input = this.$carNamesInput.value;
-    const carNames = input.split(',').map(carName => carName.trim());
+    let carNames;
+    if (!this.isValidateInput(input)) {
+      this.alertByCase(input);
+      this.$carNamesInput.value = '';
+      this.$carNamesInput.focus();
+    }
+    carNames = input.split(',').map(carName => carName.trim());
     console.log(carNames);
+  }
+
+  isValidateInput(input) {
+    const carNames = input.split(',').map(carName => carName.trim());
+    if (input === '') {
+      return false;
+    }
+    if (this.hasNameWithMoreThan5Letters(carNames)) {
+      return false;
+    }
+    if (this.hasEmptyName(carNames)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  hasNameWithMoreThan5Letters(carNames) {
+    for (const carName of carNames) {
+      if (carName.length > 5) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  hasEmptyName(carNames) {
+    for (const carName of carNames) {
+      if (carName.length === 0) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  alertByCase(input) {
+    const carNames = input.split(',').map(carName => carName.trim());
+    let errorMessage = [];
+    if (input === '') {
+      errorMessage.push('공백');
+    } else {
+      if (this.hasNameWithMoreThan5Letters(carNames)) {
+        errorMessage.push('5자가 넘는 이름');
+      }
+      if (this.hasEmptyName(carNames)) {
+        errorMessage.push('공백인 이름');
+      }
+    }
+    alert(`${errorMessage.join(', ')}을 입력하셨습니다. 다시 입력해주세요`);
   }
 }
 
