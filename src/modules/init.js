e997a3364316cf480d2ec09d14cad97f3c02e4f2
unknown <dudtjr913@naver.com>
2020-12-03 16:45:37 +0900

    feat : 다시 시작 후 input 초기화 기능 추가 <description> 다시시작을 했을 때 input이 초기화가 되지 않는 것을 발견 - 추가로 구현

--- src/modules/init.js
+++ src/modules/init.js
@@ -2,11 +2,17 @@ import {
   hideCounterElement,
   hideResultElement,
 } from '../elements/hideElement.js';
-import {carInf} from './utils.js';
+import {carInf, $carNameInput, $timesInput} from './utils.js';
 import RacingCarGame from '../class/RacingCarGame.js';
 
 export default function initGame() {
   hideCounterElement();
   hideResultElement();
+  inputReset();
   carInf.car = new RacingCarGame();
 }
+
+const inputReset = () => {
+  $carNameInput.value = '';
+  $timesInput.value = '';
+};
