40c7653f91a90ae4a7c26bce16cec84f94e62983
dande <7bdande@gmail.com>
2020-12-04 01:15:19 +0900

    feat: 자동차 이름의 유효성을 검사하는 메서드 추가
    자동차 이름의 길이가 1에서 5 사이고, 자동차가 2대 이상일 때 true를 반환하는 메서드 isValidCarNames 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-export default function RacingCarGame() {
+function isValidCarNames(carNames) {
+  const MAX_CAR_NAME_LENGTH = 5;
+  const isLValidCarNameLength = carNames.every(
+    name => name.length > 0 && name.length <= MAX_CAR_NAME_LENGTH
+  );
+  const isMoreThanTwoCars = carNames.length > 1;
+
+  if (isLValidCarNameLength && isMoreThanTwoCars) {
+    return true;
+  }
+
+  return false;
 }
-// export default class RacingCarGame {
-// }
 
 
-new RacingCarGame();
