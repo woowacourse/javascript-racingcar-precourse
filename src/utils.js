f522e4d7f50b592871dc78f9fdd7b6129664d866
zereight <zereight@zereightui-MacBookPro.local>
2020-12-04 00:48:38 +0900

    feat: func isDigit
    입력이 숫자인지 판단하는 기능

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,11 @@ export const getRandomDigit = () => {
     return Math.floor(Math.random() * 10);
 };
 
+export const isDigits = (string) => {
+    const nums = "0123456789";
+    return [...string].every(str => nums.includes(str));
+}
+
 export const isUnderFiveDigits = (string) => {
     const len = string.length;
     return (0 < len && len <= 5) ? true : false;
