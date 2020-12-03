c85f1925c9315674bea1b6b29e0ad51b623f8aa2
intae92 <kangit2013@gmail.com>
2020-12-03 22:00:12 +0900

    feat: car names 올바른 입력값 판단

--- src/js/events/getCarNamesBtnClick.js
+++ src/js/events/getCarNamesBtnClick.js
@@ -1,4 +1,5 @@
 import Component from "../components/initialComponent.js";
+import { isCorrectCarNames } from "../../utils/vaildValue.js";
 
 export default class GetCarNames extends Component {
   constructor() {
@@ -11,14 +12,15 @@ export default class GetCarNames extends Component {
       this._onClickCarNames.bind(this)
     );
     this.isHiddenRacingCountDiv = true;
+    this.inputCarNames = "";
   }
 
   _visiable() {
     try {
       if (!this.isHiddenRacingCountDiv) return;
 
-      this.target = document.querySelector(".js-hidden__div");
-      this.target.classList.remove("js-hidden__div");
+      const target = document.querySelector(".js-hidden__div");
+      target.classList.remove("js-hidden__div");
     } catch (e) {
       console.log(e);
     }
@@ -26,6 +28,13 @@ export default class GetCarNames extends Component {
 
   _onClickCarNames() {
     console.log(this.carNamesInput.value);
+    this.inputCarNames = this.carNamesInput.value.split(",");
+
+    if (!isCorrectCarNames(this.inputCarNames)) {
+      alert("잘못된 입력값 입니다");
+      return;
+    }
+
     this._visiable();
     this.isHiddenRacingCountDiv = false;
   }
--- src/utils/vaildValue.js
+++ src/utils/vaildValue.js
@@ -1,1 +1,24 @@
-export default () => {};
+/*
+isCorrectCarNames
+- 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
+- 자동차 이름 1자 이상 제한
+- 알파벳,한글,숫자로 구성된 이름만 가능
+- 중복 이름 가능
+- 공백 포함된 이름 안됨
+*/
+
+const rKorean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+const rNumber = /[0-9]/;
+const rAlphabet = /[a-zA-Z]/;
+const rEmpty = /\s/g;
+
+export const isCorrectCarNames = (inputCarNames) => {
+  for (const carName of inputCarNames) {
+    const carNamelength = carName.length;
+
+    if (rEmpty.test(carName)) return false;
+    if (carNamelength > 5 || carNamelength < 1) return false;
+  }
+
+  return true;
+};
