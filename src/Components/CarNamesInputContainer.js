8f1e971a9924aaa4d365540a2db0e7e2e99fcd45
2SOOY <1401660@naver.com>
2020-12-04 14:56:13 +0900

    Feat - 자동차 이름을 입력받을 입력 컴포넌트 추가 : CarNamesInputContainer

---
+++ src/Components/CarNamesInputContainer.js
@@ -0,0 +1,11 @@
+class CarNamesInputContainer {
+  constructor({ $target }) {
+    this.$target = $target;
+
+    this.bindEvents();
+  }
+
+  bindEvents() {}
+}
+
+export default CarNamesInputContainer;
