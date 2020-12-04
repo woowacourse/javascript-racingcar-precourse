74cb8c1b1ee427f056c21a9d5e705156ed1d34e5
ehddnr301 <dy950328@gmail.com>
2020-12-04 12:01:05 +0900

    Refactor remove anonymous function

--- src/util/handler/index.js
+++ src/util/handler/index.js
@@ -1,19 +1,21 @@
 import { namesValidation } from "../input/index.js";
 
-const namesButtonClick = (namesInput) => {
+const namesButtonClick = () => {
+  const namesInput = document.getElementById("car-names-input");
   const names = namesInput.value;
+
   const isValid = namesValidation(names, namesInput);
+
+  if (isValid) {
+    const namesButton = document.getElementById("car-names-submit");
+    namesButton.removeEventListener("click", namesButtonClick);
+  }
 };
 
 const handler = () => {
   const namesButton = document.getElementById("car-names-submit");
-  const namesInput = document.getElementById("car-names-input");
 
-  if (namesButton) {
-    namesButton.addEventListener("click", () => {
-      namesButtonClick(namesInput, namesInput);
-    });
-  }
+  namesButton.addEventListener("click", namesButtonClick);
 };
 
 export default handler;
