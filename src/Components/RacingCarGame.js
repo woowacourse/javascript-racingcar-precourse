94bc450f443188063b6c05d03488a7c21d4c60e1
2SOOY <1401660@naver.com>
2020-12-04 16:47:56 +0900

    Feat - RacingCarGame 컴포넌트 state 설계 및 초기화 메소드 추가
    - state로 라운드 수랑 플레이어 정보로 설계
    - 라운드 수 => 라운드 당 플레이어의 이동 거리 표현 가능
    - 플레이어의 이동거리 정보를 통해 승자를 구할 수 있음

--- src/Components/RacingCarGame.js
+++ src/Components/RacingCarGame.js
@@ -1,7 +1,17 @@
 class RacingCarGame {
   constructor({ $target }) {
     this.$target = $target;
+    this.state = this.initState();
   }
+
+  initState() {
+    return {
+      round: 0,
+      players: [],
+    };
+  }
+
+  setPlayers() {}
 }
 
 export default RacingCarGame;
