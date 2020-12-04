be7f8e0d675c203da2ea6b6a9dcd04fb4b1c11bf
yujo <yujo@student.42seoul.kr>
2020-12-04 16:13:02 +0900

    fix: 이벤트리스너가 여러번 등록되는 이슈 해결
    함수가 재귀적으로 호출되면서 이벤트리스너가 여러번 등록되는걸 플래그를 통해 방지

--- src/game/playGame.js
+++ src/game/playGame.js
@@ -3,13 +3,15 @@ import getCarNames from '../input/getCarNames.js';
 import getRacingCount from '../input/getRacingCount.js';
 import toggleRaceNumberInputScreen from '../init/toggleRaceNumberInputScreen.js';
 
-export default function playGame(carList = '', value = 0) {
-  const $carNamesSubmit = document.querySelector('#car-names-submit');
-  $carNamesSubmit.addEventListener('click', getCarNames);
+export default function playGame(carList = '', value = 0, recursiveCount = 0) {
+  if (!recursiveCount) {
+    const $carNamesSubmit = document.querySelector('#car-names-submit');
+    $carNamesSubmit.addEventListener('click', getCarNames);
+  }
 
   if (carList) {
     toggleRaceNumberInputScreen();
-    getRacingCount(carList);
+    getRacingCount(carList, recursiveCount);
   }
   if (carList && value) {
     toggleRaceNumberInputScreen();
--- src/input/getRacingCount.js
+++ src/input/getRacingCount.js
@@ -7,19 +7,21 @@ function checkValidRacingCount(racingCount) {
   return false;
 }
 
-function getRacingCountValue(carList) {
+function getRacingCountValue(carList, recursiveCount) {
   const $racingCountInput = document.querySelector('#racing-count-input');
   const racingCount = $racingCountInput.value;
 
   if (checkValidRacingCount(racingCount)) {
-    return playGame(carList, Number(racingCount));
+    return playGame(carList, Number(racingCount), recursiveCount + 1);
   }
   return alert('잘못 된 입력입니다! 0이상의 숫자만 입력 가능합니다');
 }
 
-export default function getRacingCount(carList) {
-  const $racingCountSubmit = document.querySelector('#racing-count-submit');
-  $racingCountSubmit.addEventListener('click', () =>
-    getRacingCountValue(carList),
-  );
+export default function getRacingCount(carList, recursiveCount) {
+  if (!recursiveCount) {
+    const $racingCountSubmit = document.querySelector('#racing-count-submit');
+    $racingCountSubmit.addEventListener('click', () =>
+      getRacingCountValue(carList, recursiveCount),
+    );
+  }
 }
