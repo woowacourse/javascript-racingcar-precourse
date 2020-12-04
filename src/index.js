b5a90f1f1981c3c53d71c4816fd8be0a02bc4a83
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 13:33:36 +0900

    feat: 공백 유무 확인 기능

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 export default function RacingCarGame() {
+	function isSpace(carNamesInput) {
+		return carNamesInput.search(/\s/) !== -1
+	}
+
 	function getCarNamesInput() {
 		const carNamesSubmit = document.querySelector("#car-names-submit");
 		carNamesSubmit.addEventListener("click", () => {
 			const carNamesInput = document.querySelector("#car-names-input").value;
-			console.log(carNamesInput)
+			
+			if (isSpace(carNamesInput) === true) {
+				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.")
+			}
+
 			return carNamesInput
 		}); 
 	}
