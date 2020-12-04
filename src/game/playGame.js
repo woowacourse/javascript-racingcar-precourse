30510d525861b0d49b8a2c8a30336bfa54b3c71b
yujo <yujo@student.42seoul.kr>
2020-12-04 19:47:30 +0900

    refactor: 가독성을 위해 이벤트리스너 등록하는 모듈 분리

--- src/game/playGame.js
+++ src/game/playGame.js
@@ -1,17 +1,11 @@
 import printResult from './printResult.js';
-import getCarNames from '../input/getCarNames.js';
 import getRacingCount from '../input/getRacingCount.js';
 import toggleRaceNumberInputScreen from '../init/toggleRaceNumberInputScreen.js';
 
-export default function playGame(carList = '', value = 0, recursiveCount = 0) {
-  if (!recursiveCount) {
-    const $carNamesSubmit = document.querySelector('#car-names-submit');
-    $carNamesSubmit.addEventListener('click', getCarNames);
-  }
-
+export default function playGame(carList = '', value = 0) {
   if (carList) {
     toggleRaceNumberInputScreen();
-    getRacingCount(carList, recursiveCount);
+    getRacingCount(carList);
   }
   if (carList && value) {
     toggleRaceNumberInputScreen();
--- src/init/initializeGame.js
+++ src/init/initializeGame.js
@@ -1,9 +1,11 @@
 import addTagID from './addTagID.js';
+import addEventListener from './addEventListener.js';
 import toggleRaceNumberInputScreen from './toggleRaceNumberInputScreen.js';
 import toggleResultScreen from './toggleResultScreen.js';
 
 export default function initializeGame() {
   addTagID();
+  addEventListener();
   toggleResultScreen();
   toggleRaceNumberInputScreen();
 }
