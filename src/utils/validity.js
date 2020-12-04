796aebed07601bd8f4a847295e885c9943bd8dfe
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-04 09:38:20 +0900

    Refactor: Validity 클래스의 메소드 개선

--- src/utils/validity.js
+++ src/utils/validity.js
@@ -1,25 +1,16 @@
-export default class Vadlidity {
-	isCarNamesValid = carNamesList => {
-		let validity = true;
-
-		for (let name = 0; name < carNamesList.length; name++) {
-			if (carNamesList[name].length > 5 || carNamesList[name].length <= 0) {
-				validity = false;
-				alert("자동차 이름을 5자 이하로 입력해주세요.");
-				break;
-			}
-		}
+export default class Validity {
+	isCarNameValid = carName => {
+		let validity = (0 > carName.length && carName.length <= 5) ? true : false;
+		
+		if (!validity) alert("자동차 이름을 5자 이하로 입력해주세요.");
 
 		return validity;
 	}
 
 	isRacingCountValid = racingCount => {
-		let validity = true;
+		let validity = (Number.isInteger(racingCount) && racingCount !== 0) ? true : false;
 
-		if (parseInt(racingCount) !== racingCount) {
-			validity = false;
-			alert("0보다 큰 정수를 입력해주세요.");
-		} 
+		if (!validity) alert("0보다 큰 정수를 입력해주세요.");
 
 		return validity;
 	}
