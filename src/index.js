c8352825a6ca2f3b9efc3beac5922fb5546c9aef
SEONBIN <tjsqls2003@gmail.com>
2020-12-04 22:06:27 +0900

    feat: edit index.js, add get car-names feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+import { setCarNames } from './user/actions.js';
 
+export default function RacingCarGame() {
+  const carNames = '';
+  const carNamesSubmitBtn = document.getElementById('car-names-submit');
+
+  carNamesSubmitBtn.addEventListener('click', () => {
+    this.carNames = setCarNames();
+
+    if (!this.carNames) {
+      return;
+    }
+
+    document.getElementById('racing-count-container').hidden = false;
+  });
+}
 
 new RacingCarGame();
