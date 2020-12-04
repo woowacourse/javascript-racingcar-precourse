68fcc223cc5f18514c24b4e6f57bfb0450ffed97
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 15:09:18 +0900

    refactor: 예외문자 유무 확인 기능 함수명 변경

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default function RacingCarGame() {	
-	function isSpecial(carNamesInput) {
+	function isSpecialCharacter(carNamesInput) {
 		const specialCharacter = /[^가-힣ㄱ-하-ㅣa-zA-z0-9,]/;
 		return specialCharacter.test(carNamesInput);
 	}
@@ -9,7 +9,7 @@ export default function RacingCarGame() {
 		carNamesSubmit.addEventListener("click", () => {
 			const carNamesInput = document.querySelector("#car-names-input").value;
 
-			if (isSpecial(carNamesInput) === false) {
+			if (isSpecialCharacter(carNamesInput) === false) {
 				return carNamesInput;
 			} else {
 				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
