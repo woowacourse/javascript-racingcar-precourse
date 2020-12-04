4f5ed593d3469e7872a78cf99c539f9277c2442f
SEONBIN <tjsqls2003@gmail.com>
2020-12-04 22:38:51 +0900

    feat: edit index.js, add get racing-count feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { setCarNames } from './user/actions.js';
+import { setCarNames, getRacingCount } from './user/actions.js';
 
 export default function RacingCarGame() {
   const carNames = '';
+  const racingCount = '';
   const carNamesSubmitBtn = document.getElementById('car-names-submit');
+  const racingCountSubmitBtn = document.getElementById('racing-count-submit');
 
   carNamesSubmitBtn.addEventListener('click', () => {
     this.carNames = setCarNames();
@@ -12,6 +14,19 @@ export default function RacingCarGame() {
     }
 
     document.getElementById('racing-count-container').hidden = false;
+  });
+
+  racingCountSubmitBtn.addEventListener('click', () => {
+    this.racingCount = getRacingCount();
+
+    if (!this.racingCount) {
+      return;
+    }
+
+    // 게임 진행
+    console.log(this.carNames, this.racingCount);
+
+    document.getElementById('game-result-container').hidden = false;
   });
 }
 
