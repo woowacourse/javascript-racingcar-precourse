8b55ebd573581d80aca6ebdd8dc3473353302683
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 15:01:30 +0900

    refactor: 공백 유무 확인 기능 삭제

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-export default function RacingCarGame() {
-	function isSpace(carNamesInput) {
-		return carNamesInput.search(/\s/) !== -1;
-	}
-	
+export default function RacingCarGame() {	
 	function isSpecial(carNamesInput) {
 		const specialCharacter = /[^가-힣ㄱ-하-ㅣa-zA-z0-9,]/;
 		return specialCharacter.test(carNamesInput);
@@ -13,7 +9,7 @@ export default function RacingCarGame() {
 		carNamesSubmit.addEventListener("click", () => {
 			const carNamesInput = document.querySelector("#car-names-input").value;
 
-			if (isSpace(carNamesInput) === true || isSpecial(carNamesInput) === true) {
+			if (isSpecial(carNamesInput) === true) {
 				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
 				return false;
 			}
