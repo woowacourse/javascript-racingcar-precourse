8150e6b38998d49879c205482acc5586737e6fc0
2SOOY <1401660@naver.com>
2020-12-04 15:07:51 +0900

    Feat - DOM 선택자에 대한 상수 파일 추가
    - DOM 선택자를 상수화 처리하여 변수로 활용

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const ID = {
+  APP: "app",
+
+  CAR_NAMES_INPUT_CONTAINER: "car-names-container",
+  CAR_NAMES_INPUT: "car-names-input",
+  CAR_NAMES_SUBMIT_BUTTON: "car-names-submit",
+
+  RACING_COUNT_INPUT_CONTAINER: "racing-count-container",
+  RACING_COUNT_INPUT: "racing-count-input",
+  RACING_COUNT_SUBMIT_BUTTON: "racing-count-submit",
+
+  RACING_RESULT_CONTAINER: "racing-result-container",
+};
