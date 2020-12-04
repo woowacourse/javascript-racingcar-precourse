e1f20bb2b0e18d1802c30416c834782974418b50
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 13:36:44 +0900

    style: 누락된 세미콜론 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default function RacingCarGame() {
 	function isSpace(carNamesInput) {
-		return carNamesInput.search(/\s/) !== -1
+		return carNamesInput.search(/\s/) !== -1;
 	}
 
 	function getCarNamesInput() {
@@ -9,10 +9,10 @@ export default function RacingCarGame() {
 			const carNamesInput = document.querySelector("#car-names-input").value;
 			
 			if (isSpace(carNamesInput) === true) {
-				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.")
+				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
 			}
 
-			return carNamesInput
+			return carNamesInput;
 		}); 
 	}
 
