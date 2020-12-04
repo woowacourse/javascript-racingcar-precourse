da65e2bae87ff97b73ca2ba57eac0e247ceb8151
intae92 <kangit2013@gmail.com>
2020-12-04 11:53:49 +0900

    set: car names, racing count 변수 선언

--- src/js/eventsHandler.js
+++ src/js/eventsHandler.js
@@ -3,5 +3,13 @@ import GetRacingCount from "./events/getRacingCountBtnClick.js";
 export default class EventHandler extends GetRacingCount {
   constructor() {
     super();
+    this.carNames = "";
+    this.racingCount = "";
+  }
+
+  _onClickRacingCount() {
+    super._onClickRacingCount();
+    this.carNames = this.carNamesInput.value;
+    this.racingCount = this.racingCountInput.value;
   }
 }
