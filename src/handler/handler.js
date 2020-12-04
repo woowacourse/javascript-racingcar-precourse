ab532ff486e0a9023b5403cad79ad1df055497ed
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-04 10:37:33 +0900

    Refactor: Handler 클래스 삭제

--- src/handler/handler.js
+++
@@ -1,7 +0,0 @@
-export default class Handler {
-	setClickHandler = (element, callbacks) => {
-		for (let callback = 0; callback < callbacks.length; callback++) {
-			element.addEventListener("click", callbacks[callback]);
-		}
-	}
-}
