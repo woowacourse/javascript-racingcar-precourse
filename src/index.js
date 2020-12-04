c0de9afa0bba5d00897bd1d85d28990202504720
Teo_Hwang <taeung2008@gmail.com>
2020-12-04 17:56:52 +0900

    feat: 시도 횟수 입력받는 기능 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Car from './Car.js';
 
 export default class RacingCarGame {
   constructor() {
+    this.carNames = [];
     this.submitCarNames();
   }
 
@@ -10,15 +11,11 @@ export default class RacingCarGame {
     const carNamesSubmitButton = document.querySelector('#car-names-submit');
 
     carNamesSubmitButton.addEventListener('click', () => {
-      const carNames = new CarName();
-      let carObjArray = [];
-
-      if (carNames.names != null) {
+      this.carNames = new CarName();
+      if (this.carNames.names != null) {
         this.showCountRacing();
-        carObjArray = this.setCar(carNames.names);
-        //시도 횟수 받기
+        this.runRacing(this.carNames.names);
       }
-      console.log(carObjArray);
     });
   }
 
@@ -26,6 +23,38 @@ export default class RacingCarGame {
     const countContainer = document.querySelector('#count-container');
 
     countContainer.style.display = 'block';
+  }
+
+  runRacing(_carNames) {
+    const racingCountSubmit = document.querySelector('#racing-count-submit');
+    let countNumber = 0;
+
+    racingCountSubmit.addEventListener('click', () => {
+      countNumber = this.getCountNumber();
+
+      this.repeatRunRacing(countNumber, _carNames);
+    });
+  }
+
+  getCountNumber() {
+    const racingCountInput = document.querySelector('#racing-count-input');
+
+    return racingCountInput.value;
+  }
+
+  repeatRunRacing(_countNumber, _carNames) {
+    const carObjArray = this.setCar(_carNames);
+
+    if (_countNumber > 0) {
+      for (let i = 0; i < _countNumber; i++) {
+        /*
+         * 생성한 Car 객체 여러번 반복하여 실행시키기
+         * or
+         * 숫자를 그대로 넣어서 Car객체 안에서 돌리기(Car(_name, _count))
+         */
+        console.log(carObjArray);
+      }
+    }
   }
 
   setCar(_carNames) {
