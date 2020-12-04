9f135c2523b7605e98e00a7b7fd07ab142b3a6fb
yujo <yujo@student.42seoul.kr>
2020-12-04 15:49:24 +0900

    refactor: 경주 횟수 입력값의 타입을 Number로 변경해서 다음 함수에 전달하도록 변경
    리턴 타입을 String -> Number 로 변경

--- src/input/getRacingCount.js
+++ src/input/getRacingCount.js
@@ -1,3 +1,5 @@
+import playGame from '../game/playGame.js';
+
 function checkValidRacingCount(racingCount) {
   if (Math.sign(racingCount) === 1) {
     return true;
@@ -5,17 +7,19 @@ function checkValidRacingCount(racingCount) {
   return false;
 }
 
-function getRacingCountValue() {
+function getRacingCountValue(carList) {
   const $racingCountInput = document.querySelector('#racing-count-input');
   const racingCount = $racingCountInput.value;
 
   if (checkValidRacingCount(racingCount)) {
-    return console.log('ok');
+    return playGame(carList, Number(racingCount));
   }
   return alert('잘못 된 입력입니다! 0이상의 숫자만 입력 가능합니다');
 }
 
-export default function getRacingCount() {
+export default function getRacingCount(carList) {
   const $racingCountSubmit = document.querySelector('#racing-count-submit');
-  $racingCountSubmit.addEventListener('click', getRacingCountValue);
+  $racingCountSubmit.addEventListener('click', () =>
+    getRacingCountValue(carList),
+  );
 }
