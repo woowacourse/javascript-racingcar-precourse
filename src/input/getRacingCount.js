33ca307f80eb66d02e9523cc23ebe79b55617272
yujo <yujo@student.42seoul.kr>
2020-12-04 15:43:53 +0900

    fix: 경주 횟수를 잘못 입력했을 때 알림이 나오지 않는 문제 해결
    함수를 리팩토링 하면서 필요한 부분까지 지워버려서 해당 부분 다시 복구함

--- src/input/getRacingCount.js
+++ src/input/getRacingCount.js
@@ -1,11 +1,21 @@
-function checkValidRacingCount(value) {
-  if (Math.sign(value) === 1) {
+function checkValidRacingCount(racingCount) {
+  if (Math.sign(racingCount) === 1) {
     return true;
   }
   return false;
 }
 
+function getRacingCountValue() {
+  const $racingCountInput = document.querySelector('#racing-count-input');
+  const racingCount = $racingCountInput.value;
+
+  if (checkValidRacingCount(racingCount)) {
+    return console.log('ok');
+  }
+  return alert('잘못 된 입력입니다! 0이상의 숫자만 입력 가능합니다');
+}
+
 export default function getRacingCount() {
   const $racingCountSubmit = document.querySelector('#racing-count-submit');
-  $racingCountSubmit.addEventListener('click', checkValidRacingCount);
+  $racingCountSubmit.addEventListener('click', getRacingCountValue);
 }
