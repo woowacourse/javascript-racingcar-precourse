8eafe484ef1a678d6f0c162d1b205ea2bb616df4
2SOOY <1401660@naver.com>
2020-12-04 21:34:43 +0900

    Feat - 라운드 횟수를 입력받을 컴포넌트 추가 : RacingCountInputContainer.js

---
+++ src/Components/RacingCountInputContainer.js
@@ -0,0 +1,13 @@
+class RacingCountContainer {
+  constructor({ $target, setRound }) {
+    this.$target = $target;
+
+    this.setRound = setRound;
+
+    this.bindEvents();
+  }
+
+  bindEvents() {}
+}
+
+export default RacingCountContainer;
