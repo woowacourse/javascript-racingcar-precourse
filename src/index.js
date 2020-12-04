9f8a41cc1f32d4c6968d84734920a171bf375ee9
Sun Jung Kim <blackmilk274@gmail.com>
2020-12-04 18:09:53 +0900

    feat : 자동차 이름 입력시 유효하지 않은 값 알림 기능 추가
    - 커스텀 에러 객체 구현
    - 에러 alert 후 오류난 input에 focus 처리
    
    this fixes #5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import ValidationError from './validationError.js';
+
 export default class RacingCarGame {}
 
 new RacingCarGame();
@@ -8,11 +10,15 @@ const getCarNamesToList = function () {
   const carNamesList = carNamesInput.value.match(regexp);
 
   if (!carNamesList) {
-    throw new Error('자동차 이름을 입력해주세요');
+    throw new ValidationError('자동차 이름을 입력해주세요', carNamesInput);
   }
   if (carNamesList.some(arr => arr.length > 5)) {
-    throw new Error('자동차 이름은 5자 이하로 입력해주세요');
+    throw new ValidationError(
+      '자동차 이름은 5자 이하로 입력해주세요',
+      carNamesInput,
+    );
   }
+  
   return carNamesList;
 };
 
@@ -23,6 +29,9 @@ const handleCarNamesSubmitBtn = function () {
     // TODO: 숨겨둔 횟수 div 보이기
   } catch (e) {
     alert(e.message);
+    if (e.errElem) {
+      e.errElem.focus();
+    } 
   }
 };
 
---
+++ src/validationError.js
@@ -0,0 +1,13 @@
+/**
+ * 사용자 입력이 유효하지 않을때 발생시키는 에러
+ * @param {string} message - 에러 메세지
+ * @param {node} errElem - 에러가 발생한 원인이 되는 엘리먼트
+ */
+
+export default class ValidationError extends Error {
+  constructor(message, errElem) {
+    super(message);
+    this.name = this.constructor.name;
+    this.errElem = errElem;
+  }
+}
