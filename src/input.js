cd4f206fcc6a8df73dd0f2cbf1a1f7211d72814b
minkyung <mnk918p@gmail.com>
2020-12-04 16:31:11 +0900

    feat: 유저가 입력한 자동차 이름들을 받는 기능 구현 input.js 파일을 만들어 유저 입력에 대한 기능 모듈화

---
+++ src/input.js
@@ -0,0 +1,27 @@
+const RETURN_MESSAGES = {
+  ERROR_NAME_LENGTH_EXCEEDED: "자동차 이름은 5자 이하로 입력해주세요!",
+  ERROR_NAME_INPUT_NONE: "이름을 입력해 주세요!"
+};
+
+export default function carName() {
+  const carNames = document.getElementById("car-names-input").value;
+
+  return carNames;
+}
+
+export function isValidCarName(carNames) {
+  const carNameArray = carNames.split(",");
+  let isValid = false;
+
+  for (let i = 0; i < carNameArray.length; i++) {
+    if (carNameArray[i].length > 5) {
+      alert(RETURN_MESSAGES.ERROR_NAME_LENGTH_EXCEEDED);
+    } else if (carNameArray[i].length === 0) {
+      alert(RETURN_MESSAGES.ERROR_NAME_INPUT_NONE);
+    } else {
+      isValid = true;
+
+      return isValid;
+    }
+  }
+}
