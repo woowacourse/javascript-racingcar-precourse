16edc62456ff018b2d665ee557202b74cef1abe1
2SOOY <1401660@naver.com>
2020-12-04 21:58:46 +0900

    Feat - RacingCountInputContainer 컴포넌트 구현
    - CarNamesInputContainer와 같은 구조
    - 입력받은 숫자에 대해 양수 판별만 진행하면 된다.

--- src/Components/RacingCountInputContainer.js
+++ src/Components/RacingCountInputContainer.js
@@ -1,13 +1,36 @@
+import { ID, MESSAGE } from "../utils/constants.js";
+import { blockContainer, showInputError } from "../utils/domUtil.js";
+import { isEmptyInput, isPositiveNumber } from "../utils/validations.js";
+
 class RacingCountInputContainer {
   constructor({ $target, setRound }) {
     this.$target = $target;
+    this.$input = this.$target.querySelector(`#${ID.RACING_COUNT_INPUT}`);
 
     this.setRound = setRound;
 
     this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$target.addEventListener("click", this.onSubmit.bind(this));
+  }
+
+  onSubmit({ target }) {
+    if (target.id !== ID.RACING_COUNT_SUBMIT_BUTTON) return;
+
+    const round = this.$input.value.trim();
+    if (isEmptyInput(round)) {
+      showInputError(this.$input, MESSAGE.EMPTY_INPUT_ERROR);
+    }
+
+    if (!isPositiveNumber(round)) {
+      showInputError(this.$input, MESSAGE.NEGATIVE_NUMBER_ERROR);
+    }
+
+    this.setRound(parseInt(round));
+    blockContainer(this.$input, target);
+  }
 }
 
 export default RacingCountInputContainer;
