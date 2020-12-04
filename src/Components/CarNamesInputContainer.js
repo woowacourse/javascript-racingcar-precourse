8f58b6f1b0ced2b53dffca232b08089f53db3d0b
2SOOY <1401660@naver.com>
2020-12-04 18:32:58 +0900

    Feat - 이름 입력에 대한 이벤트 로직 처리 변경
    - 빈 입력에 대한 처리 추가
    - 잘못된 입력이 왔을 때 적절한 메시지를 보여주는 로직 추가

--- src/Components/CarNamesInputContainer.js
+++ src/Components/CarNamesInputContainer.js
@@ -1,5 +1,6 @@
 import { getCarsNameList } from "../utils/inputUtil.js";
-import { isValidCarNames } from "../utils/validations.js";
+import { showInputError } from "../utils/domUtil.js";
+import { isEmptyInput, isValidCarNames } from "../utils/validations.js";
 import { ID, MESSAGE } from "../utils/constants.js";
 
 class CarNamesInputContainer {
@@ -19,12 +20,17 @@ class CarNamesInputContainer {
   onSubmit({ target }) {
     if (target.id !== ID.CAR_NAMES_SUBMIT_BUTTON) return;
 
-    const names = getCarsNameList(this.$input.value);
-    if (!isValidCarNames(names)) {
-      alert(MESSAGE.NAME_INPUT_ERROR);
-      this.$input.value = "";
-      return;
+    const inputData = this.$input.value.trim();
+    if (isEmptyInput(inputData)) {
+      showInputError(this.$input, MESSAGE.EMPTY_INPUT_ERROR);
     }
+
+    const names = getCarsNameList(inputData);
+    if (!isValidCarNames(names)) {
+      showInputError(this.$input, MESSAGE.NAME_INPUT_ERROR);
+    }
+
+    this.setPlayers(names);
   }
 }
 
