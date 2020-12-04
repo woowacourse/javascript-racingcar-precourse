555241b0cd57e1a00fa48d2ae000fda040f95aca
dande <7bdande@gmail.com>
2020-12-04 12:28:25 +0900

    feat: 자동차 이름의 유효성에 따라 게임을 진행하는 메서드 추가
    - 재사용성을 위해 getCarNames 메서드 분리
    - 자동차 이름이 유효하면 racingCount를 입력받을 form을 보여줌
    - 유효하지 않으면 racingCountForm을 숨기고 alert 메시지 출력

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+const carNamesForm = document.querySelector("#car-names-form");
+const racingCountForm = document.querySelector("#racing-count-form");
+
+function getCarNames() {
+  const input = document.querySelector("#car-names-input");
+  const carNames = input.value.split(",");
+  const carNamesWithoutSpace = carNames.map(name => name.trim());
+
+  return carNamesWithoutSpace;
+}
+
 function isValidCarNames(carNames) {
   const MAX_CAR_NAME_LENGTH = 5;
   const isLValidCarNameLength = carNames.every(
@@ -12,4 +23,19 @@ function isValidCarNames(carNames) {
   return false;
 }
 
+function submitCarNames(e) {
+  e.preventDefault();
+  const carNames = getCarNames();
+  const isValidInputValue = isValidCarNames(carNames);
 
+  if (isValidInputValue) {
+    racingCountForm.classList.add("active");
+    return;
+  }
+
+  racingCountForm.classList.remove("active");
+  alert("자동차 이름을 5자 이하로 콤마로 구분하여 입력해주세요.");
+}
+
+
+carNamesForm.addEventListener("submit", submitCarNames);
