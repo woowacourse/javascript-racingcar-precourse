ab9728f922590894397dffc1adeb14417a1be185
임광열 <mkitigy@gmail.com>
2020-12-04 14:45:41 +0900

    feat: add car class

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 export const MIN_ELEM_COUNTS = 2;
 export const MIN_ELEM_LENGTH = 1;
 export const MAX_ELEM_LENGTH = 5;
+export const NAME_ALERT_MESSAGE = `유효하지 않은 값입니다.\n- 이름은 최소 두 개 이상이어야 합니다.\n- 이름은 1 - 5 글자로 이루어져야 합니다.\n- 이름은 영문과 숫자의 조합만 가능합니다.\n- 쉼표는 이름과 이름 사이에만 들어갑니다.`;
+export const DEFAULT_POSITION = 0;
