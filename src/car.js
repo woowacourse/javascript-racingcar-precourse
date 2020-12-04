608ff895e915ca16f132b87e2f4d30c77ba560a7
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-04 13:33:35 +0900

    fix(car.js) : #추가로 외부참조 금지

--- src/car.js
+++ src/car.js
@@ -1,17 +1,19 @@
 export class Car {
+    #STOP_LIMIT_NUMBER
+    #NUMBER_BOUNDARY
     constructor(name) {
-        this.STOP_LIMIT_NUMBER = 3;
-        this.NUMBER_BOUNDARY = 9;
+        this.#STOP_LIMIT_NUMBER = 3;
+        this.#NUMBER_BOUNDARY = 9;
         this.name = name;
         this.position = "";
     }
 
     makeRandomNumber() {
-        return parseInt(Math.random() * this.NUMBER_BOUNDARY);
+        return parseInt(Math.random() * this.#NUMBER_BOUNDARY);
     }
 
     makeOneInningGame() {
-        if (this.makeRandomNumber() > this.STOP_LIMIT_NUMBER) {
+        if (this.makeRandomNumber() > this.#STOP_LIMIT_NUMBER) {
             return this.position += "-";
         }
     }
