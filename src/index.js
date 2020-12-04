bf814b93660603501ffa25cbdd6583a4c6567908
Hyun Cheol <mlch1603@naver.com>
2020-12-04 17:21:12 +0900

    feat: 자동차 이름 입력값 검사하기 - 빈 문자열인지 검사 - 적절하지 않은 값이면 alert 메시지

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
+const carNameContainer = document.querySelector('.car-name');
+// const tryNumberContainer = document.querySelector(".try-number");
+const carNameInput = carNameContainer.querySelector("input[type='text']");
+const carNameSubmitBtn = carNameContainer.querySelector('button');
+// const tryNumberInput = tryNumberContainer.querySelector("input[type='number']");
+// const tryNumberSubmitBtn = tryNumberContainer.querySelector("button");
+
 export default class RacingCarGame {
-  constructor() {}
+  constructor() {
+    carNameInput.focus();
+    this.setEventListeners();
+  }
+
+  setEventListeners() {
+    this.handleCarNameInput = this.handleCarNameInput.bind(this);
+    carNameSubmitBtn.addEventListener('click', this.handleCarNameInput);
+  }
+
+  handleCarNameInput() {
+    const carNameString = carNameInput.value;
+    if (this.isEmptyString(carNameString)) {
+      return;
+    }
+  }
+
+  // 이름이 각각 5자 이상인지, 빈 문자열인 이름을 포함하고 있는지는 배열로 변환한 이후에 검사
+
+  isEmptyString(value) {
+    let result = false;
+    if (!value) {
+      alert('값을 입력해주세요.');
+      carNameInput.focus();
+      result = true;
+    }
+
+    return result;
+  }
 }
 
 new RacingCarGame();
