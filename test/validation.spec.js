0db5e33e9353a9cb0b73902e82822acf22f374b7
jho2301 <jho2301@gmail.com>
2020-12-04 20:48:30 +0900

    test: 자동차 이름 입력 검증 작동 정상화

--- test/validation.spec.js
+++ test/validation.spec.js
@@ -1,22 +1,22 @@
-import CarGameContainer from '../src/components/car-game-container.js';
+import CarNamesInput from '../src/components/car-names-input.js';
 
 describe('자동차 이름 입력 검증', () => {
   let carGameContainer;
   const $target = document.createElement('div');
 
   beforeEach(() => {
-    carGameContainer = new CarGameContainer($target);
+    carGameContainer = new CarNamesInput($target);
   });
 
   test('입력 자체가 빈 문자열이면 안된다', () => {
-    expect(carGameContainer.isValidateInput('')).toBe(false);
+    expect(carGameContainer.isValidInput('')).toBe(false);
   });
 
   test('이름이 5자를 넘으면 안된다', () => {
-    expect(carGameContainer.isValidateInput('123456, 123, 123')).toBe(false);
+    expect(carGameContainer.isValidInput('123456, 123, 123')).toBe(false);
   });
 
   test('이름이 빈 문자열이면 안된다', () => {
-    expect(carGameContainer.isValidateInput('1,,2')).toBe(false);
+    expect(carGameContainer.isValidInput('1,,2')).toBe(false);
   });
 });
