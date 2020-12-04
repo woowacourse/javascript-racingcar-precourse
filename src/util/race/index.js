b4357f96d0516b3afa51bb7bee9dd18fe7824e26
ehddnr301 <dy950328@gmail.com>
2020-12-04 15:51:59 +0900

    add calculateEachRace

--- src/util/race/index.js
+++ src/util/race/index.js
@@ -11,11 +11,31 @@ const generateRandomNumber = () => Math.floor(Math.random() * 10);
 
 const moveOrStop = (number) => (number >= 4 ? true : false);
 
+const calculateEachRace = (cars) => {
+  const eachResult = cars.map((car) => {
+    const randomNum = generateRandomNumber();
+    const canMove = moveOrStop(randomNum);
+
+    if (canMove) {
+      car.moveDistance += 1;
+    }
+
+    return car.moveDistance;
+  });
+
+  return eachResult;
+};
+
 export const raceStart = () => {
   const nameInput = document.getElementById("car-names-input");
   const countInput = document.getElementById("racing-count-input");
   const namesArray = splitByComma(nameInput.value);
   const count = parseInt(countInput.value);
 
-  const racer = initialState(namesArray);
+  const cars = initialState(namesArray);
+  let eachRaceResult = [];
+
+  for (let i = 0; i < count; i++) {
+    eachRaceResult[i] = calculateEachRace(cars);
+  }
 };
