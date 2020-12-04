3f1863cd9678b8f910903ede44677bfd0ef620ae
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 15:31:56 +0900

    feat: 이름 길이 확인 기능 구현

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ export default function RacingCarGame() {
 		return specialCharacter.test(carNamesInput);
 	}
 
+	function isAvailableLength(carNamesInput) {
+		const splitCarNamesInput = carNamesInput.split(",");
+		const availableLength = 5;
+		const arrayLength = splitCarNamesInput.length
+		let i = 0;
+
+		for (i; i < arrayLength; i++) {
+			if (splitCarNamesInput[i].length > availableLength) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	function getCarNamesInput() {
 		const carNamesSubmit = document.querySelector("#car-names-submit");
 		carNamesSubmit.addEventListener("click", () => {
 			const carNamesInput = document.querySelector("#car-names-input").value;
 
-			if (isSpecialCharacter(carNamesInput) === false) {
+			if (isSpecialCharacter(carNamesInput) === false && isAvailableLength(carNamesInput) === true) {
 				return carNamesInput;
 			} else {
 				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
