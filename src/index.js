57411ebac03a23a4688f086b29ae04c7c6f90f73
lulu-kang <dalkang.hy@gmail.com>
2020-12-04 16:45:36 +0900

    refactor: indent 오류 수정

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 export default function RacingCarGame() {	
-	function isSpecialCharacter(carNamesInput) {
-		const specialCharacter = /[^가-힣ㄱ-하-ㅣa-zA-z0-9,]/;
-		return specialCharacter.test(carNamesInput);
-	}
+  function isSpecialCharacter(carNamesInput) {
+    const specialCharacter = /[^가-힣ㄱ-하-ㅣa-zA-z0-9,]/;
+    return specialCharacter.test(carNamesInput);
+  }
 
-	function isAvailableLength(carNamesInput) {
-		const splitCarNamesInput = carNamesInput.split(",");
-		const availableLength = 5;
-		const arrayLength = splitCarNamesInput.length
-		let i = 0;
+  function isAvailableLength(carNamesInput) {
+    const splitCarNamesInput = carNamesInput.split(",");
+    const availableLength = 5;
+    const arrayLength = splitCarNamesInput.length;
+    let i = 0;
 
-		for (i; i < arrayLength; i++) {
-			if (splitCarNamesInput[i].length > availableLength) {
-				return false;
-			}
-		}
+    for (i; i < arrayLength; i++) {
+      if (splitCarNamesInput[i].length > availableLength) {
+        return false;
+      }
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	function getCarNamesInput() {
-		const carNamesSubmit = document.querySelector("#car-names-submit");
-		carNamesSubmit.addEventListener("click", () => {
-			const carNamesInput = document.querySelector("#car-names-input").value;
+  function getCarNamesInput() {
+    const carNamesSubmit = document.querySelector("#car-names-submit");
+    carNamesSubmit.addEventListener("click", () => {
+      const carNamesInput = document.querySelector("#car-names-input").value;
+      
+      if (isSpecialCharacter(carNamesInput) === false && isAvailableLength(carNamesInput) === true) {
+        return carNamesInput;
+      } else {
+        alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
+      }			
+    }); 
+  }
 
-			if (isSpecialCharacter(carNamesInput) === false && isAvailableLength(carNamesInput) === true) {
-				return carNamesInput;
-			} else {
-				alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
-			}			
-		}); 
-	}
+  function init() {
+    getCarNamesInput();
+  }
 
-	function init() {
-		getCarNamesInput();
-	}
-
-	init();
+  init();
 }
 
 new RacingCarGame();
