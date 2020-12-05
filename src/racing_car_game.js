19d3d7107f94b329d2aa9e7dd1ac4c1e3d3e02dc
Tanney-102 <dhxodms89@gmail.com>
2020-12-05 17:38:34 +0900

    Implement RacingCarGame class

---
+++ src/racing_car_game.js
@@ -0,0 +1,21 @@
+import Car from "./car.js";
+
+export default class RacingCarGame {
+  constructor() {
+    this.cars = [];
+  }
+
+  // names : Array of stings
+  setRacingCars(names) {
+    names.forEach((_name) => {
+      this.cars.push(new Car(_name));
+    });
+  }
+
+  play() {
+    return this.cars.map((_car) => {
+      _car.moveForwordByRandom();
+      return _car.movedDistance;
+    });
+  }
+}
