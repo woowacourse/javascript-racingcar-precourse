28a9ea25270cdd1c9eacaf27ec4d0abab6a8da38
HyuuunjuKim <gus7wn@gmail.com>
2020-12-05 17:43:19 +0900

    :art: car-names 유효성 검사 함수 구현 후 분리

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ export default function RacingCarGame() {}
 function Car(name) {
   this.name = name;
 }
-
+console.log("east, west, south, north");
 const btnCarNames = document.getElementById("car-names-submit");
 const inputCarNames = document.getElementById("car-names-input");
+
+function isCorrectCarNames(carName) {
+  if (carName.length > 0 && carName.length <= 5) {
+    return true;
+  }
+
+  return false;
+}
+
 btnCarNames.onclick = function () {
-  console.log(inputCarNames.value);
-  let carNames = inputCarNames.value.split(",");
-  console.log(carNames);
+  const carNames = inputCarNames.value.split(",");
+  const correctCarNames = carNames.map(function (carName) {
+    carName = carName.trim();
+    if (!isCorrectCarNames(carName)) {
+      alert("모든 자동차의 이름을 5자 이하로 입력해주세요.");
+      inputCarNames.value = "";
+
+      return;
+    }
+
+    return carName;
+  });
+  console.log(correctCarNames);
 };
 new RacingCarGame();
