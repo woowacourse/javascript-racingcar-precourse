56fd8a43399d0a108912bdd672d8526a953b2a8e
Kwakcena <rhkrgudwh@gmail.com>
2020-12-05 18:38:51 +0900

    feat: Implement the initial game screen
    RacingCarGame 클래스를 작성하여 초기 게임 화면을 구현합니다.

--- README.md
+++ README.md
@@ -68,7 +68,7 @@ class Car {
 ## 구현 목록
 
 - [X] eslint 설치 및 rule 설정
-- [] RacingCarGame 클래스 모듈에서 게임 화면을 화면에 보여준다.
+- [X] RacingCarGame 클래스 모듈에서 게임 화면을 화면에 보여준다.
 - [] racing-count-input과 결과 화면이 화면에서 보이지 않도록 한다.
 - [] car-game-container를 구현한다.
   - [] car-names-input을 화면에 보여주는 클래스 모듈을 작성한다.
---
+++ src/RacingCarGame.js
@@ -0,0 +1,51 @@
+export default class RacingCarGame {
+  constructor(target) {
+    this.target = target;
+    this.createHeader(target);
+    this.createDescription(target);
+    this.createCarGameContainer(target);
+    this.createResult(target);
+  }
+
+  createHeader(target) {
+    const _header = document.createElement('h1');
+    _header.innerHTML = `🏎️ 자동차 경주 게임`;
+    target.appendChild(_header);
+  }
+
+  createDescription(target) {
+    const _description = document.createElement('p');
+    _description.innerHTML = `
+      자동차 이름을 <strong>5자 이하로</strong> 콤마로 구분하여 입력해주세요.
+      <br />
+      올바른 예) east,west,south,north <br />
+    `;
+    target.appendChild(_description);
+  }
+
+  createCarGameContainer(target) {
+    const _container = document.createElement('div');
+    _container.className = 'car-game-container';
+
+    _container.innerHTML = `
+      <div>
+        <input type="text" id="car-names-input" />
+        <button id="car-names-submit">확인</button>
+      </div>
+      <div>
+        <h4>시도할 횟수를 입력해주세요.</h4>
+        <input type="number" id="racing-count-input" />
+        <button id="racing-count-submit">확인</button>
+      </div>
+    `;
+
+    target.appendChild(_container);
+  }
+
+  createResult(target) {
+    const _result = document.createElement('div');
+    _result.innerHTML = `<h4>📄 실행 결과</h4>`;
+
+    target.appendChild(_result);
+  }
+}
