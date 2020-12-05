14004375c8100352ad3e2dcf6c8299448fb7597a
ddongule <mnk918p@gmail.com>
2020-12-06 02:44:04 +0900

    feat: 유저가 입력한 이동 횟수를 받아 화면에 구성하는 코드 구현
    result container를 만들어 시도할 횟수가 valid하면 보여줌

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { clearInput } from "./utils/input-utils.js";
-import isValidCarName from "./input-validator.js";
+import isValidCarName, { isValidRacingCount } from "./input-validator.js";
 import show, { disableBtn } from "./utils/display-utils.js";
 import Car from "./domain/car.js";
 
 const carNameBtn = document.getElementById("car-names-submit");
 const carNameInput = document.getElementById("car-names-input");
 
+const racingCountBtn = document.getElementById("racing-count-submit");
+const racingCountInput = document.getElementById("racing-count-input");
 const racingCarContainer = document.getElementById("racing-car");
+
+const resultContainer = document.getElementById("result");
 
 export default function RacingCarGame() {
   carNameBtn.addEventListener("click", () => {
@@ -21,10 +25,19 @@ export default function RacingCarGame() {
       for (let i = 0; i < carNameArray.length; i++) {
         carArray.push(new Car(carNameArray[i]));
       }
-
-      console.log(carArray);
     } else {
       clearInput(carNameInput);
+    }
+  });
+
+  racingCountBtn.addEventListener("click", () => {
+    const racingCount = racingCountInput.value;
+
+    if (isValidRacingCount(racingCount)) {
+      show(resultContainer);
+      disableBtn(racingCountBtn);
+    } else {
+      clearInput(racingCountInput);
     }
   });
 }
