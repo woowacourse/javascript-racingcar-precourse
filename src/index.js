9baf329821b46aab80bb2236ad95743e53cbf9d5
joyfuI <jong970105@gmail.com>
2020-12-05 22:46:55 +0900

    자동차 이름을 파싱하는 메소드 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-export default function RacingCarGame() {
+export default class RacingCarGame {
+  // 입력받은 자동차 이름을 배열로 반환하는 메소드
+  inputCarName(carInputString) {
+    const carArr = carInputString.split(',');
+    for (const car of carArr) {
+      if (car.length > 5) {
+        return null;
+      }
+    }
+    return carArr;
+  }
 }
-// export default class RacingCarGame {
-// }
 
 
 new RacingCarGame();
