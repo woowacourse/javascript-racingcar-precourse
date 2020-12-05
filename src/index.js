14222754337fe46a20fb8e0d3fae52d78d1afcc5
ginger-kang <kdhoon07@gmail.com>
2020-12-06 05:33:23 +0900

    feat: get cars name and racing count from user input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+  const carNamesSubmitBtn = document.querySelector("#car-names-submit");
+  const carNamesInput = document.querySelector("#car-names-input");
+  const racingCountInput = document.querySelector("#racing-count-input");
+  const racingCountSubmitBtn = document.querySelector("#racing-count-submit");
 
+  const gameStart = () => {
+    console.log("game start!");
+  };
+
+  const setRaceRound = () => {
+    console.log("set round");
+  };
+
+  const init = () => {
+    carNamesSubmitBtn.addEventListener("click", () => gameStart());
+    racingCountSubmitBtn.addEventListener("click", () => setRaceRound());
+    carNamesInput.addEventListener("keyup", event => {
+      if (event.keyCode === 13) {
+        gameStart();
+      }
+    });
+    racingCountInput.addEventListener("keyup", event => {
+      if (event.keyCode === 13) {
+        setRaceRound();
+      }
+    });
+  };
+
+  init();
+}
 
 new RacingCarGame();
