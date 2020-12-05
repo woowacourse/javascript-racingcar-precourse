a615c491150ea5244d2af402abfe818654ff82de
2SOOY <1401660@naver.com>
2020-12-05 18:13:37 +0900

    Feat - 경기 진행 상황 및 승자에 대한 렌더링 메소드 : createResultHTML, createWinnerHTML
    - RacingResultContainer 컴포넌트에서 렌더링과 관련한 메소드 설계
    - 상위 컴포넌트의 상태를 조회하는 메소드를 바탕으로 html 템플릿 적용

--- src/Components/RacingResultContainer.js
+++ src/Components/RacingResultContainer.js
@@ -1,12 +1,34 @@
+import { resultTitleHTML, playerHTML, winnerHTML } from "../utils/templates.js";
+
 class RacingResultContainer {
-  constructor({ $target, getPlayers }) {
+  constructor({ $target, getPlayers, getWinnerNameList }) {
     this.$target = $target;
     this.getPlayers = getPlayers;
+    this.getWinnerNameList = getWinnerNameList;
 
-    this.resultHTML = "";
+    this.resultHTML = resultTitleHTML();
   }
 
-  render() {}
+  createResultHTML() {
+    const tempPlayers = this.getPlayers();
+
+    const roundHTML =
+      tempPlayers.reduce((acc, cur) => {
+        acc += playerHTML(cur.name, cur.step);
+        return acc;
+      }, "<div>") + "</div><br/>";
+
+    this.resultHTML += roundHTML;
+  }
+
+  createWinnerHTML() {
+    const winners = this.getWinnerNameList();
+    this.resultHTML += winnerHTML(winners);
+  }
+
+  render() {
+    this.$target.innerHTML = this.resultHTML;
+  }
 }
 
 export default RacingResultContainer;
