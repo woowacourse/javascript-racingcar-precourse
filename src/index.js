df9f0561f821df2f2bf0bd61b1a8075a7bbe6561
ddongule <mnk918p@gmail.com>
2020-12-06 02:26:23 +0900

    feat: 유저의 입력을 받아 Car 객체를 생성

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-class Car {
-  constructor(name) {
-    this.name = name;
-    this.position = 0;
-  }
+import { clearInput } from "./utils/input-utils.js";
+import isValidCarName from "./input-validator.js";
+import show, { disableBtn } from "./utils/display-utils.js";
+import Car from "./domain/car.js";
+
+const carNameBtn = document.getElementById("car-names-submit");
+const carNameInput = document.getElementById("car-names-input");
+
+const racingCarContainer = document.getElementById("racing-car");
+
+export default function RacingCarGame() {
+  carNameBtn.addEventListener("click", () => {
+    const carName = carNameInput.value;
+    const carNameArray = carName.split(",");
+
+    if (isValidCarName(carName)) {
+      let carArray = [];
+      show(racingCarContainer);
+      disableBtn(carNameBtn);
+
+      for (let i = 0; i < carNameArray.length; i++) {
+        carArray.push(new Car(carNameArray[i]));
+      }
+
+      console.log(carArray);
+    } else {
+      clearInput(carNameInput);
+    }
+  });
 }
+
+new RacingCarGame();
