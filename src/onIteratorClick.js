e1f3a6397ba2e16d5d03fbbdebf55f84f012f288
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-05 21:59:41 +0900

    done 'onIterator.js'

--- src/onIteratorClick.js
+++ src/onIteratorClick.js
@@ -1,5 +1,25 @@
-import { iteratorInput } from "./utils.js";
+import {
+  checkIteratorValidity,
+  iterateBy,
+  iterateByCarArrayLength,
+  iteratorInput,
+  renderCurrentState,
+  renderWinner,
+} from "./utils.js";
 
-const onIteratorClick = function () {};
+const onIteratorClick = function () {
+  const checkedIterator = checkIteratorValidity(iteratorInput.value);
+  if (!checkedIterator.value) {
+    alert(checkedIterator.errorMessage);
+    return;
+  }
+  document.getElementById("result-title").style.display = "block";
+  iterateBy(
+    parseInt(iteratorInput.value, 10),
+    iterateByCarArrayLength.bind(this),
+    renderCurrentState.bind(this)
+  );
+  renderWinner(this.carArray);
+};
 
 export default onIteratorClick;
