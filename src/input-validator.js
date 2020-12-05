f43e704d2fe815777b6c4a8ece1ac6bfc5e3fd0b
ddongule <mnk918p@gmail.com>
2020-12-06 02:48:47 +0900

    chore: return message의 위치 수정
    input-utils.js 에서 input-validator.js로 수정

--- src/input-validator.js
+++ src/input-validator.js
@@ -1,4 +1,10 @@
-export function isValidCarName(carNames) {
+const RETURN_MESSAGES = {
+  ERROR_NAME_LENGTH_EXCEEDED: "자동차 이름은 5자 이하로 입력해주세요!",
+  ERROR_NAME_INPUT_NONE: "이름을 입력해 주세요!",
+  ERROR_RACING_COUNT_NEGATIVE: "1 이상의 숫자를 넣어주세요!",
+};
+
+export default function isValidCarName(carNames) {
   const carNameArray = carNames.split(",");
   let isValid = false;
 
