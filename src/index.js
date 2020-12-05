8e85a693234ac6ad11b268316cf6667e0e6dc018
yang <yaaaaang57@naver.com>
2020-12-05 23:16:35 +0900

    input and submit car name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export default function RacingCarGame() {}
+export default function RacingCarGame() {
+  const carNameInput = document.querySelector('#car-names-input');
+  const carNameSubmitBtn = document.querySelector('#car-names-submit');
+
+  carNameSubmitBtn.addEventListener('click', () => console.log(carNameInput.value));
+  carNameInput.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+      console.log(carNameInput.value);
+    }
+  });
+}
 
 new RacingCarGame();
