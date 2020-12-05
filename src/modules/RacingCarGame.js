f5c4b850a7fae1ad362aa3cdd17e35f721db9e9c
dande <7bdande@gmail.com>
2020-12-05 17:51:41 +0900

    feat: 자동차 인스턴스 생성
    - racingCars를 객체에서 배열로 변경

--- src/modules/RacingCarGame.js
+++ src/modules/RacingCarGame.js
@@ -4,6 +4,17 @@ export default class RacingCarGame {
   constructor(carNames, racingCount) {
     this.carNames = carNames;
     this.racingCount = racingCount;
-    this.racingCars = {};
+    this.racingCars = [];
   }
+
+  setRacingCars = () => {
+    this.carNames.forEach(carName => {
+      const car = new Car(carName);
+      this.racingCars.push(car);
+    });
+  };
+
+  play = () => {
+    this.setRacingCars();
+  };
 }
