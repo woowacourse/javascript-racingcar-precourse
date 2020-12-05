ca77bfc47234909d732548d68bad6c7140023112
HyuuunjuKim <gus7wn@gmail.com>
2020-12-05 23:26:25 +0900

    :zap: racingCount만큼 게임 진행 구현

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 export default function RacingCarGame(racingCount, cars) {
-  console.log(racingCount, cars);
+  this.play = function () {
+    let randomNums = [];
+    for (let i = 0; i < racingCount; i++) {
+      randomNums.push(Math.floor(Math.random() * 10));
+    }
+    randomNums.forEach((randomNum, idx) => {
+      if (randomNum > 3) {
+        cars[idx].position++;
+      }
+    });
+  };
 }
 
 function Car(name) {
   this.name = name;
+  this.position = 0;
 }
 let cars = [];
 console.log("east, west, south, north");
@@ -43,9 +54,17 @@ btnCarNames.onclick = function () {
 
 const btnRacingCount = document.getElementById("racing-count-submit");
 const inputRacingCount = document.getElementById("racing-count-input");
-const racingCount = -1;
-btnRacingCount.onclick = function () {
+let racingCount = -1;
+btnRacingCount.onclick = function (event) {
   racingCount = Number(inputRacingCount.value);
-  console.log(racingCount);
+
+  const resultBox = document.getElementById("result-box");
+  resultBox.style.display = "block";
+  const r = new RacingCarGame(racingCount, cars);
+  for (let i = 0; i < racingCount; i++) {
+    r.play();
+    for (let s in cars) {
+      console.log(cars[s].position);
+    }
+  }
 };
-new RacingCarGame(racingCount, cars);
