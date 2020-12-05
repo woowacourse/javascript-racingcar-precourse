e5273d96af1e4bac211fe656e09a7f40947f4924
Tanney-102 <dhxodms89@gmail.com>
2020-12-05 18:39:26 +0900

    Add racing count Form

--- src/index.js
+++ src/index.js
@@ -25,6 +25,17 @@ const isValidNames = (namesArray) => {
   return result;
 };
 
+const addRacingCountForm = (e) => {
+  const racingCountForm = document.createElement("div");
+
+  racingCountForm.innerHTML = `
+  <h4>시도할 횟수를 입력해주세요.</h4>
+  <input id="racing-count-input" type="number" />
+  <button id="racing-count-submit">확인</button>
+  `;
+  e.target.closest(".car-game-container").append(racingCountForm);
+};
+
 const passNamesInputToGameController = (e) => {
   const nameInputStr = e.target.parentNode.children[0].value;
   const namesArray = nameInputStr.split(",").map((_name) => _name.trim());
@@ -32,8 +43,8 @@ const passNamesInputToGameController = (e) => {
   if (!isValidNames(namesArray) || isNamesArrayEmpty(namesArray)) {
     return;
   }
-
   gameController.setRacingCars(namesArray);
+  addRacingCountForm(e);
 };
 
 const routeDocClickEvent = (e) => {
