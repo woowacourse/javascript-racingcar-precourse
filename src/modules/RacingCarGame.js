20e87dcafea3c1c0811bd7d77a47b56654900d42
dande <7bdande@gmail.com>
2020-12-05 17:54:59 +0900

    feat: 전진 조건에 따라 step을 업데이트하는 메서드 추가
    - random 숫자를 구하는 메서드 모듈로 분리
    - 기준 숫자 이상일 경우에만 step을 1씩 더함

--- src/modules/RacingCarGame.js
+++ src/modules/RacingCarGame.js
@@ -1,10 +1,14 @@
 import Car from "./Car.js";
+import { getRandomNumber } from "./Utils.js";
 
 export default class RacingCarGame {
   constructor(carNames, racingCount) {
     this.carNames = carNames;
     this.racingCount = racingCount;
     this.racingCars = [];
+    this.MIN_RANDOM_NUMBER = 0;
+    this.MAX_RANDOM_NUMBER = 9;
+    this.ADD_STEP_STANDARD = 4;
   }
 
   setRacingCars = () => {
@@ -12,6 +16,17 @@ export default class RacingCarGame {
       const car = new Car(carName);
       this.racingCars.push(car);
     });
+  };
+
+  updateCarStep = car => {
+    const randomNumber = getRandomNumber(
+      this.MIN_RANDOM_NUMBER,
+      this.MAX_RANDOM_NUMBER
+    );
+
+    if (randomNumber >= this.ADD_STEP_STANDARD) {
+      car.step++;
+    }
   };
 
   play = () => {
---
+++ src/modules/Utils.js
@@ -0,0 +1,3 @@
+export const getRandomNumber = (minNumber, maxNumber) => {
+  return Math.floor(Math.random() * maxNumber) + minNumber;
+};
