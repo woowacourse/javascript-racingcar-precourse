b84e4481ec2a9de2a7e1bd5f843925a5359a7280
joyfuI <jong970105@gmail.com>
2020-12-05 23:38:46 +0900

    RacingCarGame 클래스 생성자 추가

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import Car from './car';
 
 export default class RacingCarGame {
-  // 입력받은 자동차 이름을 배열로 반환하는 메소드
-  inputCarName(carInputString) {
+  constructor(cars, count) {
+    this.cars = cars;
+    this.count = count;
+  }
+
+  // 입력받은 자동차 이름을 Car 배열로 반환하는 메소드
+  static inputCarName(carInputString) {
     const carArr = carInputString.split(',');
     for (const car of carArr) {
       if (car.length > 5) {
         return null;
       }
     }
+    carArr.map(car => new Car(car));
     return carArr;
   }
 }
-
-
-new RacingCarGame();
