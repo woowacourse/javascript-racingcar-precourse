15e9aeb3eb4ba1c833620b0e890b723428a4263a
Huijiny <jiin20803@gmail.com>
2020-12-05 11:26:10 +0900

    fix: '=' to ':' in constatns.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const CAR_GAME_GUIDE = {
-    CAR_NAME_ERROR = "자동차 이름을 5자 이하로 써주세요. 한 개라도 5자 이상일 경우 다음단계로 넘어가지 않습니다.",
-    ONLY_NUMBER = "숫자만 입력해주세요!"
-}
+  CAR_NAME_ERROR:
+    "자동차 이름을 5자 이하로 써주세요. 한 개라도 5자 이상일 경우 다음단계로 넘어가지 않습니다.",
+  ONLY_NUMBER: "숫자만 입력해주세요!",
+};
