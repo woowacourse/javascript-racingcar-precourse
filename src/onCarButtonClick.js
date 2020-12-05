2701d2e7c95eccf1e4414fd0c4bc1340e1edd78c
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-05 13:27:04 +0900

    done 'onCarButtonClick.js'

--- src/onCarButtonClick.js
+++ src/onCarButtonClick.js
@@ -1,5 +1,18 @@
-import { carInput } from "./utils.js";
+import {
+  carInput,
+  checkCarNameValidity,
+  showIteratorElements,
+  switchToCarInstance,
+} from "./utils.js";
 
-const onCarButtonClick = function () {};
+const onCarButtonClick = function () {
+  const checkedCarNames = checkCarNameValidity(carInput.value);
+  if (checkedCarNames.value) {
+    this.setCarArray(
+      checkedCarNames.value.map((name) => switchToCarInstance(name))
+    );
+    showIteratorElements();
+  } else alert(checkedCarNames.errorMessage);
+};
 
 export default onCarButtonClick;
