b1acd1d41b83f0dd0dc21b32e64e2bf3a2c5c17f
ginger-kang <kdhoon07@gmail.com>
2020-12-06 05:43:35 +0900

    feat: create a module to check if user input is valid

---
+++ src/util.js
@@ -0,0 +1,28 @@
+const isSplitedRest = names => names.split(",")[0] !== names;
+
+const hasLeastTwoCars = names => names.split(",").length >= 2;
+
+const isLessSixCharacters = names => {
+  const nameArray = names.split(",");
+  for (let i = 0; i < nameArray.length; i++) {
+    if (nameArray[i].length > 5) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const hasEmptyString = names => names.split(",").includes("");
+
+export const isValidInputNames = names => {
+  return (
+    isSplitedRest(names) &&
+    hasLeastTwoCars(names) &&
+    isLessSixCharacters(names) &&
+    !hasEmptyString(names)
+  );
+};
+
+export const isValidInputCount = count => {
+  return count >= 1;
+};
