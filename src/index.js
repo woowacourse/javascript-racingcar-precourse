57c906b653007c14dfa2b01988a8725912aa5641
seojihwan <seojihwan213@naver.com>
2020-12-05 22:43:39 +0900

    feat: 쉼표 기준으로 자동차 이름에 대한 배열 생성

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export default function RacingCarGame() {}
+export default function RacingCarGame() {
+  const carNamesInput = document.getElementById('car-names-input');
+  const carNamesSubmit = document.getElementById('car-names-submit');
+
+  const handleClickCarNamesSubmit = () => {
+    const carNamesList = carNamesInput.value.split(',');
+    console.log(carNamesList);
+  };
+  carNamesSubmit.addEventListener('click', handleClickCarNamesSubmit);
+}
 
 new RacingCarGame();
