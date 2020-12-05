969641e3725f60e8f69167135b17ce9df7608458
ddongule <mnk918p@gmail.com>
2020-12-05 16:07:54 +0900

    feat: display를 위한 display-utils.js 파일 생성, 기능 구현
    1) 시도할 횟수를 보여주는 기능
    2) 버튼을 비활성화하는 기능
    3) 결과를 보여주기위한 div 생성 기능

---
+++ src/display-utils.js
@@ -0,0 +1,11 @@
+export function show(elementToShow) {
+  elementToShow.style.display = "block";
+}
+
+export function disableBtn(buttonToDisable) {
+  buttonToDisable.disabled = true;
+}
+
+export function createResultDivToShow() {
+  const eachResult = document.createElement("div");
+}
