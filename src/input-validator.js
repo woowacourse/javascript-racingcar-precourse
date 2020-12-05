aec4cb1b12ec20e79749708461a75fae380815df
ddongule <mnk918p@gmail.com>
2020-12-05 15:25:03 +0900

    refactor: 유저가 입력한 input 내용의 유효성을 검사하는 기능을 파일로 분리
    유저의 입력을 관리하는 input.js 에서 input-utils.js로 네이밍 변경

--- src/input-validator.js
+++ src/input-validator.js
@@ -1,15 +1,3 @@
-const RETURN_MESSAGES = {
-  ERROR_NAME_LENGTH_EXCEEDED: "자동차 이름은 5자 이하로 입력해주세요!",
-  ERROR_NAME_INPUT_NONE: "이름을 입력해 주세요!",
-  ERROR_RACING_COUNT_NEGATIVE: "1 이상의 숫자를 넣어주세요!"
-};
-
-export function getUserInputValue(elementId) {
-  const inputValue = document.getElementById(elementId).value;
-
-  return inputValue;
-}
-
 export function isValidCarName(carNames) {
   const carNameArray = carNames.split(",");
   let isValid = false;
