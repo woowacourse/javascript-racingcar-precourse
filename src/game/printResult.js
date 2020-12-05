dc8180be012bf7112969634592ae8c0904919794
yujo <yujo@student.42seoul.kr>
2020-12-05 13:46:40 +0900

    feat: 매 경주마다 결과를 각 자동차 인스턴스에 저장하는 기능 구현

--- src/game/printResult.js
+++ src/game/printResult.js
@@ -1,3 +1,35 @@
-export default function printResult(carList, value) {
-  console.log(carList, value);
+import toggleResultScreen from '../init/toggleResultScreen.js';
+
+function getRandomNumber() {
+  const min = 0;
+  const max = 10;
+
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function race(car) {
+  if (getRandomNumber() > 3) {
+    car.count++;
+  }
+}
+
+function raceResultTemplate(car) {
+  return `${car.name}: ${car.count}<br>`;
+}
+
+function printRaceResult(car) {
+  const $resultScreen = document.querySelector('#app > div:last-child');
+
+  $resultScreen.insertAdjacentHTML('beforeend', raceResultTemplate(car));
+}
+
+export default function printResult(carList, racingCount) {
+  const $resultScreen = document.querySelector('#app > div:last-child');
+
+  toggleResultScreen();
+  while (racingCount--) {
+    carList.forEach((car) => race(car));
+    carList.forEach((car) => printRaceResult(car));
+    $resultScreen.insertAdjacentHTML('beforeend', '<br>');
+  }
 }
