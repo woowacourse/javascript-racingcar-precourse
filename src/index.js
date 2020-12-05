baa69f338f6e81e25b2754ec506fa260e6a2db78
seojihwan <seojihwan213@naver.com>
2020-12-05 23:00:37 +0900

    feat: 각 자동차 이름이 공백이 아닌 1~ 5글자 인지 확인

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ export default function RacingCarGame() {
   const handleClickCarNamesSubmit = () => {
     const carNamesList = carNamesInput.value.split(',');
     console.log(carNamesList);
+    console.log(isValidCarNames(carNamesList));
+  };
+
+  const isValidCarNames = carNames => {
+    for (const carName of carNames) {
+      if (carName.length > 5 || carName.trim().length === 0) {
+        return false;
+      }
+    }
+
+    return true;
   };
   carNamesSubmit.addEventListener('click', handleClickCarNamesSubmit);
 }
