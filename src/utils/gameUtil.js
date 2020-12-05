861a53cea5be409a48b98697e76e601e6ef5a428
2SOOY <1401660@naver.com>
2020-12-05 12:11:05 +0900

    Fix - getRandomNumber 함수 오타 수정

--- src/utils/gameUtil.js
+++ src/utils/gameUtil.js
@@ -1,5 +1,5 @@
 import { NUM } from "./constants.js";
 
-export const getRandomNumber = () => Matht.floor(Math.random() * NUM.RANGE);
+export const getRandomNumber = () => Math.floor(Math.random() * NUM.RANGE);
 
 export const getForwardSteps = num => (num <= NUM.BOUND ? NUM.STOP : NUM.GO);
