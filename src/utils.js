4efcd0efc6dd04dbb409652541dbf70bc9f0cba0
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-05 11:55:45 +0900

    add 'checkCarNameValidity'

--- src/utils.js
+++ src/utils.js
@@ -20,3 +20,14 @@ export const splitNamesByComma = (carNames) =>
     .join("")
     .split(",") // 컴마 기준으로 잘라서 배열 생성
     .filter((f) => f !== ""); // ,, 이렇게 컴마 사이에 값 없는 경우와 맨 앞, 맨 뒤에 컴마 있을 경우 처리
+
+export const checkCarNameValidity = (carNames) => {
+  if (carNames.length === 0)
+    return { value: false, errorMessage: "Write something and try again." };
+  const resultOfSpliting = splitNamesByComma(carNames);
+  if (JSON.stringify(resultOfSpliting) === "[]")
+    return { value: false, errorMessage: "Empty space isn't allowed." };
+  if (resultOfSpliting.length > 5)
+    return { value: false, errorMessage: "Car names should be less than 5" };
+  return { value: resultOfSpliting, errorMessage: "" };
+};
