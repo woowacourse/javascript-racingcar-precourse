26de0f7cc28b70aa2221a2654110a4055580982b
swimme <2sy100@likelion.org>
2020-12-07 05:10:28 +0900

    feat: 자동차 이름 입력받기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+export default class RacingCarGame {
+  constructor() {
+    this.carNameInputField = document.getElementById("car-names-input");
+    this.carNameSubmitButton = document.getElementById("car-names-submit");
 
+    this.racingCountInputField = document.getElementById("racing-count-input");
+    this.racingCountSubmitButton = document.getElementById("racing-count-submit");
+    this.racingCountContainer = document.getElementById("racing-count-container");
+
+    this.gameResultContainer = document.getElementById("game-result-container");
+  }
+
+  getCarNamesInput() {
+    const carNamesInput = this.carNameInputField.value;
+    return carNamesInput;
+  }
+}
 
 new RacingCarGame();
+// test
+//onsole.log(game.getCarNamesInput());
