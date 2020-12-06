025b137821b4a0aad80a29f3924cb572204a564e
swimme <2sy100@likelion.org>
2020-12-07 06:42:51 +0900

    feat: 자동차에 부여된 숫자에 따라 움직이게 하기

--- src/car.js
+++ src/car.js
@@ -2,4 +2,11 @@ export default class Car {
   constructor(name) {
     this.name = name;
   }
+
+  move(number) {
+    if (number >= 4) {
+      this.distance++;
+      this.position += "-";
+    }
+  }
 }
