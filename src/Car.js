737a7007c080cdf152d2b2f6c1dfdef2b085f305
Shim MunSeong <Puterism.K@gmail.com>
2020-12-06 18:07:44 +0900

    feat: 각 자동차의 전진 or 멈춤 여부를 결정하는 기능

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,15 @@
 class Car {
   constructor(name) {
     this.name = name;
+    this.forwardCount = 0;
+  }
+
+  moveForwardRandomly() {
+    const randomValue = Math.floor(Math.random() * 10);
+
+    if (randomValue >= 4) {
+      this.forwardCount += 1;
+    }
   }
 }
 
