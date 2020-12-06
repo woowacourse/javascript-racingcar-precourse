5211d2cda047723ac7d7454593cfb67327e4e938
Daniel <elrion018@gmail.com>
2020-12-06 23:09:10 +0900

    refactor(utils) : delete isHigherThan function

--- src/modules/models/Car/index.js
+++ src/modules/models/Car/index.js
@@ -4,20 +4,21 @@ export default class Car {
   constructor(name) {
     this.name = name;
     this.moveForwardDistance = 0;
-    this.MAXVAL = 9;
-    this.MINVAL = 0;
+    this.MAX_NUMBER = 9;
+    this.MIN_NUMBER = 0;
+    this.STANDARD_NUMBER = 3;
   }
 
   randomNumber() {
     const number = Math.floor(
-      Math.random() * (this.MAXVAL - this.MINVAL + 1) + this.MINVAL,
+      Math.random() * (this.MAX_NUMBER - this.MIN_NUMBER + 1) + this.MIN_NUMBER,
     );
 
     return number;
   }
 
   moveForward() {
-    if (isHigherThan(this.randomNumber(), 3)) {
+    if (this.randomNumber() > this.STANDARD_NUMBER) {
       this.moveForwardDistance++;
     }
   }
