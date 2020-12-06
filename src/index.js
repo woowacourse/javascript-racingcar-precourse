e767a8ac7e764083a4ca9fb11c94ebd48ee6224b
ket87 <ket8780@gmail.com>
2020-12-06 19:52:23 +0900

    자동차 입력값 검증 메서드 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
 // export default function RacingCarGame() {
 // }
 
-export default class RacingCarGame {}
+export default class RacingCarGame {
+  constructor() {
+    this.initContainer();
+    this.initEvent();
+    this.cars = [];
+  }
+
+  initContainer() {
+    this.racingCountContainer = document.getElementById("racing-count-container");
+    this.racingResultContainer = document.getElementById("racing-count-container");
+    this.carGameContainer = document.getElementById("car-game-container");
+    this.carNamesInput = document.getElementById("car-names-input");
+    this.racingCountInput = document.getElementById("racing-count-input");
+
+    this.racingCountContainer.style.display = none;
+    this.racingResultContainer.style.display = none;
+  }
+
+  initEvent() {
+    this.carGameContainer.addEventListener("click", e => {
+      switch (e.target.id) {
+        case "car-names-submit":
+          break;
+        case "racing-count-submit":
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  _validateCarNames(input) {
+    const stringArr = input.split("");
+    const unvalidatedStringArr = stringArr.filter(str => {
+      const trimedStr = str.trim();
+      return trimedStr.length > 5;
+    });
+    if (unvalidatedStringArr.length > 0) {
+      let errorStr = "";
+      unvalidatedStringArr.forEach(str => (errorStr += `다음 입력값은 올바르지 않습니다. ${str}\n`));
+      this._showErrorAlert(errorStr);
+      this.initValueAndFoucsIn("names");
+    }
+  }
+}
 
 new RacingCarGame();
