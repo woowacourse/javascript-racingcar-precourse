2718917e10c01298ae32306c28cf09b181872def
ddongule <mnk918p@gmail.com>
2020-12-06 22:37:29 +0900

    fix: clearInput에서 element의 value가 없을 때 생기는 오류를 해결

--- src/utils/input-utils.js
+++ src/utils/input-utils.js
@@ -1,9 +1,11 @@
-export default function getUserInputValue(elementId) {
+export function getUserInputValue(elementId) {
   const inputValue = document.getElementById(elementId).value;
 
   return inputValue;
 }
 
-export function clearInput(inputId) {
-  document.getElementById(inputId).value = "";
+export function clearInput(elementToClear) {
+  if (elementToClear) {
+    elementToClear.value = "";
+  }
 }
