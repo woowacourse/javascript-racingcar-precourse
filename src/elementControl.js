ddb2e7bf81cdab389801fa3bc85e328777ec7089
yang <yaaaaang57@naver.com>
2020-12-06 20:31:40 +0900

    divide element control

--- src/elementControl.js
+++ src/elementControl.js
@@ -1,3 +1,57 @@
+import { checkCarName, checkCount } from './checkInput.js';
+
+const carNameInput = document.querySelector('#car-names-input');
+const carNameSubmit = document.querySelector('#car-names-submit');
+const racingCountInput = document.querySelector('#racing-count-input');
+const racingCountSubmit = document.querySelector('#racing-count-submit');
+
+export const focusInput = target => {
+  document.querySelector(target).focus();
+};
+
+export const clearValue = target => {
+  document.querySelector(target).value = '';
+};
+
+const afterInputCarName = game => {
+  const carNameChecked = checkCarName(carNameInput.value);
+  if (carNameChecked) {
+    showCountInput();
+    game.carNames = game.carNames.concat(carNameChecked);
+    focusInput('#racing-count-input');
+  } else {
+    clearValue('#car-names-input');
+    focusInput('#car-names-input');
+  }
+};
+
+const afterInputCount = game => {
+  const countChecked = checkCount(racingCountInput.value);
+  if (countChecked) {
+    showResult();
+    game.countValue = countChecked;
+    game.setCars();
+  } else {
+    clearValue('#racing-count-input');
+    focusInput('#racing-count-input');
+  }
+};
+
+export const setEventListener = game => {
+  carNameSubmit.addEventListener('click', () => afterInputCarName(game));
+  carNameInput.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+      afterInputCarName(game);
+    }
+  });
+  racingCountSubmit.addEventListener('click', () => afterInputCount(game));
+  racingCountInput.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+      afterInputCount(game);
+    }
+  });
+};
+
 export const showCountInput = () => {
   const racingCountDiv = document.querySelector('#racing-count-div');
   const carNameInput = document.querySelector('#car-names-input');
--- src/index.js
+++ src/index.js
@@ -1,58 +1,24 @@
-import { checkCarName, checkCount } from './checkInput.js';
-import { showCountInput, showResult } from './elementControl.js';
+import { setEventListener } from './elementControl.js';
 import Car from './car.js';
 
 export default function RacingCarGame() {
-  const carNameInput = document.querySelector('#car-names-input');
-  const carNameSubmit = document.querySelector('#car-names-submit');
-  const racingCountInput = document.querySelector('#racing-count-input');
-  const racingCountSubmit = document.querySelector('#racing-count-submit');
-  let carNames = [];
-  let countValue = 0;
-  let carObjectList = [];
+  this.carNames = [];
+  this.countValue = 0;
+  this.carObjectList = [];
 
-  carNameSubmit.addEventListener('click', () => this.afterInputCarName());
-  carNameInput.addEventListener('keydown', e => {
-    if (e.keyCode === 13) {
-      this.afterInputCarName();
-    }
-  });
-  racingCountSubmit.addEventListener('click', () => this.afterInputCount());
-  racingCountInput.addEventListener('keydown', e => {
-    if (e.keyCode === 13) {
-      this.afterInputCount();
-    }
-  });
-
-  this.afterInputCarName = () => {
-    const carNameChecked = checkCarName(carNameInput.value);
-    if (carNameChecked) {
-      showCountInput();
-      carNames = carNames.concat(carNameChecked);
-      racingCountInput.focus();
-    } else {
-      carNameInput.value = '';
-      carNameInput.focus();
-    }
-  };
-
-  this.afterInputCount = () => {
-    const countChecked = checkCount(racingCountInput.value);
-    if (countChecked) {
-      showResult();
-      countValue = countChecked;
-      this.setCars(carNames, countValue);
-    } else {
-      carNameInput.value = '';
-      carNameInput.focus();
-    }
-  };
-
-  this.setCars = names => {
-    names.forEach(el => {
-      carObjectList.push(new Car(el));
+  this.setCars = () => {
+    this.carNames.forEach(el => {
+      this.carObjectList.push(new Car(el));
     });
+    console.log(this.carNames);
+    console.log(this.countValue);
+    console.log(this.carObjectList);
   };
 }
 
-new RacingCarGame();
+const init = game => {
+  setEventListener(game);
+};
+
+const game = new RacingCarGame();
+init(game);
