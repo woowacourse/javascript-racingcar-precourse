a23d8b53ff403e52caf0da2ff1df55634e872cf8
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-07 05:23:38 +0900

    Refactor: 난수 생성 메소드 개선

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,7 +1,18 @@
+import {
+	MAXIMUM_RANDOM_NUMBER,
+	MINIMUM_RANDOM_NUMBER,
+} from "../constants/constants.js";
+
 export default class Random {
 	getRandomNumber = () => {
-		const randomNumber = Math.floor(Math.random() * 9 + 1);
+		const randomNumber = this.makeRandomNumber(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
 	
+		return randomNumber;
+	}
+
+	makeRandomNumber = (min, max) => {
+		const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+		
 		return randomNumber;
 	}
 }
