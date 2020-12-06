c806f5167bf20c7cfbe44b251bf49b8664f8db36
ddongule <mnk918p@gmail.com>
2020-12-06 23:24:53 +0900

    fix: 입력된 자동차 이름의 유효성을 정확히 검사하지 못하던 오류 해결
    1) 유저가 자동차의 이름을 입력하지 않았을 때
    2) 유저가 자동차 이름을 중복으로 입력했을 때

--- src/input-validator.js
+++ src/input-validator.js
@@ -1,24 +1,25 @@
 const RETURN_MESSAGES = {
   ERROR_NAME_LENGTH_EXCEEDED: "자동차 이름은 5자 이하로 입력해주세요!",
   ERROR_NAME_INPUT_NONE: "이름을 입력해 주세요!",
+  ERROR_NAME_DUPLICATED: "이름에 중복이 있습니다!",
   ERROR_RACING_COUNT_NEGATIVE: "1 이상의 숫자를 넣어주세요!",
 };
 
-export default function isValidCarName(carNames) {
+export function isValidCarName(carNames) {
   const carNameArray = carNames.split(",");
   let isValid = false;
 
-  for (let i = 0; i < carNameArray.length; i++) {
-    if (carNameArray[i].length === 0) {
-      alert(RETURN_MESSAGES.ERROR_NAME_INPUT_NONE);
-    } else if (carNameArray[i].length > 5) {
-      alert(RETURN_MESSAGES.ERROR_NAME_LENGTH_EXCEEDED);
-    } else {
-      isValid = true;
-
-      return isValid;
-    }
+  if (carNameArray.includes("")) {
+    alert(RETURN_MESSAGES.ERROR_NAME_INPUT_NONE);
+  } else if (carNameArray.length !== [...new Set(carNameArray)].length) {
+    alert(RETURN_MESSAGES.ERROR_NAME_DUPLICATED);
+  } else if (!carNameArray.every((name) => name.length < 6)) {
+    alert(RETURN_MESSAGES.ERROR_NAME_LENGTH_EXCEEDED);
+  } else {
+    isValid = true;
   }
+
+  return isValid;
 }
 
 export function isValidRacingCount(racingCount) {
