8e507372ae7e3730968e3c2a12f531fcb5ddc68f
intae92 <kangit2013@gmail.com>
2020-12-06 09:47:02 +0900

    feat: 횟수마다 게임 기록을 저장할 객체 선언

--- src/js/racingGame.js
+++ src/js/racingGame.js
@@ -1,1 +1,28 @@
-export default () => {};
+/*
+4이상 전진 "-" forward
+3이하 멈춤 "" stay
+*/
+
+export default class RacingGame {
+  constructor(carNames, racingCount) {
+    this.carNames = carNames;
+    this.racingCount = racingCount;
+    this.racingGameRecord = {};
+    this.initialGameRound = 0;
+    this.maxScore = 0;
+    this.forward = 4;
+    this.stay = 3;
+
+    this.initialSetGameRecord(this.carNames, this.initialGameRound);
+  }
+
+  initialSetGameRecord(carNames, gameRound) {
+    this.racingGameRecord[gameRound] = {};
+    carNames.forEach((car, idx) => {
+      this.racingGameRecord[gameRound][idx] = {
+        carName: car,
+        score: 0,
+      };
+    });
+  }
+}
