d02f29139d4500af585002391e6d88d474c90439
lulu-kang <dalkang.hy@gmail.com>
2020-12-06 19:23:35 +0900

    feat: random 값 비교 후 과정 출력하는 기능

--- src/game.js
+++ src/game.js
@@ -1,5 +1,17 @@
+import { createRandomNumber } from './create.js'
+
+export function printProgress(car) {
+  const result = document.querySelector("#result")
+  car.random = createRandomNumber();
+  if (car.random >= 4) {
+    car.result += '-';
+  }
+  result.innerText += `${car.name}: ${car.result} \n`
+}
+
 export function racingCountPlay(cars, racingCountInput) {
   const carsLength = cars.length
+  const result = document.querySelector("#result")
   const resultDiv = document.querySelector("#result-div")
   resultDiv.style.display = "block"
   let i;
@@ -7,7 +19,8 @@ export function racingCountPlay(cars, racingCountInput) {
 
   for (i = 0; i < racingCountInput; i++) {
     for (j = 0; j < carsLength; j++) {
-      console.log(i, cars[j])
+      printProgress(cars[j])
     }
+  result.innerText += "\n"
   }
 }
