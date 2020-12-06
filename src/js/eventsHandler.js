3007a343be1a39bbcd9a208a1530178e4bc27eb1
intae92 <kangit2013@gmail.com>
2020-12-06 09:39:59 +0900

    refactor: racingCount변수 string->number

--- src/js/eventsHandler.js
+++ src/js/eventsHandler.js
@@ -3,13 +3,13 @@ import GetRacingCount from "./events/getRacingCountBtnClick.js";
 export default class EventHandler extends GetRacingCount {
   constructor() {
     super();
-    this.carNames = "";
-    this.racingCount = "";
+    this.carNames = [];
+    this.racingCount = 0;
   }
 
   _onClickRacingCount() {
     super._onClickRacingCount();
-    this.carNames = this.carNamesInput.value;
-    this.racingCount = this.racingCountInput.value;
+    this.carNames = this.carNamesInput.value.split(",");
+    this.racingCount = Number(this.racingCountInput.value);
   }
 }
