03c8628cc04e8a531ee4b0e58b7689309797bb60
ket87 <ket8780@gmail.com>
2020-12-06 18:54:26 +0900

    자동차  이동 메서드 구현

--- src/Car.js
+++ src/Car.js
@@ -3,7 +3,15 @@ export default class Car {
     this.name = name;
     this.distance = 0;
   }
+
   _makeRandomNumber(max = 9, min = 0) {
     return Math.floor(Math.random() * max) + min;
+  }
+
+  move() {
+    const randomNumber = this._makeRandomNumber();
+    if (randomNumber >= 4) {
+      this.distance += 1;
+    }
   }
 }
