05d2bbe0c1af2fa65c6e43628aeefe524ff164fb
intae92 <kangit2013@gmail.com>
2020-12-06 10:01:46 +0900

    feat: 게임결과 값 변수에 선언

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import EventsHandler from "./js/eventsHandler.js";
+import RacingGame from "./js/racingGame.js";
+import render from "./js/render.js";
 
 export default class App extends EventsHandler {
   constructor() {
     super();
+  }
+
+  _onClickRacingCount() {
+    super._onClickRacingCount(this.carNames, this.racingCount);
+    const racingGame = new RacingGame(this.carNames, this.racingCount);
+    const racingGameResult = [this.carNames, this.racingCount].concat(
+      racingGame.getRacingGameRecord()
+    );
+
+    // render(racingGameResult);
   }
 }
