80c6bd0dd34d60dffe7d122e4f235cfec59b434d
intae92 <kangit2013@gmail.com>
2020-12-06 09:42:46 +0900

    style: 주석 삭제

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -4,5 +4,5 @@ export default () => {
   const min = variablesObj.RANDOM_NUMBER_MIN;
   const max = variablesObj.CAR_NAME_LENGTH_MAX;
 
-  return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
