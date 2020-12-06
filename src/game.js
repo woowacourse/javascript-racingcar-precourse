1cf1ced9fc4aefb42681f975aace06efa83a214b
swimme <2sy100@likelion.org>
2020-12-07 06:36:51 +0900

    feat: 게임 실행시 입력된 이름들로 자동차 인스턴스 생성하기

--- src/game.js
+++ src/game.js
@@ -1,3 +1,15 @@
+import Car from "./car.js";
 export default class RacingCarGame {
-  constructor() {}
+  constructor(carNames, racingCount) {
+    this.racingCars = this.createCars(carNames);
+    this.racingCount = racingCount;
+  }
+
+  createCars(carNames) {
+    const cars = carNames.map((carName) => new Car(carName));
+    return cars;
+  }
 }
+
+// //test
+// new RacingCarGame(["east", "west"], 3);
