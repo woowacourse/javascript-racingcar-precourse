71fae482c0f49690083a6ad161374a203e6864d9
ket87 <ket8780@gmail.com>
2020-12-06 18:48:47 +0900

    랜덤 넘버 생성 메서드 구현

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,9 @@
 export default class Car {
   constructor(name) {
     this.name = name;
+    this.distance = 0;
+  }
+  _makeRandomNumber(max = 9, min = 0) {
+    return Math.floor(Math.random() * max) + min;
   }
 }
