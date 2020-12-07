32e70ee95f9ac734a9b7078a4b3544c0c4eb69dc
Daniel <elrion018@gmail.com>
2020-12-07 14:26:34 +0900

    feat(RacingCarGameModel) : add setProxy function

--- src/modules/models/RacingCarGameModel/index.js
+++ src/modules/models/RacingCarGameModel/index.js
@@ -3,6 +3,24 @@ import { Car } from '../../models';
 export default class RacingCarGameModel {
   constructor() {
     this._carInstances = null;
+    return this.setProxy();
+  }
+
+  setProxy() {
+    return new Proxy(this, {
+      get(target, property) {
+        return target[property];
+      },
+
+      set(target, property, value) {
+        if (property === '_carInstances') {
+          return target.setInstances(target, property, value);
+        }
+        if (property === '_racingCount') {
+          // return target.setRacingCountAndGameContinue(target, property, value);
+        }
+      },
+    });
   }
 
   setInstances(target, property, names) {
