7c8661a9144d2f4db5641dfb1db9b3629098ac8e
jooeun <joonie811@naver.com>
2020-12-08 02:29:36 +0900

    feat(DOM): add id to elements

--- src/lib/classes/racingCarGame.js
+++ src/lib/classes/racingCarGame.js
@@ -1,1 +1,21 @@
-export default class RacingCarGame {}
+import { addIdToElements } from '../utils/documentHandler.js';
+
+export default class RacingCarGame {
+	constructor() {
+		this.prepareDOM();
+		this.carNamesInput = document.querySelector('#car-names-input');
+		this.carNamesSubmit = document.querySelector('#car-names-submit');
+		this.racingCountInput = document.querySelector('#racing-count-input');
+		this.racingCountSubmit = document.querySelector('#racing-count-submit');
+		this.countInputPart = document.querySelector('#count-input-part');
+		this.resultPart = document.querySelector('#result-part');
+		this.cars = [];
+		this.winners = [];
+		this.maxProgress = 0;
+		this.nTries = 0;
+	}
+
+	async prepareDOM() {
+		await addIdToElements();
+	}
+}
---
+++ src/lib/utils/documentHandler.js
@@ -0,0 +1,19 @@
+export const addIdToElements = () => {
+	const [carNamesInput, racingCountInput] = document.getElementsByTagName(
+		'input'
+	);
+	const [carNamesSubmit, racingCountSubmit] = document.getElementsByTagName(
+		'button'
+	);
+	carNamesInput.id = 'car-names-input';
+	racingCountInput.id = 'racing-count-input';
+	carNamesSubmit.id = 'car-names-submit';
+	racingCountSubmit.id = 'racing-count-submit';
+
+	const childrenOfCarGameContainer = document.querySelector(
+		'.car-game-container'
+	).children;
+	childrenOfCarGameContainer[1].id = 'count-input-part';
+	const childrenOfApp = document.querySelector('#app').children;
+	childrenOfApp[3].id = 'result-part';
+};
