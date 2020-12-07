c7d71a1ee85809520961060f82a9fbb7deafd4f5
cuza22 <yeji.woo97@gmail.com>
2020-12-08 05:21:51 +0900

    [index.js] export GAME

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import { startGame } from './utils.js';
+import { carNamesListener, racingCountListener } from './input.js';
+import { ID } from './constants.js';
+
 export default function RacingCarGame() {
+    startGame(this);
+    console.log(this);
+
+    ID.CAR_NAME_BUTTON.addEventListener('click', carNamesListener());
+    ID.RACING_COUNT_BUTTON.addEventListener('click', racingCountListener());
+    console.log(this.numberOfCars, this.racingCount);
 }
-// export default class RacingCarGame {
-// }
 
-
-new RacingCarGame();
+export let GAME = new RacingCarGame();
