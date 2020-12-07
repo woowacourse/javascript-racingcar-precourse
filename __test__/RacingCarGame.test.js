46964a5293ce701d853ee5aee795991feabd3aa5
bigsaigon333 <likepeppermint@gmail.com>
2020-12-07 23:57:20 +0900

    test: 최종 우승자를 선택하는 함수의 유닛테스트 작성 등
    1. 최종 우승자를 선택하는 함수의 유닛테스트 작성
    2. 사용자가 입력한 자동차 이름이 공백문자를 포함하지 않으며 중복되지 않음을 검증하는 유닛테스트 추가
    3. refactoring으로 에러발생하는 테스트 함수 수정
    4. 테스트파일에서는 eslint 작동하지 않도록 eslint 설정 변경

--- __test__/RacingCarGame.test.js
+++ __test__/RacingCarGame.test.js
@@ -1,33 +1,38 @@
-import { RacingCarGame } from "../src/RacingCarGame.js";
-import { getRandomNumber } from "../src/util.js";
-
-jest.spyOn(document, "querySelector").mockReturnValue(document.documentElement);
+import { Car } from "../src/Car.js";
+import {
+  getRandomNumber,
+  isValidCarNamesInput,
+  isValidRacingCount,
+  getWinners
+} from "../src/util.js";
 
 describe("사용자가 입력한 자동차의 이름을 검증한다", () => {
-
-  const racingCarGame = new RacingCarGame();
   test("자동차 이름은 5자 이하여야 한다.", () => {
-    expect(racingCarGame.isValidCarNamesInput("east,west")).toBe(true);
+    expect(isValidCarNamesInput("east,west")).toBe(true);
+    expect(isValidCarNamesInput("east,westjackson")).toBe(false);
+  });
 
-    expect(racingCarGame.isValidCarNamesInput("east,westjackson")).toBe(false);
+  test("자동차 이름은 공백문자를 포함하고 있지 않아야 한다.", () => {
+    expect(isValidCarNamesInput(",js,3")).toBe(false);
+  });
+
+  test("자동차 이름은 중복되어서는 안된다", () => {
+    expect(isValidCarNamesInput("abc,abc")).toBe(false);
   });
 
   test("자동차 이름은 콤마로 구분되며, 2개 이상의 자동차 이름으로 구성되어야 한다.", () => {
-    expect(racingCarGame.isValidCarNamesInput("east,west")).toBe(true);
-
-    expect(racingCarGame.isValidCarNamesInput("southjackson")).toBe(false);
+    expect(isValidCarNamesInput("east,west")).toBe(true);
+    expect(isValidCarNamesInput("southjackson")).toBe(false);
   });
 });
 
 describe("사용자가 입력한 시도할 횟수를 검증한다", () => {
-  const racingCarGame = new RacingCarGame();
-
   test("시도할 횟수는 0보다 큰 정수이다(자연수이다)", () => {
-    expect(racingCarGame.isValidRacingCount(1)).toBe(true);
-    expect(racingCarGame.isValidRacingCount(2e5)).toBe(true);
-    expect(racingCarGame.isValidRacingCount(0.3)).toBe(false);
-    expect(racingCarGame.isValidRacingCount(-52)).toBe(false);
-    expect(racingCarGame.isValidRacingCount(7.0)).toBe(true);
+    expect(isValidRacingCount(1)).toBe(true);
+    expect(isValidRacingCount(2e5)).toBe(true);
+    expect(isValidRacingCount(7.0)).toBe(true);
+    expect(isValidRacingCount(0.3)).toBe(false);
+    expect(isValidRacingCount(-52)).toBe(false);
   });
 });
 
@@ -44,5 +49,27 @@ describe("자동차의 전진조건을 검증한다.", () => {
       expect(randomNumber).toBeGreaterThanOrEqual(MIN);
       expect(randomNumber).toBeLessThanOrEqual(MAX);
     }
+  });
+});
+
+describe("최종 우승자를 선택한다", () => {
+  test("가장 많이 전진해있는 자동차의 배열을 반환한다", () => {
+    const winner = new Car("1");
+    const car2 = new Car("2");
+    const car3 = new Car("3");
+  
+    winner.position = 100;
+    expect(getWinners([winner, car2, car3])).toEqual([winner]);
+  });
+
+  test("가장 많이 전진해있는 자동차가 2개 이상일 경우 모두 반환한다", () => {
+    const winner1 = new Car("1");
+    const winner2 = new Car("2");
+    const car3 = new Car("3");
+  
+    winner1.position = 100;
+    winner2.position = 100;
+
+    expect(getWinners([winner1, winner2, car3])).toEqual([winner1, winner2]);
   });
 });
