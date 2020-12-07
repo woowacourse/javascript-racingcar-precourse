58ead5f2370c4334d4cec0f2404fae48c8ef11b4
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-07 12:50:43 +0900

    feat : 자동차 경주/ 결과 HTML 생성 / 우승자 찾기/ 우승자HTML생성 가능한 cars.js 생성

--- src/cars.js
+++ src/cars.js
@@ -1,15 +1,59 @@
-import { Car } from "./car.js"
+import { Car } from "./car.js";
 
 export class Cars {
     constructor(carNameArray) {
-        this.cars = carNameArray.map(item => Car(item))
+        this.carNameArray = carNameArray.split(",");
+        this.carObjectArray = this.carNameArray.map(item => new Car(item));
+        this.winner = [];
+        this.isChecking = true;
     }
 
     makeCarsOneGame() {
-        this.cars.forEach(car => car.makeOneInningGame());
+        this.carObjectArray.forEach(car => car.makeOneInningGame());
+    }
+
+    makeHTMLAboutOneGame() {
+        let HTMLAboutOneGame = `<div>`;
+        this.carObjectArray.forEach(car => HTMLAboutOneGame += `<div>${car.name} : ${car.position}</div>`);
+        HTMLAboutOneGame += `</div><br>`;
+
+        return HTMLAboutOneGame;
+    }
+
+    findWinner() {
+        let scoreSheet = this.getCarsResult().sort((a, b) => b[1] - a[1]);
+        this.index = 0;
+        this.winner.push(scoreSheet[0][0]);
+        while (this.isChecking) {
+            this.isSame(scoreSheet, this.index);
+        }
+    }
+
+    isSame(array) {
+        if (this.index === array.length - 1) {
+            return this.isChecking = false;
+        }
+
+        if (array[this.index][1] !== array[this.index + 1][1]) {
+            return this.isChecking = false;
+        }
+
+        this.index += 1;
+        this.winner.push(array[this.index + 1][0]);
+    }
+
+    makeWinnerHTML() {
+        let HTMLAboutWinner = `<div> 최종우승자 : `;
+        HTMLAboutWinner += `${this.winner.join(", ")}`;
+        HTMLAboutWinner += `</div>`;
+
+        return HTMLAboutWinner;
     }
 
     getCarsResult() {
-        this.result = this.cars.map(car => car.getResult());
+        return this.result = this.carObjectArray.map(car => [car.name, car.position.length]);
     }
+
 }
+
+
