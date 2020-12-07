dae334c958e650e19317efe5bf326015e3e4af82
cuza22 <yeji.woo97@gmail.com>
2020-12-07 23:17:57 +0900

    constants.js

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const ID = {
+    CAR_INPUT: 'car-names-input',
+    CAR_NAME_BUTTON: 'car-names-submit',
+    RACING_COUNT_INPUT: 'racing-count-input',
+    RACING_COUNT_BUTTON: 'racing-count-submit',
+};
+
+export const MESSAGE = {
+    CAR: '-',
+    WINNER: '최종 우승자: ',
+};
+
+export const ERROR = {
+    REWRITE: '다시 입력하세요!',
+}
