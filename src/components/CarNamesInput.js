7239cabde0347f9a34ca4718faf16d9f19113619
Huijiny <jiin20803@gmail.com>
2020-12-08 01:14:26 +0900

    refact: fix typo compotnent to component

---
+++ src/components/CarNamesInput.js
@@ -0,0 +1,46 @@
+import { isValidNames } from "../utils/validations.js";
+import { Car } from "../classes/Car.js";
+
+export class CarNamesInput {
+  constructor({ setCars }) {
+    this.initializeDOM();
+    this.initializeEvents();
+    this.setCars = setCars;
+  }
+
+  initializeDOM = () => {
+    this.carNamesInput = document.getElementById("car-names-input");
+    this.carNamesSubmit = document.getElementById("car-names-submit");
+  };
+
+  initializeEvents = () => {
+    this.carNamesSubmit.addEventListener("click", () => {
+      this.handleCarNamesInput();
+    });
+  };
+
+  handleCarNamesInput = () => {
+    let names = this.getNamesFromInput();
+
+    if (!isValidNames(names)) {
+      return;
+    }
+
+    let cars = this.makeCars(names);
+    this.setCars(cars);
+  };
+
+  getNamesFromInput = () => {
+    return this.carNamesInput.value.split(",").map((name) => name.trim());
+  };
+
+  makeCars = (names) => {
+    let cars = [];
+
+    names.forEach((name) => {
+      cars.push(new Car(name));
+    });
+
+    return cars;
+  };
+}
---
+++ src/components/PlayRacingGame.js
@@ -0,0 +1,71 @@
+import { getRandomNumber } from "../utils/random.js";
+import { NUMBER as NUM } from "../constants/constants.js";
+import { CarNamesInput } from "./carNamesInput.js";
+import { RacingCountInput } from "./RacingCountInput.js";
+import { setVisibility } from "../utils/handleDom.js";
+import { GameResult } from "./GameResult.js";
+
+export class PlayRacingGame {
+  constructor() {
+    this.cars = [];
+    this.count = 0;
+
+    this.initializeDOM();
+    this.render();
+  }
+
+  initializeDOM = () => {
+    this.racingCountContainer = document.getElementById(
+      "racing-count-container"
+    );
+    this.resultContainer = document.getElementById("result-container");
+  };
+
+  setCars = (newCars) => {
+    this.cars = newCars;
+    setVisibility(this.racingCountContainer, "visible");
+  };
+
+  runRaces = (count) => {
+    this.count = count;
+    this.racing(this.cars, this.count);
+    setVisibility(this.resultContainer, "visible");
+  };
+
+  render = () => {
+    new CarNamesInput({ setCars: this.setCars });
+    new RacingCountInput({ runRaces: this.runRaces });
+    this.gameResultContainer = new GameResult({ getWinners: this.getWinners });
+  };
+
+  racing = (cars, count) => {
+    for (let i = 0; i < count; i++) {
+      this.racingInRound(cars);
+      this.gameResultContainer.updateResultPerRound(cars);
+    }
+    this.gameResultContainer.updateResultHTMLWithWinners();
+  };
+
+  racingInRound = (cars) => {
+    cars.forEach((car) => {
+      getRandomNumber(NUM.RANDOM_START, NUM.RANDOM_END) >=
+      NUM.GO_FORWARD_CONDITION
+        ? car.go()
+        : car.stop();
+    });
+  };
+
+  getWinners = () => {
+    let maxDistance = -Infinity;
+
+    this.cars.forEach((car) => {
+      if (car.distance > maxDistance) {
+        maxDistance = car.distance;
+      }
+    });
+    console.log(maxDistance);
+    return this.cars
+      .filter((car) => car.distance === maxDistance)
+      .map((car) => car.name);
+  };
+}
