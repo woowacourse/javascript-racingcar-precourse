c3ab3979c23ebfd09fd7e0564e9dbfcfe3e4fcc2
cuza22 <yeji.woo97@gmail.com>
2020-12-08 07:54:10 +0900

    [utils.js] add isGameEnd & modify playGame

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,23 @@
+import { printResultMessage, printStatusMessage } from "./output.js";
+
 // functions game progress
 export function startGame(game) {
     game.nowCount = 0;
 }
 
 export function playGame(game, carList) {
-    for (const car of carList) {
-        car.moveCar();
+    while (!isGameEnd(game)) {
+        for (const car of carList) {
+            car.moveCar();
+        }        
+        printStatusMessage(carList);
+        increaseCount(game);
     }
-    increaseCount(game);
 }
 
 export function endGame(carList) {
     let winnerList = getWinner(carList);
-    printResult(winnerList);
+    printResultMessage(winnerList);
 }
 
 // setters
@@ -32,17 +37,22 @@ export function increaseCount(game) {
     game.nowCount = game.nowCount + 1;
 }
 
+export function isGameEnd(game) {
+    return game.nowCount === game.totalCount;
+}
+
 // functions for game playing
 export function getWinner(carList) {
     let winnerList = new Array();
     let maxCount = 0;
     for (const car of carList) {
+        console.log(car.name, car.move);
         if (car.move > maxCount) {
             winnerList.length = 0;
             winnerList.push(car.name);
             maxCount = car.move;
         }
-        if (car.move === maxCount) {
+        else if (car.move === maxCount) {
             winnerList.push(car.name);
         }
     }
