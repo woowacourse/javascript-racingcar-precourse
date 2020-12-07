183889f4e4230d79fcc0227875ae79465eacea12
cuza22 <yeji.woo97@gmail.com>
2020-12-08 02:45:46 +0900

    [output.js]

---
+++ src/output.js
@@ -0,0 +1,20 @@
+import {CLASS, MESSAGE} from './constants';
+
+export function carStatus(car) {
+    return (car.name + ": " + MESSAGE.CAR.repeat(car.move));
+}
+
+export function printStatusMessage(carList) {
+    let statusMessage = document.createElement('p');
+    for (let i=0; i<carList.length; i++) {
+        statusMessage.append(carStatus(carList[i]));
+        statusMessage.append(document.createElement('br'));
+    }
+    CLASS.GAMEBOARD.appendChild(statusMessage);
+}
+
+export function printResultMessage(winnerList) {
+    let resultMessage = document.createElement('p');
+    resultMessage.append(winnerList.join(', '));
+    CLASS.GAMEBOARD.appendChild(resultMessage);
+}
