fc7be32f0cd1eacc187d4bfd7d4b598fbe3e4c69
cuza22 <yeji.woo97@gmail.com>
2020-12-07 23:30:45 +0900

    [utils.js] create utils.js

---
+++ src/utils.js
@@ -0,0 +1,44 @@
+import Car from './car.js';
+
+export default function gameBoard() {
+    function gameBoard(numberOfCars, racingCount) {
+        this.numberOfCars = numberOfCars;
+        this.totalCount = racingCount;
+        this.nowCount = 0;
+    }
+
+    function createCars(carNameList) {
+        let carList = new Array();
+        for (let i=0; i<this.numberOfCars; i++) {
+            let car = new Car(carNameList[i]);
+            carList.push(car);
+        }
+        return carList;
+    }
+
+    function increaseCount() {
+        this.nowCount = this.nowCount + 1;
+    }
+
+    function isGameEnd() {
+        return (this.totalQuarter == this.nowCount);
+    }
+
+    function playGame() {
+        // call car list
+        // play 
+        
+        // increase 'nowCount'
+        increaseCount();
+    }
+
+    function endGame() {
+        let winnerList = new Array(getWinner());
+        printResult(winnerList);
+    }
+
+    function getWinner() {
+
+    }
+
+}
