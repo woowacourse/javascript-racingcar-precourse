1f7f65de79aa9c3297a7443630a661d6566d8f7a
Seona <suna9267@gmail.com>
2020-12-07 23:54:50 +0900

    두번째 인풋창, 확인버튼을 띄워주는 showNumTry 함수 생성

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 let firstButton = document.getElementById('#car-names-submit');
 let secondButton = document.getElementById('#racing-count-submit');
+
+function showNumTry() {
+  document.getElementById('numTry').style.display = 'block';
+}
