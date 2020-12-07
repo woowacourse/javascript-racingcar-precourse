d9f9645a994c6dd78cfe27ef5e792c91d2323433
jooeun <joonie811@naver.com>
2020-12-08 02:37:08 +0900

    style: delete useTab setting

--- src/lib/classes/racingCarGame.js
+++ src/lib/classes/racingCarGame.js
@@ -1,21 +1,21 @@
 import { addIdToElements } from '../utils/documentHandler.js';
 
 export default class RacingCarGame {
-	constructor() {
-		this.prepareDOM();
-		this.carNamesInput = document.querySelector('#car-names-input');
-		this.carNamesSubmit = document.querySelector('#car-names-submit');
-		this.racingCountInput = document.querySelector('#racing-count-input');
-		this.racingCountSubmit = document.querySelector('#racing-count-submit');
-		this.countInputPart = document.querySelector('#count-input-part');
-		this.resultPart = document.querySelector('#result-part');
-		this.cars = [];
-		this.winners = [];
-		this.maxProgress = 0;
-		this.nTries = 0;
-	}
+  constructor() {
+    this.prepareDOM();
+    this.carNamesInput = document.querySelector('#car-names-input');
+    this.carNamesSubmit = document.querySelector('#car-names-submit');
+    this.racingCountInput = document.querySelector('#racing-count-input');
+    this.racingCountSubmit = document.querySelector('#racing-count-submit');
+    this.countInputPart = document.querySelector('#count-input-part');
+    this.resultPart = document.querySelector('#result-part');
+    this.cars = [];
+    this.winners = [];
+    this.maxProgress = 0;
+    this.nTries = 0;
+  }
 
-	async prepareDOM() {
-		await addIdToElements();
-	}
+  async prepareDOM() {
+    await addIdToElements();
+  }
 }
