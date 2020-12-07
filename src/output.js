8902aafc1728365164b7b43af49744a544bef608
cuza22 <yeji.woo97@gmail.com>
2020-12-08 07:26:54 +0900

    [output.js] modify functions

--- src/output.js
+++ src/output.js
@@ -6,15 +6,15 @@ function carStatus(car) {
 
 export function printStatusMessage(carList) {
     let statusMessage = document.createElement('p');
-    for (let i=0; i<carList.length; i++) {
-        statusMessage.append(carStatus(carList[i]));
+    for (const car of carList) {
+        statusMessage.append(carStatus(car));
         statusMessage.append(document.createElement('br'));
     }
-    CLASS.GAMEBOARD.appendChild(statusMessage);
+    CLASS.RESULTBOARD[0].appendChild(statusMessage);
 }
 
 export function printResultMessage(winnerList) {
     let resultMessage = document.createElement('p');
     resultMessage.append(winnerList.join(', '));
-    CLASS.GAMEBOARD.appendChild(resultMessage);
+    CLASS.RESULTBOARD[0].appendChild(resultMessage);
 }
