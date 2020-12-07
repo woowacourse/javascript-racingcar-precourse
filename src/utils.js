cdfc3af9a6ed92f920d113a10612639bffaa3018
cuza22 <yeji.woo97@gmail.com>
2020-12-08 00:17:11 +0900

    [utils.js]

--- src/utils.js
+++ src/utils.js
@@ -26,8 +26,11 @@ export default function gameBoard() {
 
     function playGame() {
         // call car list
+        let carList = this.carList;
         // play 
-        
+        for (let i=0; i<this.numberOfCars; i++) {
+            carList[i].moveCar();
+        }
         // increase 'nowCount'
         increaseCount();
     }
@@ -38,7 +41,20 @@ export default function gameBoard() {
     }
 
     function getWinner() {
-
+        let carList = this.carList;
+        let winnerList = new Array();
+        let maxCount = 0;
+        for (let i=0; i<this.numberOfCars; i++) {
+            if (carList[i].move > maxCount) {
+                winnerList.length = 0;
+                winnerList.push(carlist[i].name);
+                maxCount = carlist[i].move;
+            }
+            if (carList[i].move == maxCount) {
+                winnerList.push(carlist[i].name);
+            }
+        }
+        return winnerList;
     }
 
 }
