6c8cea7ad5dfbb4a033bf76d59b3743a6282812c
kiminseob <kis6473@naver.com>
2020-12-08 14:26:39 +0900

    refactor: index.js 함수들 모듈로 분리
    - alert 관련 함수들 -> input-alert.js
    - input 검증 함수들 -> input-validator.js
    - 문자열 split, 배열 누적합 함수 -> util.js
    - 변수값 private로 변경, 접근자 getter/setter로 변경
    - isEnd 변수 제거 -> racingCount 횟수로 종료 여부 판단

--- src/index.js
+++ src/index.js
@@ -1,117 +1,58 @@
-import AppContainer from './app-container.js';
-import Util from './util.js';
+import {getRandomNumbers, splitAndTrim, prefixSumReducer} from './util.js';
+import {isValidCarNames, isValidRacingCount} from './input-validator.js';
+import {alertCarName, alertRacingCount} from './input-alert.js';
+import EventListener from './event-listener.js';
 import Car from './car.js';
 
+
 export default class RacingCarGame {
+  #cars
+  #racingCount
+
   constructor() {
-    new AppContainer(document.getElementById('app'), this);
-    this.cars = [];
-    this.racingCount = 0;
-    this.isEnd = false;
+    new EventListener(this);
+
+    this.#cars = [];
+    this.#racingCount = 0;
   }
 
   start() {
-    if (this.racingCount === 0) return;
-
-    this.cars.forEach((car) => {
+    this.#cars.forEach((car) => {
       car.moveCount = Array
           .from(
-              {length: this.racingCount},
-              () => this.isForward(Util.getRandomNumbers()),
+              {length: this.#racingCount},
+              () => this.#isForward(getRandomNumbers(0, 9)),
           )
-          .reduce(this.moveCountReducer, []);
+          .reduce(prefixSumReducer, []);
     });
-
-    this.isEnd = true;
-    console.log(this.cars);
   }
 
-  setCars(carNames) {
-    carNames = this.splitCarNamesWithComma(carNames);
+  set cars(carNames) {
+    carNames = splitAndTrim(carNames, ',');
 
-    if (!this.isValidCarNames(carNames)) {
-      return this.alertInvalidCarName(carNames);
-    }
+    if (!isValidCarNames(carNames)) return alertCarName(carNames);
 
-    this.cars = carNames.map(carName => new Car(carName));
-    console.log(this.cars);
+    this.#cars = carNames.map(carName => new Car(carName));
   }
 
-  setRacingCount(racingCount) {
-    if (!this.isValidRacingCount(racingCount)) {
-      return this.alertInvalidRacingCount(racingCount);
-    }
-
-    this.racingCount = racingCount;
-    console.log(this.racingCount);
+  get cars() {
+    return this.#cars;
   }
 
-  splitCarNamesWithComma(carNames) {
-    return carNames.split(',')
-        .filter(carName => carName.trim().length !== 0)
-        .map(carName => carName.trim());
+  set racingCount(racingCount) {
+    if (!isValidRacingCount(racingCount)) return alertRacingCount(racingCount);
+
+    this.#racingCount = racingCount;
   }
 
-  alertInvalidCarName(carNames) {
-    if (carNames.length === 0) {
-      return alert('자동차 이름을 입력해주세요.');
-    }
-
-    if (this.isOutOfLength(carNames)) {
-      return alert('자동차 이름은 5자 이하로 입력해주세요.');
-    }
-
-    alert('자동차 이름을 중복되지 않게 입력해주세요.');
+  get racingCount() {
+    return this.#racingCount;
   }
 
-  alertInvalidRacingCount(racingCount) {
-    if (racingCount < 1) {
-      return alert('1 이상의 숫자를 입력해주세요.');
-    }
-  }
-
-  moveCountReducer(accumulator, currentValue, i) {
-    const previousValue = accumulator[i-1];
-
-    if (previousValue) {
-      accumulator.push(previousValue + currentValue);
-    } else {
-      accumulator.push(currentValue);
-    }
-
-    return accumulator;
-  }
-
-  isForward(randomNumber) {
+  #isForward(randomNumber) {
     if (randomNumber >= 4) return 1;
 
     return 0;
-  }
-
-  isValidCarNames(carNames) {
-    if (carNames.length === 0
-      || this.isOutOfLength(carNames)
-      || this.isDuplicate(carNames)) {
-      return false;
-    }
-
-    return true;
-  }
-
-  isValidRacingCount(racingCount) {
-    if (racingCount < 1) return false;
-
-    return true;
-  }
-
-  isOutOfLength(carNames) {
-    return carNames.some(carName => carName.length > 5);
-  }
-
-  isDuplicate(carNames) {
-    return carNames.some(carName =>
-      carNames.indexOf(carName) !== carNames.lastIndexOf(carName),
-    );
   }
 }
 
