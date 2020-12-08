4291cc8bfb54730f1605f7ef9db828a027221056
kiminseob <kis6473@naver.com>
2020-12-08 14:23:21 +0900

    refactor: index.js 입력검증함수 input-validator.js로 이동
    - 자동차 이름, 레이싱 횟수에 대한 입력값 검증 함수를 가진다.

---
+++ src/input-validator.js
@@ -0,0 +1,21 @@
+import {isEmpty, isOutOfLength, isDuplicate} from './util.js';
+
+class InputValidator {
+  isValidCarNames(carNames) {
+    if (isEmpty(carNames) || isOutOfLength(carNames)|| isDuplicate(carNames)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  isValidRacingCount(racingCount) {
+    if (racingCount < 1) return false;
+
+    return true;
+  }
+}
+
+const inputValidator = new InputValidator();
+
+export const {isValidCarNames, isValidRacingCount} = inputValidator;
