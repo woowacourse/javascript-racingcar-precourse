4b88faffd5a77d2c280b51f5e7c8c97c20e17c30
kiminseob <kis6473@naver.com>
2020-12-08 14:09:51 +0900

    refactor: 다른 곳에서 사용하던 유용한 함수들 util로 이동
    - splitAndTrim : 문자열을 구분 문자로 나누고 빈값은 필터링한 후 양끝 공백을
      제거한다.
    - prefixSumReducer : 배열 원소들의 누적합 배열을 새로 만들어 반환하는 reducer
    - isOutOfLength : 배열 원소들의 길이 max(default: 5) 값을 판별한다.
    - isDuplicate : 배열 원소들의 중복 여부를 판별한다.
    - isEmpty : 배열이 비어 있는지 여부를 판별한다.
    - showContainer : id 엘리먼트를 보여준다.

--- src/util.js
+++ src/util.js
@@ -2,8 +2,50 @@ class Util {
   getRandomNumbers(min = 0, max = 9) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  splitAndTrim(stringInput, delimiter = ',') {
+    return stringInput.split(delimiter)
+        .filter(part => part.trim().length !== 0)
+        .map(part => part.trim());
+  }
+
+  prefixSumReducer(accumulator, currentValue, i) {
+    const previousValue = accumulator[i-1];
+
+    if (previousValue) {
+      accumulator.push(previousValue + currentValue);
+    } else {
+      accumulator.push(currentValue);
+    }
+
+    return accumulator;
+  }
+
+  isOutOfLength(arrayInput, max = 5) {
+    return arrayInput.some(part => part.length > max);
+  }
+
+  isDuplicate(arrayInput) {
+    return arrayInput.some(part =>
+      arrayInput.indexOf(part) !== arrayInput.lastIndexOf(part),
+    );
+  }
+
+  isEmpty(arrayInput) {
+    return arrayInput.length === 0;
+  }
+
+  showContainer(id) {
+    document.getElementById(id).hidden = false;
+  }
 }
 
 const util = new Util();
 
-export default util;
+export const {
+  getRandomNumbers,
+  splitAndTrim,
+  prefixSumReducer,
+  isOutOfLength, isDuplicate, isEmpty,
+  showContainer,
+} = util;
