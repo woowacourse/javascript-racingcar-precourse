c79128c1df89532454e73cba1cf86653d317fb0a
cuza22 <yeji.woo97@gmail.com>
2020-12-08 09:39:16 +0900

    delete unused images

---
+++ _site/src/car.js
@@ -0,0 +1,17 @@
+export default function Car(name) {
+    this.name = name;
+    this.move = 0;
+
+    this.moveCar = function() {
+        if (isMoved(randomNumberCreater()))
+            this.move = this.move + 1;
+    }
+}
+
+function randomNumberCreater() {
+    return Math.floor(Math.random() * 10);
+}
+
+function isMoved(randomNumber) {
+    return ((randomNumber > 4) ? true : false);
+}
---
+++ _site/src/utils.js
@@ -0,0 +1,61 @@
+import { printResultMessage, printStatusMessage } from "./output.js";
+
+// functions game progress
+export function startGame(game) {
+    game.nowCount = 0;
+}
+
+export function playGame(game, carList) {
+    console.log(carList);
+    while (!isGameEnd(game)) {
+        for (let car of carList) {
+            car.moveCar();
+        }        
+        printStatusMessage(carList);
+        increaseCount(game);
+    }
+}
+
+export function endGame(carList) {
+    let winnerList = getWinner(carList);
+    printResultMessage(winnerList);
+}
+
+// setters
+export function setTotalCount(game, totalCount) {
+    game.totalCount = totalCount;
+}
+
+export function setCarList(game, carList) {
+    game.carList = carList;
+}
+
+export function setWinnerList(game, winnerList) {
+    game.winnerList = winnerList;
+}
+
+export function increaseCount(game) {
+    game.nowCount = game.nowCount + 1;
+}
+
+export function isGameEnd(game) {
+    return game.nowCount === game.totalCount;
+}
+
+// functions for game playing
+export function getWinner(carList) {
+    let winnerList = new Array();
+    let maxCount = 0;
+    for (let car of carList) {
+        console.log(car.name, car.move);
+        if (car.move > maxCount) {
+            winnerList.length = 0;
+            winnerList.push(car.name);
+            maxCount = car.move;
+        }
+        else if (car.move === maxCount) {
+            winnerList.push(car.name);
+        }
+    }
+    return winnerList;
+}
