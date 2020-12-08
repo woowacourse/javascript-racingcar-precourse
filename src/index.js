6f3b402cb89cce55c1ffbf792fed04b530773f07
nxxc <nomad.nxxc@gmail.com>
2020-12-08 12:32:54 +0900

    [fix] 중복된 이름일 경우 체크
    - 자동차 구분을 위하여 중복된 이름일 경우도 불가능하게 변경함

--- src/index.js
+++ src/index.js
@@ -22,27 +22,36 @@ export default class RacingCarGame {
     this.carNamesForm.setOnClick(this.onCarNamesSubmitBtnClick);
   }
 
-  carNameValidator() {
-    for (let i = 0; i < this.carNames.length; i += 1) {
-      const carName = this.carNames[i].replace(' ', '');
-      if (carName.length > 5) {
-        this.isCarNamesValid = false;
-        return;
-      }
-    }
-    this.isCarNamesValid = true;
-  }
-
-  carNameSeparator() {
-    this.carNames = this.carNamesForm.value.split(',');
-    console.log(this.carNames);
-  }
-
   onCarNamesSubmitBtnClick = () => {
     this.carNameSeparator();
     this.carNameValidator();
     this.carNamesForm.clearInput();
   };
+
+  carNameSeparator() {
+    this.carNames = this.carNamesForm.value
+      .split(',')
+      .map((carName) => carName.replace(' ', ''));
+  }
+
+  carNameValidator() {
+    this.isCarNamesValid = this.checkLength() && this.checkOverlap();
+  }
+
+  checkLength() {
+    for (let i = 0; i < this.carNames.length; i += 1) {
+      const carName = this.carNames[i];
+      if (carName.length > 5) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkOverlap() {
+    const carNamesSet = new Set(this.carNames);
+    return carNamesSet.size === this.carNames.length;
+  }
 }
 
 new RacingCarGame();
