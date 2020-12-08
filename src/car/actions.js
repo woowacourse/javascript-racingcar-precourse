1be9730242edb734aa70b71eb6de2e3d3896bee7
SEONBIN <tjsqls2003@gmail.com>
2020-12-08 12:10:39 +0900

    refactor(directory): classify feature about game

--- src/car/actions.js
+++ src/car/actions.js
@@ -21,34 +21,4 @@ const moveRacingCars = function (_racingCars) {
   }
 };
 
-const checkRacingLog = function (_racingCars) {
-  let racingLog = '';
-
-  for (let i = 0; i < _racingCars.length; i++) {
-    const racingCar = _racingCars[i];
-
-    racingLog += `${racingCar.name}: ${'-'.repeat(racingCar.moveCount)}\n`;
-  }
-
-  return `${racingLog}\n`;
-};
-
-const pickWinner = function (_racingCars) {
-  let maxMoveCount = 0;
-  let winners = [];
-
-  for (let i = 0; i < _racingCars.length; i++) {
-    const racingCar = _racingCars[i];
-
-    if (racingCar.moveCount > maxMoveCount) {
-      maxMoveCount = racingCar.moveCount;
-      winners = [racingCar.name];
-    } else if (racingCar.moveCount === maxMoveCount) {
-      winners.push(racingCar.name);
-    }
-  }
-
-  return `최종우승자: ${winners.join(', ')}`;
-};
-
-export { setRacingCars, moveRacingCars, checkRacingLog, pickWinner };
+export { setRacingCars, moveRacingCars };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import { getCarNames, getRacingCount } from './user/actions.js';
-import {
-  setRacingCars,
-  moveRacingCars,
-  checkRacingLog,
-  pickWinner,
-} from './car/actions.js';
+import play from './game/actions.js';
 
 const carNamesSubmitBtn = document.getElementById('car-names-submit');
 const racingCountSubmitBtn = document.getElementById('racing-count-submit');
@@ -15,20 +10,6 @@ const gameResult = document.getElementById('game-result');
 export default function RacingCarGame() {
   let carNames;
   let racingCount;
-
-  const play = function (_carNames, _racingCount) {
-    const racingCars = setRacingCars(_carNames);
-    let racingResult = '';
-
-    for (let i = 0; i < _racingCount; i++) {
-      moveRacingCars(racingCars);
-      racingResult += checkRacingLog(racingCars);
-    }
-
-    racingResult += pickWinner(racingCars);
-
-    return racingResult;
-  };
 
   const showHiddenContainer = function (_container) {
     _container.hidden = false;
