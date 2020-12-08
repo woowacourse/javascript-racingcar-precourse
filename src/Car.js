badea0199afbdf33b0fb3f982c102cb52fc4d6e6
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-08 20:44:22 +0900

    feat : 주어진 횟수 동안 각각의 자동차가 이동하는 기능 생성

--- src/Car.js
+++ src/Car.js
@@ -3,4 +3,10 @@ export default class Car {
     this.name = name;
     this.movingDistance = 0;
   }
+
+  drive() {
+    const randomNumber = Math.floor(Math.random() * 10);
+    this.movingDistance = randomNumber > 3 ? this.movingDistance + 1 : this.movingDistance;
+    console.log(`${randomNumber} - ${this.name} : ${this.movingDistance}`);
+  }
 }
--- src/RacingCarGame.js
+++ src/RacingCarGame.js
@@ -1,1 +1,12 @@
-export default function RacingCarGame(racingCars, racingCount) {}
+export default function RacingCarGame(racingCars, racingCount) {
+  this.racingCars = racingCars;
+  this.racingCount = racingCount;
+
+  this.playRacingGame = () => {
+    for (let i = 0; i < this.racingCount; i++) {
+      this.racingCars.forEach((value) => value.drive());
+    }
+  };
+
+  this.playRacingGame();
+}
