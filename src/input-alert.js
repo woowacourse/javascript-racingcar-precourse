eced3b34518b04c29a5151ccc6ec0c5ce3ec3968
kiminseob <kis6473@naver.com>
2020-12-08 14:20:35 +0900

    refactor: index.js의 alert함수를 input-alert.js로 이동
    - 자동차 이름, 레이싱 횟수에 잘못된 입력을 할 시 alert으로 메시지를 보여준다

---
+++ src/input-alert.js
@@ -0,0 +1,25 @@
+import {isEmpty, isOutOfLength} from './util.js';
+
+class InputAlert {
+  alertCarName(carNames) {
+    if (isEmpty(carNames)) {
+      return alert('자동차 이름을 입력해주세요.');
+    }
+
+    if (isOutOfLength(carNames)) {
+      return alert('자동차 이름은 5자 이하로 입력해주세요.');
+    }
+
+    return alert('자동차 이름을 중복되지 않게 입력해주세요.');
+  }
+
+  alertRacingCount(racingCount) {
+    if (racingCount < 1) {
+      return alert('1 이상의 숫자를 입력해주세요.');
+    }
+  }
+}
+
+const inputAlert = new InputAlert();
+
+export const {alertCarName, alertRacingCount} = inputAlert;
