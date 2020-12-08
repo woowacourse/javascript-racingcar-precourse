2fe987cf99b4f3e5dbe590352f5e912b28fbf946
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-08 22:51:10 +0900

    refactor : 이름 유효성 검사 코드 수정
    - 공백으로 시작하거나 끝나는 이름 한꺼번에 오류처리 할 수 있도록 코드 수정
    - 이름이 한 글자 미만인 경우 오류처리 하도록 코드 추가

--- src/utils/validators/carNameValidator.js
+++ src/utils/validators/carNameValidator.js
@@ -1,22 +1,22 @@
+const isEmpty = (carNames) => {
+  return carNames.some((name) => name === '');
+};
+
 export default function carNameValidator(carNames) {
   if (!carNames) {
-    alert('이름을 입력해주세요');
-    return false;
+    return alert('이름을 입력해주세요');
   }
 
-  if (carNames.every((value) => value.startsWith(' '))) {
-    alert('이름은 공백으로 시작할 수 없습니다.');
-    return false;
-  }
-
-  if (carNames.every((value) => value.endsWith(' '))) {
-    alert('이름은 공백으로 끝날 수 없습니다.');
-    return false;
+  if (carNames.some((value) => value.startsWith(' ') || value.endsWith(' '))) {
+    return alert('이름은 공백으로 시작하거나 끝날 수 없습니다.');
   }
 
   if (!carNames.every((value) => value.length <= 5)) {
-    alert('5글자 이하의 이름으로 입력해주세요');
-    return false;
+    return alert('5글자 이하의 이름으로 입력해주세요');
+  }
+
+  if (isEmpty(carNames)) {
+    return alert('한 글자 이상의 이름을 가져야합니다');
   }
 
   return true;
