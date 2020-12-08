637bf8384f9acef820966d96c03a8f81992822e0
lulu-kang <dalkang.hy@gmail.com>
2020-12-08 14:08:54 +0900

    refactor: getCarNamesInput 리팩토링

--- src/check.js
+++ src/check.js
@@ -26,6 +26,16 @@ export function isDuplicated(carNamesInput) {
   }
 }
 
+export function isAvailableNames(carNamesInput) {
+  if (isSpecialCharacter(carNamesInput) === false && isDuplicated(carNamesInput) === false && isAvailableLength(carNamesInput) === true) {
+    const racingCountDiv = document.querySelector("#racing-count-div");
+    racingCountDiv.style.display = 'block';
+    return true;
+  } else {
+    alert("5자 이하의 문자를, 공백 없이, 콤마로 구분하여 입력해 주세요.");
+  }
+}
+
 export function makeResultEmpty() {
   let result = document.querySelector("#result");
   let winner = document.querySelector("#winner")
