f6732f5a5f88ae687b81c62340255ca05e8efe76
LikeLion <wooseopim93@gmail.com>
2020-12-08 19:16:13 +0900

    feat: 레이싱 게임 초기화 관련 함수 모음

---
+++ src/initial.js
@@ -0,0 +1,49 @@
+function carInfo(name, count) {
+  this.name = name;
+  this.count = count;
+}
+
+export const isCarForward = () => {
+  const going = Math.floor(Math.random()*10);
+
+  if (going < 5) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export const userInput = (inputString) => {
+  const inputArray = inputString.split(",");
+  const carArray = [];
+
+  for ( let i = 0; i < inputArray.length; i++ ) {
+    inputArray[i].length > 0 && carArray.push(new carInfo(inputArray[i], 0))
+  }
+
+  return carArray
+}
+
+export const setContainerVisible = (className, visible) => {
+  document.getElementsByClassName(className)[0].getElementsByClassName.display = visible
+    ? "initial"
+    : "none";
+}
+
+export const init = () => {
+  setContainerVisible("car-game-count-container", false);
+  setContainerVisible("car-game-result-container", false);
+
+  document.getElementById("car-names-input").value = "";
+  document.getElementById("racing-count-input").value = "";
+  
+  clear()
+}
+
+export const clear = () => {
+  const resultContainer = document.getElementsByClassName("car-game-result-container")[0];
+  const nodeLength = resultContainer.children.length;
+  for ( let i = 1; i < nodeLength; i++) {
+    resultContainer.children[1].remove()
+  }
+}
