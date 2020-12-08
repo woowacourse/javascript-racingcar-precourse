6f552d0f2d1dbb394879cd85f068f1acb90672b2
kiminseob <kis6473@naver.com>
2020-12-08 14:01:18 +0900

    refactor: app-printer 파일명 racing-result-printer로 변경
    - 함수 재배치
    - 생성자에서 this를 함수에 바인딩한 것을 없애고 화살표 함수로 변경
    - printAll 함수를 추가하여 레이싱 결과, 최종 우승자를 한 번에 프린트
    - 'br' 엘리먼트를 만들어서 개행 했던 부분을 innerHTML에 <br> 태그를 넣는 것으
      수정

---
+++ src/racing-result-printer.js
@@ -0,0 +1,55 @@
+class RacingResultPrinter {
+  constructor() {
+    this.racingResultContainer = document.getElementById('racing-result');
+  }
+
+  printAll = (racingCarGame) => {
+    this.printRacingResult(racingCarGame);
+    this.printWinnersName(racingCarGame);
+  }
+
+  printRacingResult = ({cars, racingCount}) => {
+    for (let racingRound = 0; racingRound < racingCount; ++racingRound) {
+      const roundResultContainer = this.#createRoundResult(cars, racingRound);
+
+      this.racingResultContainer.appendChild(roundResultContainer);
+      this.racingResultContainer.appendChild(document.createElement('br'));
+    }
+  }
+
+  printWinnersName = ({cars, racingCount}) => {
+    const winnersNameContainer = document.createElement('div');
+    const winnersName = this.#findWinnersName(cars, racingCount);
+
+    winnersNameContainer.innerHTML = `최종 우승자: ${winnersName.join(', ')}`;
+
+    this.racingResultContainer.appendChild(winnersNameContainer);
+  }
+
+  #createRoundResult(cars, racingRound) {
+    const roundResultContainer = document.createElement('div');
+
+    cars.forEach((car)=>{
+      const roundResultText
+      = `${car.name}: ${'-'.repeat(car.moveCount[racingRound])}<br>`;
+
+      roundResultContainer.innerHTML += roundResultText;
+    });
+
+    return roundResultContainer;
+  }
+
+  #findWinnersName(cars, racingCount) {
+    const maxMoveCount = cars
+        .map(car => car.moveCount[racingCount - 1])
+        .reduce((acc, cur) => Math.max(acc, cur));
+
+    return cars
+        .filter(car => car.moveCount[racingCount - 1] === maxMoveCount)
+        .map(car => car.name);
+  }
+}
+
+const racingResultPrinter = new RacingResultPrinter();
+
+export const {printAll} = racingResultPrinter;
