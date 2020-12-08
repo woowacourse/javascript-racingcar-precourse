9ca13170f5a98015ff5b899d0a2aa53d0a60258b
sunhpark42 <sunhpark42@gmail.com>
2020-12-08 15:37:48 +0900

    style: delete JSDocs

--- src/Car.js
+++ src/Car.js
@@ -1,32 +1,17 @@
-/**
- * @author sunhpark42 <sunhpark42@gmail.com>
- * Car module with getRandomNumber and Go
- */
 export default class Car {
-  /**
-   * init Car by name, position.
-   * @param {string} name
-   */
   constructor(name) {
     this.name = name;
     this.position = 0;
   };
-  /**
-   * getRandom Number 0 - 9
-   * @return {number}
-   */
+
   getNumber() {
     return Math.floor(Math.random() * Math.floor(10));
   }
-  /**
-   * go foward 1 step.
-   */
+
   go() {
     this.position += 1;
   };
-  /**
-   * play one stage
-   */
+
   play() {
     if (this.getNumber() >= 4) {
       this.go();
