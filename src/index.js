f1cda265b0ba1542f0cb246fcda8ac766e0e50e8
Minto <xpeed96@naver.com>
2021-07-31 10:50:00 +0900

    make-file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
+// export default function RacingCarGame() {
+
 // }
+// // export default class RacingCarGame {
+// // }
 
+// new RacingCarGame();
 
-new RacingCarGame();
+const nameInput = document.querySelector("#car-names-input");
+const nameBtn = document.querySelector("#car-names-submit");
+const countInput = document.querySelector("#racing-count-input");
+const countBtn = document.querySelector("#racing-count-submit");
+const result = document.querySelector(".car-game-list");
+
+let namesArray = [];
+
+nameBtn.addEventListener("click", () => {
+  const names = nameInput.value;
+  nameValue = names.split(",");
+  for (let i = 0; i < nameValue.length; i++) {
+    namesArray.push({ value: nameValue[i], count: 0 });
+  }
+});
+
+countBtn.addEventListener("click", () => {
+  for (let i = 0; i < Number(countInput.value); i++) {
+    for (let j = 0; j < namesArray.length; j++) {
+      if (Math.floor(Math.random() * 10) >= 4) {
+        namesArray[j].count++;
+      }
+    }
+  }
+  showResult("sex", "html", "css");
+  console.log(namesArray);
+});
+
+function showResult() {
+  for (let i = 0; i < arguments.length; i++) {
+    result.innerHTML = `${arguments[0]}: ${arguments[1]}`;
+    console.log("sex");
+  }
+}
