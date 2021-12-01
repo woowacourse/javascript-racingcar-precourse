a03bbae9cb58238852dae19177e026341499432b
Jeongmin Woo <greenblues1190@gmail.com>
2021-12-01 23:00:05 +0900

    feat(*): 자동차 이름 유효성 검증하기 기능 추가
    - isValidCarNames: 중복과 이름 길이 검사

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 function App() {
+  function isValidCarNames(carNameList) {
+    const isUnique = (array) => {
+      const set = new Set(array);
+
+      return array.length === set.size;
+    };
+
+    const isLengthValid = (array, min, max) =>
+      array.every((str) => str.length >= min && str.length <= max);
+
+    return isUnique(carNameList) && isLengthValid(carNameList, 1, 5);
+  }
+
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
+
   document.querySelector('#car-names-submit').addEventListener('click', () => {
     const inputString = document.querySelector('#car-names-input').value.trim();
     const nameList = inputString.split(',').map((name) => name.trim());
+
+    if (!isValidCarNames(nameList)) {
+      alert('잘못된 입력입니다.');
+      document.querySelector('#car-names-input').value = '';
+
+      return;
+    }
+
     console.log(nameList);
   });
 }
