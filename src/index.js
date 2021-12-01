e64028c3fd178ed093db4a7f5fd162a5bced4a65
minbr0ther <minbr0ther@naver.com>
2021-12-01 23:11:32 +0900

    feat: 시도할 횟수 입력 폼 가리기, 실행결과 제목 가리기

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-export default function RacingCarGame() {}
+export default function RacingCarGame() {
+  const init = () => {
+    document.querySelector('#racing-count-title').style.display = 'none';
+    document.querySelector('#racing-count-form').style.display = 'none';
+    document.querySelector('#racing-result-title').style.display = 'none';
+  };
+
+  init();
+}
 
 new RacingCarGame();
