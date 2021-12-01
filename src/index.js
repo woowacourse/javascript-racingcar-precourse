d81fec48a32d572473cded52b81a3ba00ed849ad
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-12-01 19:13:57 +0900

    feat: 차 이름 입력시 예외처리하는 함수들 추가

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ export default class RacingCarGame {
     event.preventDefault();
     const $carNameInput = document.querySelector("#car-names-input");
     const carNames = $carNameInput.value.split(",");
+    const alertMessage = this.createCarNameAlertMessage(carNames);
+
+    if (alertMessage) {
+      alert(alertMessage);
+      return;
+    }
     console.log(carNames);
     this.permitRacingCountForm();
   };
@@ -30,6 +36,39 @@ export default class RacingCarGame {
     const $racingCountButton = document.querySelector("#racing-count-submit");
     $racingCountInput.disabled = false;
     $racingCountButton.disabled = false;
+  };
+
+  isDuplicatedCarName = (carNames) => {
+    return [...new Set(carNames)].length !== carNames.length;
+  };
+
+  hasEmpty = (carNames) => {
+    return carNames.some((carName) => carName === "");
+  };
+
+  isInvalidCarNameLength = (carNames) => {
+    return carNames.some((carName) => carName.length > 5);
+  };
+
+  hasSpace = (carNames) => {
+    return carNames.some((carName) => carName.match(/[" "]/));
+  };
+
+  createCarNameAlertMessage = (carNames) => {
+    if (this.isDuplicatedCarName(carNames)) {
+      return "이름이 중복되었습니다";
+    }
+    if (this.hasEmpty(carNames)) {
+      return "이름은 공백이 될수 없습니다";
+    }
+    if (this.isInvalidCarNameLength(carNames)) {
+      return "이름은 5글자이하로 입력해주세요";
+    }
+    if (this.hasSpace(carNames)) {
+      return "이름에 공백이 포함되서는 안됩니다";
+    }
+
+    return "";
   };
 }
 
