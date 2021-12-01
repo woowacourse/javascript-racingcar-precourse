9beb77b1144ae18b3de3f9409d00b6b84a084c29
Jeongmin Woo <greenblues1190@gmail.com>
2021-12-02 01:34:33 +0900

    feat(App): 레이싱 횟수 유효성 검증하기 기능 추가
    - 입력값 유효성 검증
    - 검증 실패 시 에러메시지를 보여주고 racing-count-input의 value를 빈 문자열로 초기화
    - 검증 성공 시 remainingRacingCount 업데이트

--- src/index.js
+++ src/index.js
@@ -1,43 +1,69 @@
+/* eslint-disable class-methods-use-this */
 import Car from './Car.js';
-import isValidCarNames from './utils/utils.js';
+import { isValidCarNames, isRacingCountValid } from './utils/utils.js';
 import { $, hideElement, showElement } from './dom/helpers.js';
 
-function App() {
-  hideElement('#racing-count-heading');
-  hideElement('#racing-count-form');
-  hideElement('#result-heading');
+class App {
+  constructor() {
+    this.remainingRacingCount = 0;
+  }
 
-  $('#car-names-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
-  document
-    .querySelector('#racing-count-form')
-    .addEventListener('submit', (e) => {
+  init() {
+    this.hideElements();
+    this.setEventListner();
+  }
+
+  hideElements() {
+    hideElement('#racing-count-heading');
+    hideElement('#racing-count-form');
+    hideElement('#result-heading');
+  }
+
+  setEventListner() {
+    $('#car-names-form').addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+    $('#racing-count-form').addEventListener('submit', (e) => {
       e.preventDefault();
     });
 
-  $('#car-names-submit').addEventListener('click', () => {
-    const inputString = $('#car-names-input').value.trim();
-    const nameList = inputString.split(',').map((name) => name.trim());
+    $('#car-names-submit').addEventListener('click', () => {
+      const inputString = $('#car-names-input').value.trim();
+      const nameList = inputString.split(',').map((name) => name.trim());
 
-    if (!isValidCarNames(nameList)) {
-      alert('잘못된 입력입니다.');
-      $('#car-names-input').value = '';
+      if (!isValidCarNames(nameList)) {
+        alert('잘못된 입력입니다.');
+        $('#car-names-input').value = '';
 
-      return;
-    }
+        return;
+      }
 
-    const carList = nameList.map((name) => new Car(name));
+      const carList = nameList.map((name) => new Car(name));
 
-    console.log(carList);
+      console.log(carList);
 
-    showElement('#racing-count-heading');
-    showElement('#racing-count-form');
-  });
+      showElement('#racing-count-heading');
+      showElement('#racing-count-form');
+    });
 
-  $('#racing-count-submit').addEventListener('click', () => {
-    console.log($('#racing-count-input').valueAsNumber);
-  });
+    $('#racing-count-submit').addEventListener('click', () => {
+      const racingCount = $('#racing-count-input').valueAsNumber;
+
+      if (!isRacingCountValid(racingCount)) {
+        alert('잘못된 입력입니다.');
+        $('#racing-count-input').value = '';
+
+        return;
+      }
+
+      this.remainingRacingCount = racingCount;
+
+      console.log(this);
+    });
+  }
 }
 
-App();
+(() => {
+  const app = new App();
+  app.init();
+})();
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,7 +3,14 @@ const isUnique = (array) => array.length === new Set(array).size;
 const isLengthValid = (array, min, max) =>
   array.every((str) => str.length >= min && str.length <= max);
 
-const isValidCarNames = (carNameList) =>
+export const isValidCarNames = (carNameList) =>
   isUnique(carNameList) && isLengthValid(carNameList, 1, 5);
 
-export default isValidCarNames;
+const isRangeValid = (number, min, max) => number >= min && number <= max;
+
+export const isRacingCountValid = (racingCount) => {
+  return (
+    Number.isInteger(racingCount) &&
+    isRangeValid(racingCount, 1, Number.MAX_SAFE_INTEGER)
+  );
+};
