65fe3b6b23c3174819272b1caf0dd7c04c2dbd07
LAH1203 <lah1203@naver.com>
2021-12-01 18:57:19 +0900

    refactor: .eslintrc의 prettier 내용을 .prettierrc로 이동

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,6 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
     "import/extensions": "off",
     "import/prefer-default-export": "off",
     "max-depth": ["error", 2],
