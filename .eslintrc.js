71c8652797f0016ed3717b80e110b358c841748e
minbr0ther <minbr0ther@naver.com>
2021-12-01 21:03:24 +0900

    chore: eslint & prettier 설치

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+};
