a03e0995a4af760338448c97ba2d76540ed77512
LAH1203 <lah1203@naver.com>
2021-12-01 17:04:23 +0900

    chore: NHN의 tui eslint에 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: [],
+  extends: ['tui'],
   plugins: [],
   parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
   rules: {
     'import/extensions': 'off',
     'import/prefer-default-export': 'off',
+    'linebreak-style': ['error', 'windows'],
     'max-depth': ['error', 2],
     'max-lines-per-function': ['error', 15],
     'spaced-comment': ['error', 'always'],
     'no-new': 'off',
     'no-alert': 'off',
     'no-console': 'off',
-    'no-plusplus': 'off',
-  },
+    'no-plusplus': 'off'
+  }
 };
