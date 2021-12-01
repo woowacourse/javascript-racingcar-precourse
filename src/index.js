84a483c6b8d89da32724e355f2bdcde0dbee8f8f
minbr0ther <minbr0ther@naver.com>
2021-12-01 23:17:33 +0900

    feat: 자동차 이름을 입력 받는다

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ export default function RacingCarGame() {
     document.querySelector('#racing-count-title').style.display = 'none';
     document.querySelector('#racing-count-form').style.display = 'none';
     document.querySelector('#racing-result-title').style.display = 'none';
+    eventListeners();
+  };
+
+  const handleCarNamesSubmit = e => {
+    e.preventDefault();
+    const carNames = document.querySelector('#car-names-input').value;
+    console.log(carNames);
+  };
+
+  const eventListeners = () => {
+    document.querySelector('#car-names-submit').onclick = handleCarNamesSubmit;
   };
 
   init();
