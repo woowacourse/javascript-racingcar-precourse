fbf529219b6e617600d35615b852cd17ed76d300
LAH1203 <lah1203@naver.com>
2021-12-01 23:02:45 +0900

    feat: 입력된 레이싱 횟수만큼 모든 자동차를 전진 또는 멈추게 하는 기능 구현

---
+++ src/controllers/play/playGame.js
@@ -0,0 +1,17 @@
+import { LIMIT_OF_RANDOM_NUMBER, MINIMUM_NUMBER_TO_ADVANCE } from "../../utils/constants.js";
+import { getRandomNumber } from "./getRandomNumber.js";
+
+export const playGame = (cars, racingCount) => {
+  for (let i = 0; i < racingCount; i++) {
+    cars.forEach(car => {
+      const { min, max } = LIMIT_OF_RANDOM_NUMBER;
+      const ranNum = getRandomNumber(min, max);
+
+      if (ranNum >= MINIMUM_NUMBER_TO_ADVANCE) {
+        car.location++;
+      }
+    });
+  }
+
+  return cars;
+};
