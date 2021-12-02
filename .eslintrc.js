03184acf38453a4c480ca56843cf018b069c448a
parksil0 <parksil00@naver.com>
2021-12-02 22:23:55 +0900

    style : eslint, prettier 초기 셋팅

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'prettier',
+    'eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': ['error', 'always'],
+  },
+};
