470e08b4f684d5e8adc34ad9706ac5a63eeb655e
mangming2 <leejiho35@gmail.com>
2021-12-02 12:03:03 +0900

    [Update]: index.js
    html에 div 추가없이 index.js에서만 car 들 출력하게 업데이트

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import Car from './car.js';
 
 
 export default function racingGame(){
@@ -6,25 +6,27 @@ export default function racingGame(){
   const carNameSubmitBtn=document.querySelector('#car-names-submit');
   const racingCountInput=document.querySelector('#racing-count-input');
   const racingCountSubmitBtn=document.querySelector('#racing-count-submit');
+  const resultBox= document.querySelector('#app');
   
-  function Car(name) {
-    this.name = name;
-    this.count=0;
-  }
+
   function makeinstance(){
     let listValue=carNameInput.value.split(',');
     let carList=listValue.map(name=>new Car(name));
     let carResult="";
     console.log(listValue);
     carList.forEach(carName => {
-      carName.count+=1;
+      carName.count+=MissionUtils.Random.pickNumberInRange(1, 9);
       carResult+=`${carName.name}:${'-'.repeat(carName.count)}<br> `;
       console.log(carName.name);
       console.log(carName.count);
       console.log(carResult);
       
     });
-    
+    const racingResult=document.createElement('div');
+    racingResult.innerHTML=`${carResult}`;
+    console.log(carResult);
+    console.log(racingResult.innerHTML);
+    resultBox.appendChild(racingResult);
      
   }
   carNameSubmitBtn.addEventListener("click",()=>{
