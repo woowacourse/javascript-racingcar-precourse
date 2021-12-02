77e883684b67aa96f5bdb9938a4c662927a800bb
jswith <9geul@naver.com>
2021-12-02 21:04:01 +0900

    refactor: 함수 이름 수정

--- src/data/carNameInput.js
+++ src/data/carNameInput.js
@@ -2,9 +2,8 @@ import { $ } from '../utils/dom.js';
 import { MAX_CAR_NAME_LENGTH } from './constatns.js';
 import { Car } from './Car.js';
 
-const isNonInput = () => $('#car-names-input').value == '';
-
-const isValidLength = () =>
+export const isNonCarNames = () => $('#car-names-input').value == '';
+export const isValidLength = () =>
   $('#car-names-input')
     .value.split(',')
     .every((carName) => carName.length <= MAX_CAR_NAME_LENGTH);
@@ -13,8 +12,6 @@ const getCarNames = () => {
   return $('#car-names-input').value.split(',');
 };
 
-const genCarInstances = () => {
+export const genCarInstances = () => {
   return getCarNames().map((carName) => new Car(carName));
 };
-
-export { isNonInput, isValidLength, genCarInstances };
--- src/data/racingCountInput.js
+++ src/data/racingCountInput.js
@@ -1,5 +1,5 @@
 import { $ } from '../utils/dom';
 
-export const isNonInput = () => $('#racing-count-input').value == '';
+export const isNonRacingCount = () => $('#racing-count-input').value == '';
 export const isNumber = () => !isNaN(Number($('#racing-count-input').value));
 export const isNegative = () => Number($('#racing-count-input').value) < 0;
