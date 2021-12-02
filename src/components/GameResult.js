bc897cbcbc46270c24a087624c0482674cac78b2
moonheekim0118 <hellomooneekim@gmail.com>
2021-12-02 23:27:32 +0900

    feat: 우승한 자동차를 화면에 출력한다.

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,11 +1,12 @@
 import Component from '../core/Component.js';
-import { $ } from '../utils/dom.js';
+import { $, createElement } from '../utils/dom.js';
 import { GAME_STATUS } from '../utils/constants.js';
 import { printCarResult } from '../utils/game.js';
 
 export default class GameResult extends Component {
   initDoms() {
     this.container = $('#game-result');
+    this._winnerElem = createElement('racing-winners', '', 'span');
   }
 
   _printResult() {
@@ -20,7 +21,10 @@ export default class GameResult extends Component {
   }
 
   render() {
-    if (this.props.gameStatus !== GAME_STATUS.END) return;
-    this.container.innerHTML = this._printResult();
+    const { gameStatus, winners } = this.props;
+    if (gameStatus !== GAME_STATUS.END) return;
+    this.container.innerHTML = `${this._printResult()} <br/> 최종 우승자:`;
+    this._winnerElem.innerHTML = winners.join(',');
+    this.container.appendChild(this._winnerElem);
   }
 }
