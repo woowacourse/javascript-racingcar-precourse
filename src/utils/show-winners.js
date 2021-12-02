044c863e0b2c5a53e7d4273c7f9a07b48f7046d6
hanyoonseok <hys6078@naver.com>
2021-12-02 18:19:40 +0900

    feat: 우승자 출력 함수 작성 우승자 리스트에서 이름만을 추출하여 우승 메시지 생성 최종 우승자 컴포넌트 결과에 추가

--- src/utils/show-winners.js
+++ src/utils/show-winners.js
@@ -1,13 +1,37 @@
+import { $, createElementWithContents } from './common.js';
+
 const sortCarsByMove = carObjectList => carObjectList.sort((a, b) => b.move - a.move);
 
-const getWinners = carObjectList => {
+const getWinnersObjectList = carObjectList => {
   const sortedCarObjectList = sortCarsByMove(carObjectList);
   const winnersMove = sortedCarObjectList[0].move;
 
   return sortedCarObjectList.filter(x => x.move === winnersMove);
 };
 
+const makeWinnersMessage = winnersObjectList => {
+  const winnersArray = winnersObjectList.map(x => x.name);
+  const winnersMessage = winnersArray.join(', ');
+
+  return winnersMessage;
+};
+
+const makeWinnersSpan = winnersMessage => {
+  const winnersSpan = createElementWithContents('span', `${winnersMessage}`);
+  winnersSpan.id = 'racing-winners';
+
+  return winnersSpan;
+};
+
+const makeWinnersComponent = winnersObjectList => {
+  const winnersMessage = makeWinnersMessage(winnersObjectList);
+  const resultDiv = $('result');
+  const finalWinnersText = createElementWithContents('span', '최종 우승자: ');
+  resultDiv.appendChild(finalWinnersText);
+  resultDiv.appendChild(makeWinnersSpan(winnersMessage));
+};
+
 export default function showWinners(carObjectList) {
-  const winnersObjectList = getWinners(carObjectList);
-  console.log(winnersObjectList);
+  const winnersObjectList = getWinnersObjectList(carObjectList);
+  makeWinnersComponent(winnersObjectList);
 }
