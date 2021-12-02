8ba6a1e3d794fd4c504bb4ea512cdf50824c9ff1
minbr0ther <minbr0ther@naver.com>
2021-12-02 09:52:06 +0900

    feat: 자동차 이름 공백 입력시 예외 처리

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { $ } from './utils.js';
+import { $, isEmptyCarNames } from './utils.js';
 import { default as UI } from './DOMUtils.js';
 
 export default function RacingCarGame() {
@@ -7,10 +7,16 @@ export default function RacingCarGame() {
     UI.hideSection();
   };
 
+  const isValidCarNames = string => {
+    if (isEmptyCarNames(string)) return alert('공백 입력을 하실 수 없습니다.');
+  };
+
   const handleCarNamesSubmit = e => {
     e.preventDefault();
+
     const carNames = $('#car-names-input').value;
-    console.log(carNames);
+    if (!isValidCarNames(carNames)) return;
+    // 2. 검증이 완료되면 자동차를 생성한다.
   };
 
   const activateEventListeners = () => {
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 export const $ = selector => document.querySelector(selector);
+
+export const isEmptyCarNames = string => {
+  return /\s/g.test(string);
+  //주어진 문자열 str 중 정규 표현식이 일치하는 부분이 있으면 true, 아니면, false.
+};
