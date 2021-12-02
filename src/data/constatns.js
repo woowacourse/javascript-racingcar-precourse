757105cf4e7dc7ba24b68c394abee1b07e7b096e
jswith <9geul@naver.com>
2021-12-02 20:22:04 +0900

    refactor: 상수 파일 분리

---
+++ src/data/constatns.js
@@ -0,0 +1,3 @@
+export const MAX_CAR_NAME_LENGTH = 5;
+export const MIN_NUMBER = 0;
+export const MAX_NUMBER = 9;
--- src/utils/genRandomNumber.js
+++ src/utils/genRandomNumber.js
@@ -1,5 +1,4 @@
-const MIN_NUMBER = 0;
-const MAX_NUMBER = 9;
+import { MIN_NUMBER, MAX_NUMBER } from '../data/constatns';
 
 const genRandomNumber = () => {
   return MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
