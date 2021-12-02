2eca5a49d4a0360f1603364c645241d49f72260e
moonheekim0118 <hellomooneekim@gmail.com>
2021-12-02 21:33:19 +0900

    feat: Component추상 클래스를 작성한다 - props key를 저장하여 부모 컴포넌트에서 해당 props가 업데이트 되었는지 확인 후 리렌더링

---
+++ src/core/Component.js
@@ -0,0 +1,55 @@
+export default class Component {
+  constructor(props = {}) {
+    this.container = null;
+    this.state = {};
+    this.props = props;
+    this.propsKeys = Object.keys(props);
+    this.childrens = [];
+    this.init();
+  }
+
+  init() {
+    this.initState();
+    this.initDoms();
+    this.bindEvents();
+    this.initChildrens();
+    this.render();
+  }
+
+  initState() {}
+
+  initDoms() {}
+
+  bindEvents() {}
+
+  initChildrens() {}
+
+  appendRootEvent(type, handler) {
+    this.container.addEventListener(type, handler);
+  }
+
+  setState(nextState) {
+    this.state = { ...this.state, ...nextState };
+    this.updateComponent();
+  }
+
+  setProps(nextProps) {
+    const newProps = {};
+    this.propsKeys.forEach(key => {
+      if (nextProps[key] && nextProps[key] !== this.props[key])
+        newProps[key] = nextProps[key];
+    });
+    if (Object.keys(newProps).length === 0) return;
+    this.props = { ...this.props, ...newProps };
+    this.render();
+  }
+
+  updateComponent() {
+    this.childrens.forEach(child => {
+      child.setProps(this.state);
+    });
+    this.render();
+  }
+
+  render() {}
+}
