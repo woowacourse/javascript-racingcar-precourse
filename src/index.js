b6c5713edd16ad8777f686bc33c03f11d1f4cf52
jswith <9geul@naver.com>
2021-12-02 21:14:42 +0900

    feat: 시도 횟수를 입력 받는 함수

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { isNonInput, isValidLength, genCarInstances } from './data/carNameInput.js';
+import { isNonCarNames, isValidLength, genCarInstances } from './data/carNameInput.js';
+import { isNonRacingCount, isNumber, isNegative } from './data/racingCountInput.js';
 import { $, $$ } from './utils/dom.js';
 
 $$('form').forEach((selector) =>
@@ -8,10 +9,19 @@ $$('form').forEach((selector) =>
 );
 
 $('#car-names-submit').addEventListener('click', function (e) {
-  if (isNonInput() || !isValidLength()) {
+  if (isNonCarNames() || !isValidLength()) {
     alert('자동차 이름을 5자 이하로 콤마로 구분하여 입력해주세요.');
     $('#car-names-input').focus();
     return;
   }
   const carNames = genCarInstances();
+});
+
+$('#racing-count-submit').addEventListener('click', function (e) {
+  if (isNonRacingCount() || !isNumber() || isNegative()) {
+    alert('1 이상의 정수를 입력해주세요.');
+    $('#racing-count-submit').focus();
+    return;
+  }
+  const attemptCounts = $('#racing-count-input').value;
 });
