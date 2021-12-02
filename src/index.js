d6bbe64591f6eedffb7fd1ed10beee3b96d1922c
jswith <9geul@naver.com>
2021-12-02 20:43:56 +0900

    feat: 자동차 이름을 쉼표로 구분하여 입력받는 함수

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import { $$ } from './utils/dom.js';
+import { isNonInput, isValidLength, genCarInstances } from './data/input.js';
+import { $, $$ } from './utils/dom.js';
 
 $$('form').forEach((selector) =>
   selector.addEventListener('submit', function (e) {
     e.preventDefault();
   })
 );
+
+$('#car-names-submit').addEventListener('click', function (e) {
+  if (isNonInput() || !isValidLength()) {
+    alert('자동차 이름을 5자 이하로 콤마로 구분하여 입력해주세요.');
+    $('#car-names-input').focus();
+    return;
+  }
+  const carNames = genCarInstances();
+});
