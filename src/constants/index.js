909a1bfd7215306776db356feace35b8972cbb9a
InSeongSo <goflvhxj2547@gmail.com>
2021-12-02 14:49:31 +0900

    ✒️Feat: 레이싱 횟수 입력 이벤트
    1. type=number인 input 태그에 입력되는 값을 focusout 시 검증하며 그 항목은 아래와 같다.
      - 0만 입력되었는지
      - 소수점이 입력되었는지
      - 음수가 입력되었는지 검사한다.
    2. 에러 메세지를 상수화하여 alert에 사용하고 잘못된 값을 입력했다면 해당 값을 지우고 focus한다.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,14 @@
-export const ERROR_MESSAGES = {
+export const NUMBER = Object.freeze({
+  ZERO: 0,
+});
+
+export const EMPTY = '';
+
+export const ERROR_MESSAGES = Object.freeze({
   blankError: '값을 입력해 주세요.',
   lengthError: '길이를 확인해 주세요.',
   dupError: '중복된 값이 존재합니다.',
-};
+  zeroError: '0이 아닌 양의 정수를 입력해주세요.',
+  negativeError: '음수가 아닌 양의 정수를 입력해주세요.',
+  decimalError: '소수가 아닌 양의 정수를 입력해주세요.',
+});
