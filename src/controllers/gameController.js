55d42d7040b114df6dc3a88d082c05480bd48148
2doo <2doo@kakao.com>
2021-12-02 22:33:27 +0900

    feat(gameController): 입력값 (cars, racingCount) 검증 클래스 메소드 구현

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -17,6 +17,21 @@ class GameController {
   get racingCount() {
     return this.#racingCount;
   }
+
+  static isValidCars(cars) {
+    const names = cars.map((car) => car.name);
+
+    const isValidLength = names.map(
+      (name) => name.length >= 1 && name.length < 10
+    );
+    const hasUniqueNames = names.length === new Set(names).size;
+    return isValidLength && hasUniqueNames;
+  }
+
+  static isValidRacingCount(racingCount) {
+    const isInValidRange = racingCount >= 1 && racingCount < 10;
+    return isInValidRange;
+  }
 }
 
 export default GameController;
