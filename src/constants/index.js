cb468fc8976896bd8cb4bfa853f5f3bbe3d6c70d
2doo <2doo@kakao.com>
2021-12-02 22:56:59 +0900

    refactor(constants): 입력값의 범위에 대한 constants 추가
    * isValidCars에서 자동차 이름 길이 체크를 잘못된 값으로 하던 문제를 수정

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const CAR_NAME_MAX_LENGTH = 5;
+
+export const RACING_COUNT_RANGE = {
+  min: 1,
+  max: 9,
+};
--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,3 +1,4 @@
+import { CAR_NAME_MAX_LENGTH, RACING_COUNT_RANGE } from '../constants/index.js';
 import Line from '../models/line.js';
 
 class GameController {
@@ -22,14 +23,17 @@ class GameController {
     const names = cars.map((car) => car.name);
 
     const isValidLength = names.map(
-      (name) => name.length >= 1 && name.length < 10
+      (name) => name.length > 0 && name.length <= CAR_NAME_MAX_LENGTH
     );
     const hasUniqueNames = names.length === new Set(names).size;
+
     return isValidLength && hasUniqueNames;
   }
 
   static isValidRacingCount(racingCount) {
-    const isInValidRange = racingCount >= 1 && racingCount < 10;
+    const isInValidRange =
+      racingCount >= RACING_COUNT_RANGE.min &&
+      racingCount <= RACING_COUNT_RANGE.max;
     return isInValidRange;
   }
 }
