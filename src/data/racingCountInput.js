d1818dfd6cf3f8d051d531f8e3a696ab2cf4a951
jswith <9geul@naver.com>
2021-12-02 21:12:29 +0900

    fix: import 오류 수정

--- src/data/racingCountInput.js
+++ src/data/racingCountInput.js
@@ -1,4 +1,4 @@
-import { $ } from '../utils/dom';
+import { $ } from '../utils/dom.js';
 
 export const isNonRacingCount = () => $('#racing-count-input').value == '';
 export const isNumber = () => !isNaN(Number($('#racing-count-input').value));
