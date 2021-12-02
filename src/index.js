4692b2cbc171a8021ae722a1d68424c9b7f2e334
marble <kangsw1025@naver.com>
2021-12-02 20:28:20 +0900

    feat: 시도할 횟수 입력받기 및 유효성 검증

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 const $car_names_input = document.getElementById("car-names-input");
 const $car_names_submit = document.getElementById("car-names-submit");
+const $racing_count_input = document.getElementById("racing-count-input");
+const $racing_count_submit = document.getElementById("racing-count-submit");
 let cars = [];
-
-const onClickSubmitCarNames = () => {
-  cars = [];
-  const carInputs = $car_names_input.value;
-  if (checkIsValidCarInputs(carInputs)) {
-    cars = carInputs.split(",");
-  }
-};
+let racingCount = [];
 
 const isCarInputsSplitBySpace = carInputs => {
   let isSplitBySpace = false;
@@ -43,4 +38,51 @@ const checkIsValidCarInputs = carInputs => {
   return isValid;
 };
 
+const checkIsInputCar = racingCountInput => {
+  let isInput = true;
+  if (racingCountInput === []) {
+    alert("자동차 이름을 입력 후 시도할 횟수를 입력해 주세요.");
+    isInput = false;
+  }
+
+  return isInput;
+};
+
+const checkIsIntergerInput = racingCountInput => {
+  let isInterger = true;
+  if (!Number.isInteger(parseInt(racingCountInput))) {
+    alert("시도할 횟수를 입력해 주세요.");
+    isInterger = false;
+  }
+
+  return isInterger;
+};
+
+const checkIsValidCountInput = racingCountInput => {
+  let isValid = true;
+  if (!checkIsInputCar(racingCountInput)) {
+    isValid = false;
+  } else if (!checkIsIntergerInput(racingCountInput)) {
+    isValid = false;
+  }
+
+  return isValid;
+};
+
+const onClickSubmitCarNames = () => {
+  cars = [];
+  const carInputs = $car_names_input.value;
+  if (checkIsValidCarInputs(carInputs)) {
+    cars = carInputs.split(",");
+  }
+};
+
+const onClickSubmitRacingCount = () => {
+  const racingCountInput = $racing_count_input.value;
+  if (checkIsValidCountInput(racingCountInput)) {
+    console.log("validInput");
+  }
+};
+
 $car_names_submit.addEventListener("click", onClickSubmitCarNames);
+$racing_count_submit.addEventListener("click", onClickSubmitRacingCount);
