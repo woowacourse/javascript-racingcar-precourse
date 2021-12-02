10fd6dd0c760fd66aabacd02ac9375c42bf7d459
2doo <2doo@kakao.com>
2021-12-03 01:32:11 +0900

    feat(utils/errorHandler): EMPTY_INPUT 에러 상황 추가

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,5 +1,6 @@
 import { CAR_NAME_MAX_LENGTH, RACING_COUNT_RANGE } from '../constants/index.js';
 
+export const EMPTY_INPUT = 'EMPTY_INPUT';
 export const INPUT_CAR_NAMES_BAD_LENGTH = 'INPUT_CAR_NAMES_BAD_LENGTH';
 export const INPUT_CAR_NAMES_HAS_DUPLICATES = 'INPUT_CAR_NAMES_HAS_DUPLICATES';
 export const INPUT_RACING_COUNT_VALID_NUMBER =
@@ -8,6 +9,7 @@ export const INPUT_RACING_COUNT_VALID_NUMBER =
 const DEFAULT_ERROR_MESSAGE = '알 수 없는 오류가 발생했습니다.';
 
 const errorMessagesRecord = {
+  [EMPTY_INPUT]: '값이 입력되지 않았습니다.',
   [INPUT_CAR_NAMES_BAD_LENGTH]: `자동차 이름은 ${CAR_NAME_MAX_LENGTH}자 이하로 입력해야 합니다.`,
   [INPUT_CAR_NAMES_HAS_DUPLICATES]: '중복된 자동차 이름이 존재합니다.',
   [INPUT_RACING_COUNT_VALID_NUMBER]: `시도할 횟수는 ${RACING_COUNT_RANGE.min} ~ ${RACING_COUNT_RANGE.max}회여야 합니다.`,
