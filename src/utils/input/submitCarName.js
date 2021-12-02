c30e3cf8e2d141db1b932401644adaabfafc1e97
JuYoung <ken44929@gmail.com>
2021-12-02 20:32:34 +0900

    feat: 자동차 이름 유효성 검사 기능 추가

--- src/utils/input/submitCarName.js
+++ src/utils/input/submitCarName.js
@@ -1,9 +1,16 @@
 import $ from '../common/selector.js';
+import { carNameValidation } from '../validation/carNameValidation.js';
 
 export const submitCarName = e => {
   e.preventDefault();
-  const carNames = $('car-names-input').value.split(',');
+  const carNames = $('#car-names-input').value.trim().split(',');
+  const { isError, inValidText } = carNameValidation(carNames);
 
-  console.log('carNames: ', carNames);
+  if (isError) {
+    alert(inValidText);
+    $('#car-names-input').value = '';
+    return;
+  }
+
   return carNames;
 };
---
+++ src/utils/validation/carNameValidation.js
@@ -0,0 +1,34 @@
+export const carNameValidation = carNames => {
+  const validation = {
+    isError: false,
+    inValidText: '',
+  };
+
+  carNames.map(carName => {
+    if (isNaN(Number(carName))) {
+      validation.isError = true;
+      validation.inValidText = '잘못된 자동차 이름이 있습니다.';
+      return validation;
+    }
+
+    if (!carName) {
+      validation.isError = true;
+      validation.inValidText = '자동차 이름에 공백이 있습니다.';
+      return validation;
+    }
+
+    if (carName.length > 5) {
+      validation.isError = true;
+      validation.inValidText = '자동차 이름이 5글자가 넘습니다.';
+      return validation;
+    }
+
+    if (carNames.indexOf(carName) !== carNames.lastIndexOf(carName)) {
+      validation.isError = true;
+      validation.inValidText = '중복된 자동차 이름이 있습니다.';
+      return validation;
+    }
+  });
+
+  return validation;
+};
