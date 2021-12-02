1b86635eeb6b9443829f952686f1ab2a04ba272c
jswith <9geul@naver.com>
2021-12-02 21:05:51 +0900

    feat: 시도할 횟수가 0인지 체크하는 함수

--- src/data/racingCountInput.js
+++ src/data/racingCountInput.js
@@ -3,3 +3,4 @@ import { $ } from '../utils/dom';
 export const isNonRacingCount = () => $('#racing-count-input').value == '';
 export const isNumber = () => !isNaN(Number($('#racing-count-input').value));
 export const isNegative = () => Number($('#racing-count-input').value) < 0;
+export const isZero = () => Number($('#racing-count-input')) === 0;
