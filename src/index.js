b3257fd8101e78e3c42fc8d139fbca3688ea09b2
DomMorello <zuzudnf@gmail.com>
2021-12-02 21:35:09 +0900

    refactor: gameEventHandler 함수 분리
    각 이벤트리스너에 onCarNameSubmit, onRepeatCountSubmit 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import Input from './input/Input.js';
-import playGame from './game/playGame.js';
+
+const ERROR_MESSAGE = '입력 오류! 공백을 포함하지 않은 5자 이하 이름만 입력해주세요!';
+
+function onCarNamesSubmit(event, userInput) {
+  event.preventDefault();
+  userInput.setCarNames(document.querySelector('#car-names-input').value.split(','));
+}
+
+function onRepeatCountSubmit(event, userInput) {
+  event.preventDefault();
+  userInput.setRepeatCount(document.querySelector('#racing-count-input').value);
+  // TODO: playGame
+}
 
 function gameEventHandler() {
   const $carNamesButton = document.querySelector('#car-names-submit');
@@ -7,13 +19,10 @@ function gameEventHandler() {
   const userInput = new Input();
 
   $carNamesButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    userInput.setCarNames(document.querySelector('#car-names-input').value);
+    onCarNamesSubmit(event, userInput);
   });
   $repeatCountButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    userInput.setRepeatCount(document.querySelector('#racing-count-input').value);
-    playGame(userInput);
+    onRepeatCountSubmit(event, userInput);
   });
 }
 
