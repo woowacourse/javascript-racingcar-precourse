e67c468243c0bf5a07dada91608f7549d775d032
hozzijeong <ghwls0905@naver.com>
2021-12-02 21:37:06 +0900

    Feat: 자동차 이름 조건 확인

--- src/car.js
+++ src/car.js
@@ -1,4 +1,21 @@
-export function Car(name) {
+// import { ERROR_MSG } from "./constant";
+
+function Car(name) {
     this.name = name;
     this.distance = 0;
+}
+
+export function CheckCarName(names) {
+    let isPass = true;
+    names.forEach((element) => {
+        if (element.length > 5 || element.length < 1) {
+            // alert(ERROR_MSG.over_range);
+            isPass = false;
+        } else if (element === "") {
+            // alert(ERROR_MSG.input_something);
+            isPass = false;
+        }
+    });
+
+    return isPass;
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import { Car } from "./car.js";
+// import { Car } from "./car.js";
 import { checkInputValidate } from "./input.js";
+import { CheckCarName } from "./car.js";
 
-export default function Play() {
+export default function SetGame() {
     const $racing_count_input = document.getElementById("racing-count-input");
     const $racing_count_btn = document.getElementById("racing-count-submit");
+
+    const $car_names_input = document.getElementById("car-names-input");
+    const $car_names_submit = document.getElementById("car-names-submit");
+
     this.game_information = {
         racing_count: 0,
         car_names: [],
@@ -11,10 +16,19 @@ export default function Play() {
     this.init = () => {
         $racing_count_btn.addEventListener("click", function (e) {
             e.preventDefault();
-            if (checkInputValidate($racing_count_input.value)) {
-                setRacingCount($racing_count_input.value);
-            }
+            const input_val = $racing_count_input.value;
+            if (checkInputValidate(input_val)) setRacingCount(input_val);
+            else alert();
         });
+
+        $car_names_submit.addEventListener("click", function (e) {
+            e.preventDefault();
+            const names = $car_names_input.value.split(",");
+            if (CheckCarName(names)) setCarNames(names);
+            else alert();
+        });
+        // 클릭되는 순간에 값이 나오기 때문에, 이게 어떻게 되는지 모르겠네..둘 중에 어느 버튼을 누르더라도,
+        // 조건이 확인된다면, 게임이 실행될 수 있게끔 설정해야함.
     };
 
     const getGameInformation = () => {
@@ -32,4 +46,4 @@ export default function Play() {
     this.init();
 }
 
-new Play();
+new SetGame();
