5d4691c020b4ce9a546ffa10019707e1a75b2778
KIM YEJI <rladpwl0512@gmail.com>
2021-12-02 23:31:57 +0900

    feat: 시도 횟수를 입력받는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import $ from './utils/utils.js';
 
 export default function RacingCarGame() {
-  const isOverFive = userInput => {
-    userInput = userInput.split(',').map(car => car.trim());
-    if (userInput.some(car => car.length > 5)) {
+  const isOverFive = carInput => {
+    carInput = carInput.split(',').map(car => car.trim());
+    if (carInput.some(car => car.length > 5)) {
       return true;
     }
     return false;
   };
 
-  const isDuplicate = userInput => {
-    userInput = userInput.split(',').map(car => car.trim());
-    if (userInput.length !== new Set(userInput).size) {
+  const isDuplicate = carInput => {
+    carInput = carInput.split(',').map(car => car.trim());
+    if (carInput.length !== new Set(carInput).size) {
       return true;
     }
     return false;
   };
 
-  const isValidInput = userInput => {
-    if (userInput.length === 0 || isOverFive(userInput) || isDuplicate(userInput)) {
+  const isValidCarInput = carInput => {
+    if (carInput.length === 0 || isOverFive(carInput) || isDuplicate(carInput)) {
       return false;
     }
     return true;
   };
 
-  const printError = userInput => {
-    if (userInput.length === 0) {
+  const printCarError = carInput => {
+    if (carInput.length === 0) {
       alert('빈값을 입력했습니다. 자동차 이름을 입력하세요.');
-    } else if (isOverFive(userInput)) {
+    } else if (isOverFive(carInput)) {
       alert('자동차 이름은 5글자를 초과할 수 없습니다.');
-    } else if (isDuplicate(userInput)) {
+    } else if (isDuplicate(carInput)) {
       alert('중복된 이름이 있습니다.');
     }
   };
@@ -38,15 +38,45 @@ export default function RacingCarGame() {
     $('#car-names-input').value = '';
   };
 
+  const isValidCountInput = countInput => {
+    if (countInput === '' || Number(countInput) < 1) {
+      return false;
+    }
+    return true;
+  };
+
+  const printCountError = countInput => {
+    if (countInput === '') {
+      alert('공백입니다. 숫자를 입력하세요.');
+    } else if (Number(countInput) < 1) {
+      alert('1이상의 수를 입력하세요.');
+    }
+  };
+
+  const resetCountInput = () => {
+    $('#racing-count-input').value = '';
+  };
+
   $('#car-names-form').addEventListener('submit', e => {
     e.preventDefault();
-    const userInput = $('#car-names-input').value;
-    if (isValidInput(userInput)) {
-      console.log('ok!');
+    const carInput = $('#car-names-input').value;
+    if (isValidCarInput(carInput)) {
+      console.log('ok');
       return;
     }
-    printError(userInput);
+    printCarError(carInput);
     resetCarInput();
+  });
+
+  $('#racing-count-form').addEventListener('submit', e => {
+    e.preventDefault();
+    const countInput = $('#racing-count-input').value;
+    if (isValidCountInput(countInput)) {
+      console.log('ok');
+      return;
+    }
+    printCountError(countInput);
+    resetCountInput();
   });
 }
 
