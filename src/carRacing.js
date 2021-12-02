d68043627b876fddb1c783e59baa7b988aafc277
mangming2 <leejiho35@gmail.com>
2021-12-02 23:57:21 +0900

    [Update]: carRacing
    carRacing에서 race, winner 출력하는 함수 모듈화

--- src/carRacing.js
+++ src/carRacing.js
@@ -2,18 +2,26 @@ import Car from './car.js';
 
 export default function carRacing(Carnames,count){ 
   const carList=Carnames.map(name=>new Car(name));
-  let carResult="";
+  let carResult='';
+
+  //race출력 함수
+  function getRaces(carList){
+  let raceResult="";
   for(let i=0;i<count;i++){
     carList.forEach(carName => {
       const randomNum=MissionUtils.Random.pickNumberInRange(1, 9);
       if(randomNum>=4){
         carName.count+=1;
       }
-      carResult+=`${carName.name}:${'-'.repeat(carName.count)}<br> `;
-      console.log(carResult);
+      raceResult+=`${carName.name}:${'-'.repeat(carName.count)}<br> `;
+      console.log(raceResult);
     });
-    carResult+=`<br>`
-  };
+    raceResult+=`<br>`
+   };
+   return raceResult;
+  }
+  
+  //winner 출력 함수
   function getWinner(cars){
     console.log(cars)
     let winnerList=[];
@@ -30,7 +38,10 @@ export default function carRacing(Carnames,count){
     const winners=winnerList.join(', ');
     return winners;
   }
+  
+  const race=getRaces(carList);
   const winner=getWinner(carList);
+  carResult+=race;
   carResult+=`<span>최종 우승자: </span>`;
   carResult+=`<span id="racing-winners">${winner}</span>`;
   return carResult;
