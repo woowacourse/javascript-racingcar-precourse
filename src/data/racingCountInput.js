f49ef0a84014f44cc8b3f764ff1c4a25aaef3cb1
jswith <9geul@naver.com>
2021-12-02 21:01:38 +0900

    feat: 시도할 횟수가 음수인지 체크하는 함수

--- src/data/racingCountInput.js
+++ src/data/racingCountInput.js
@@ -2,3 +2,4 @@ import { $ } from '../utils/dom';
 
 export const isNonInput = () => $('#racing-count-input').value == '';
 export const isNumber = () => !isNaN(Number($('#racing-count-input').value));
+export const isNegative = () => Number($('#racing-count-input').value) < 0;
