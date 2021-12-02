8d2df519c5b6c2dc4643880d5948692510b168a9
kkojae <jaejeung3210@gmail.com>
2021-12-02 19:56:44 +0900

    feat: 시도할 횟수가 빈 값인지 확인 기능
    - 시도할 횟수의 길이가 0인지 확인하는 기능 추가

--- src/events/RacingCountEvent.js
+++ src/events/RacingCountEvent.js
@@ -4,14 +4,27 @@ export default class RacingCountEvent {
   constructor() {
     this.$racingCountInput = DOM.racingCountInput;
     this.$racingCountSubmit = DOM.racingCountSubmit;
-    this.racingCount = 0;
+    this.racingCount = '';
   }
+
+  isEmpty = () => {
+    if (this.racingCount.length === 0) {
+      console.log('양의 정수를 입력해 주세요.');
+      return false;
+    }
+
+    return true;
+  };
+
+  validateCount = () => {
+    this.isEmpty();
+  };
 
   onClickSubmit = () => {
     this.$racingCountSubmit.addEventListener('click', (event) => {
       event.preventDefault();
       this.racingCount = this.$racingCountInput.value;
-      console.log(this.racingCount);
+      this.validateCount();
     });
   };
 }
