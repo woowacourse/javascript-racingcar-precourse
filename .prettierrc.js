3915aecdaef2108d3e8af08b3f8922b14a9dac47
Hoji <b4bynew@gmail.com>
2021-12-02 13:01:42 +0900

    chore: eslint, prettier 설정 추가

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+};
