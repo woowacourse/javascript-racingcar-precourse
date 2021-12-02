8e88ca9d83f64bd3fa4e6a544cf1fb0380e5b61a
moonheekim0118 <hellomooneekim@gmail.com>
2021-12-02 22:38:23 +0900

    fix: 자동차 이름 입력 검증을 추가한다. - 자동차 이름이 중복될 경우

--- src/utils/input.js
+++ src/utils/input.js
@@ -1,7 +1,12 @@
 export const parseCarNames = names => names.split(',').map(name => name.trim());
 
+export const hasDuplicatedName = names => {
+  const { length } = names;
+  return new Set(names).size < length;
+};
 export const isValidCarNames = names => {
   if (names.length === 0) return false;
+  if (hasDuplicatedName(names)) return false;
   for (const name of names) {
     const { length } = name;
     if (length === 0 || length > 5) return false;
