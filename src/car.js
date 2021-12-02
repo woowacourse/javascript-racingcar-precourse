c86d2d2733879d6ff22a8ea1242458433901a766
kevin7498 <kevin7498@naver.com>
2021-12-02 17:39:14 +0900

    feat: 전진 가능성을 검사하는 함수 'checkCanMove' 구현

--- src/car.js
+++ src/car.js
@@ -1,6 +1,6 @@
 export default class Car {
 
-    //차 이름과 횟수 받음
+    // 차 이름과 횟수 받음
     constructor(carName, racingCount) {
         this.carName = carName;
         this.racingCount = racingCount;
@@ -8,5 +8,23 @@ export default class Car {
 
     makeRandomNum() {
         return MissionUtils.Random.pickNumberInRange(0, 9);
+    }
+
+    // 각 회차마다 갈 수 있는지 없는지 저장
+    checkCanMove() {
+        const canMove = [];
+
+        for(let i = 0; i < this.racingCount; i++) {
+            const randomNum = this.makeRandomNum();
+
+            // 나중에 여기 함수로 분리
+            if(randomNum > 3) {
+                canMove.push(true);
+            } else {
+                canMove.push(false);
+            }
+        }
+
+        return canMove;
     }
 }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Car from "./car.js"
+
+const a = new Car("a", 3);
+console.log(a.checkCanMove());
