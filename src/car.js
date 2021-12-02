75b9bce8a1b8834a4a6184e9c89f0fd1a76480fe
jiyong LEE <jyman1995@gmail.com>
2021-12-02 17:57:12 +0900

    feat: Car 클래스 추가

---
+++ src/car.js
@@ -0,0 +1,20 @@
+export default class Car {
+  constructor(name) {
+    this.carName = name;
+    this.forward = 0;
+  }
+
+  getCarName() {
+    return this.carName;
+  }
+
+  getForward() {
+    return this.forward;
+  }
+
+  goAndStop() {
+    if (Math.floor(Math.random() * 10) >= 4) {
+      this.increase += 1;
+    }
+  }
+}
