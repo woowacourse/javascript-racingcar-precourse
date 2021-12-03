1ef9e7d0ada2e35da6f506828fbff1ab92bb1025
(yunjin) <sion11290805@gmail.com>
2021-12-03 13:33:04 +0900

    feat: 게임진행(게임결과 화면에)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import { gameStart } from './components/game.js';
 
 export function Car(userInput) {
   this.userInput = userInput;
+  const gameResultWrap = document.getElementById("racing-result");
+  const gameResultDiv = document.createElement('div');
+  const gameResultSpan = document.createElement('span');
+  gameResultSpan.setAttribute("id", "racing-winners")
   const gameResult = gameStart(userInput);
-
-  console.log(gameResult)
+  gameResultDiv.innerHTML = `${gameResult.gameProcess}\n 최중 우승자: ${gameResult.gmaeWinner} `;
+  gameResultSpan.textContent = gameResult.gmaeWinner;
+  gameResultWrap.appendChild(gameResultDiv)
 }
 
 formEvent()
