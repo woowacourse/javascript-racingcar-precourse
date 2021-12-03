3df8c45bb531a191a69d7bd296eb3acab271ee67
kevin7498 <kevin7498@naver.com>
2021-12-03 20:55:02 +0900

    refactor: 횟수를 검사하는 함수 'isValidCount'를 여러 함수로 분리

--- src/InputRacingCount.js
+++ src/InputRacingCount.js
@@ -19,13 +19,32 @@ export default class InputRacingCount {
         })
     }
 
-    isValidCount(inputCount) {
+    checkNumberForm(inputCount) {
         if(inputCount === "" || isNaN(inputCount)) {
             return false;
         }
-        
+
+        return true;
+    }
+
+    checkNumberSize(inputCount) {
         if(inputCount <= 0 || inputCount.indexOf('.') !== -1) {
             return false;
+        }
+
+        return true;
+    }
+
+    invalidCountAction() {
+        alert("유효하지 않은 입력입니다!");
+        this.$racingCountInput.value = "";
+
+        return false;
+    }
+
+    isValidCount(inputCount) {
+        if(!(this.checkNumberForm(inputCount) && this.checkNumberSize(inputCount))) {
+            this.invalidCountAction();
         }
 
         return true;
