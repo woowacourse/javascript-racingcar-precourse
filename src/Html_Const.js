86f77ac8a2415347c11e1358748bd6b842a502a0
mieumje <66072832+mieumje@users.noreply.github.com>
2021-12-03 20:22:38 +0900

    Feat : 자동차 이름 검증
    입력된 자동차 이름이 5자 이하인지 확인
    입력된 자동차 이름이 중복되지 않은지 확인

---
+++ src/Html_Const.js
@@ -0,0 +1,4 @@
+export const $Car_names_input = document.querySelector('#car-names-input');
+export const $Car_names_submit = document.querySelector('#car-names-submit');
+export const $Racing_count_input = document.querySelector('#racing-count-input');
+export const $Racing_count_submit = document.querySelector('#racing-count-submit');
