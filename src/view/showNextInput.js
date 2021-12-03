0e2562991415f9723638b885dbb8058bb54ede1a
DomMorello <zuzudnf@gmail.com>
2021-12-03 20:02:17 +0900

    style: EOL 추가

---
+++ src/view/showNextInput.js
@@ -0,0 +1,6 @@
+export default function showNextInput() {
+  const $racingCountHead = document.querySelector('#racing-count-head');
+  const $racingCountForm = document.querySelector('#racing-count-form');
+  $racingCountHead.style.display = 'block';
+  $racingCountForm.style.display = 'block';
+}
