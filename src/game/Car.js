f926d8925d83785286caa95b95b2cfdcd68bbebc
DomMorello <zuzudnf@gmail.com>
2021-12-03 20:25:50 +0900

    feat: 랜덤확률로 전진하여 position 저장하는 go 메서드 추가

--- src/game/Car.js
+++ src/game/Car.js
@@ -1,9 +1,16 @@
 export default class Car {
   constructor(name) {
     this.name = name;
+    this.position = 0;
   }
 
   getName() {
     return this.name;
+  }
+
+  go() {
+    if (window.MissionUtils.Random.pickNumberInRange(0, 9) > 4) {
+      this.position += 1;
+    }
   }
 }
