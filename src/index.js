a01087f5623c1a654b636c9d4a3664ad748db8f6
gytks4 <gytks4@naver.com>
2021-12-03 21:26:29 +0900

    feat: car 객체 move 함수 구현

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import RacingCarGame from './RacingCarGame.js';
 class Car {
   constructor(name) {
     this.name = name;
+    this.step = 0;
+  }
+  move() {
+    this.step += this.randomMove();
+  }
+  randomMove() {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      return 1;
+    }
+    return 0;
   }
 }
+
+export default Car;
 
 new RacingCarGame();
