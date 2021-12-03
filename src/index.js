49028fa20c985a525ccd29b1663a7bc501bccb6f
kamwoo <rkadndud06@naver.com>
2021-12-03 17:40:16 +0900

    feat: car names submit 버튼 액션 메소드 추가
    입력된 텍스트를 쉼표(,)단위로 분리
    차 이름 리스트의 유효성 검사 후 true일 경우 carList프로퍼티에 저장

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import ValidateUtils from './validateUtils.js';
+
 class RacingCarGame {
   constructor() {
     this.configureElementProperties();
@@ -11,10 +13,22 @@ class RacingCarGame {
     this.$racingCountInput = document.querySelector('#racing-count-input');
     this.$racingCountSubmit = document.querySelector('#racing-count-submit');
     this.$resultHeader = document.querySelector('#result-header');
+
+    this.$carNamesSubmit.addEventListener('click', this.onClickCarNamesSubmit);
   }
 
   configureInputProperties() {
     this.carList;
     this.raceNumber;
   }
+
+  onClickCarNamesSubmit = (event) => {
+    event.preventDefault();
+    const carList = this.$carNamesInput.value.split(',');
+    if (ValidateUtils.carNamesText(carList)) {
+      this.carList = carList;
+    }
+  };
 }
+
+new RacingCarGame();
