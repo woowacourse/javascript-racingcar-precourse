f16aa28a7d877a3e333e3824bb93954c2431257f
leegwae <leegwae@gmail.com>
2021-12-04 08:08:14 +0900

    fix: MissionUtils의 메서드 사용에 Random util 명시

--- src/game/utils/generateRandomNumber.js
+++ src/game/utils/generateRandomNumber.js
@@ -3,8 +3,9 @@ import { RANGE_RANDOM_NUMBER } from '../const.js';
 const { MissionUtils } = window;
 
 function generateRandomNumber() {
+  console.log(MissionUtils);
   const { min, max } = RANGE_RANDOM_NUMBER;
-  return MissionUtils.pickNumberInRange(min, max);
+  return MissionUtils.Random.pickNumberInRange(min, max);
 }
 
 export default generateRandomNumber;
