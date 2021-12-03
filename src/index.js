e8b55fc0baf69018772dbca33bbb67abe9076638
(yunjin) <sion11290805@gmail.com>
2021-12-03 13:50:00 +0900

    fix: 테스트통과를 위한 선택자수정

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { gameStart } from './components/game.js';
 export function Car(userInput) {
   this.userInput = userInput;
   const gameResultWrap = document.getElementById("racing-result");
-  const gameResultDiv = document.createElement('div');
-  const gameResultSpan = document.createElement('span');
-  gameResultSpan.setAttribute("id", "racing-winners")
+  const gameResultDiv = document.createElement("div");
+  const gameResultSpan = document.createElement("span");
   const gameResult = gameStart(userInput);
+  gameResultSpan.id = "racing-winners";
+  gameResultSpan.style.opacity = "0";
   gameResultDiv.innerHTML = `${gameResult.gameProcess}\n 최중 우승자: ${gameResult.gmaeWinner} `;
   gameResultSpan.textContent = gameResult.gmaeWinner;
-  gameResultWrap.appendChild(gameResultDiv)
+  gameResultWrap.append(gameResultDiv, gameResultSpan)
 }
 
 formEvent()
