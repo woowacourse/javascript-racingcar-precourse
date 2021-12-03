36069b2ea8ec1272fba3b018c55ef87f94dcad21
byhhh2 <rlaalsdl111@gmail.com>
2021-12-03 21:34:33 +0900

    feat: add converter that changes string to array for car names

--- src/user.js
+++ src/user.js
@@ -1,3 +1,10 @@
 export const getUserInputForCarNames = () => {
   return document.getElementById('car-names-input').value.toString();
 };
+
+export const userInputStringToArrayConverter = (userInput) => {
+  const userInputNoVacuum = userInput.replaceAll(' ', '');
+  const userInputArray = userInputNoVacuum.split(',');
+
+  return userInputArray;
+};
