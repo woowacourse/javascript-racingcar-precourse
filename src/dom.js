11778e6e79ff65f56a8818cd7145b806a519b545
byhhh2 <rlaalsdl111@gmail.com>
2021-12-03 21:44:14 +0900

    feat: add initial style

---
+++ src/dom.js
@@ -0,0 +1,9 @@
+export const setInitialStyle = () => {
+  const racingCountText = document.getElementById('racing-count-text');
+  const racingCountForm = document.getElementById('racing-count-form');
+  const racingResultText = document.getElementById('racing-result-text');
+
+  racingCountText.style.display = 'none';
+  racingCountForm.style.display = 'none';
+  racingResultText.style.display = 'none';
+};
