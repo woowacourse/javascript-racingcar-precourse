b76b9aa4e93ace37766eb64c54f891b44849a8b1
leegwae <leegwae@gmail.com>
2021-12-04 04:41:04 +0900

    refactor: 초기화부터 다시 진행하기 위해 구현한 action 삭제

--- src/dom/const.js
+++ src/dom/const.js
@@ -1,15 +1,19 @@
 export const ID_APP = 'app';
 
+const ID_FORM_CAR_NAMES = 'car-names-form';
 const ID_INPUT_CAR_NAMES = 'car-names-input';
-const ID_BUTTON_SUBMIT_CAR_NAMES = 'car-names-submit';
+const ID_BUTTON_CAR_NAMES = 'car-names-submit';
 export const FORM_CAR_NAMES = {
+  formId: ID_FORM_CAR_NAMES,
   inputId: ID_INPUT_CAR_NAMES,
-  buttonId: ID_BUTTON_SUBMIT_CAR_NAMES,
+  buttonId: ID_BUTTON_CAR_NAMES,
 };
 
+const ID_FORM_RACING_COUNT = 'racing-count-form';
 const ID_INPUT_RACING_COUNT = 'racing-count-input';
 const ID_BUTTON_SUBMIT_RACING_COUNT = 'racing-count-submit';
 export const FORM_RACING_COUNT = {
+  formId: ID_FORM_RACING_COUNT,
   inputId: ID_INPUT_RACING_COUNT,
   buttonId: ID_BUTTON_SUBMIT_RACING_COUNT,
 };
--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
-import Form from './form.js';
-import {
-  ID_APP,
-  FORM_CAR_NAMES,
-  FORM_RACING_COUNT,
-  ACTION_CHECK_CAR_NAMES,
-  ACTION_CHECK_RACING_COUNT,
-} from './dom/const.js';
-import Car from './car.js';
-import isValidCarNamesInput from './utils/isValidCarNamesInput.js';
-import isValidRacingCountInput from './utils/isValidRacingCountInput.js';
-import toCarNameList from './utils/toCarNameList.js';
+import { ID_APP } from './dom/const.js';
 
 export default class RacingGame {
   constructor() {
     this.app = document.getElementById(ID_APP);
-    this.carNamesForm = new Form(
-      FORM_CAR_NAMES.inputId,
-      FORM_CAR_NAMES.buttonId
-    );
-    this.racingCountForm = new Form(
-      FORM_RACING_COUNT.inputId,
-      FORM_RACING_COUNT.buttonId
-    );
 
     this.app.onclick = this.onClick.bind(this);
-    this.carNamesForm.setButtonAction(ACTION_CHECK_CAR_NAMES);
-    this.racingCountForm.setButtonAction(ACTION_CHECK_RACING_COUNT);
 
     this.cars = [];
     this.racingCount = 0;
-  }
-
-  [ACTION_CHECK_CAR_NAMES](e) {
-    e.preventDefault();
-
-    const userInput = this.carNamesForm.getInputValue();
-    if (!isValidCarNamesInput(userInput)) return;
-
-    const carNames = toCarNameList(userInput);
-    this.cars = Car.generateCars(carNames);
-  }
-
-  [ACTION_CHECK_RACING_COUNT](e) {
-    e.preventDefault();
-
-    const userInput = this.racingCountForm.getInputValue();
-    if (!isValidRacingCountInput(userInput)) return;
-
-    this.racingCount = Number(userInput);
   }
 
   onClick(event) {
