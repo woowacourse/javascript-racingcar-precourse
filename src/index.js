707d7f4708ff8a4f53fd7f5c3b68c7be28b7c772
leegwae <leegwae@gmail.com>
2021-12-04 02:16:11 +0900

    feat: add feature to check racing count

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import Form from './form.js';
-import { ID_APP, FORM_CAR_NAMES, ACTION_CHECK_CAR_NAMES } from './dom/const.js';
+import {
+  ID_APP,
+  FORM_CAR_NAMES,
+  FORM_RACING_COUNT,
+  ACTION_CHECK_CAR_NAMES,
+  ACTION_CHECK_RACING_COUNT,
+} from './dom/const.js';
 import Car from './car.js';
 import isValidCarNamesInput from './utils/isValidCarNamesInput.js';
+import isValidRacingCountInput from './utils/isValidRacingCountInput.js';
 import toCarNameList from './utils/toCarNameList.js';
 
 export default class RacingGame {
@@ -11,11 +18,17 @@ export default class RacingGame {
       FORM_CAR_NAMES.inputId,
       FORM_CAR_NAMES.buttonId
     );
+    this.racingCountForm = new Form(
+      FORM_RACING_COUNT.inputId,
+      FORM_RACING_COUNT.buttonId
+    );
 
     this.app.onclick = this.onClick.bind(this);
     this.carNamesForm.setButtonAction(ACTION_CHECK_CAR_NAMES);
+    this.racingCountForm.setButtonAction(ACTION_CHECK_RACING_COUNT);
 
     this.cars = [];
+    this.racingCount = 0;
   }
 
   [ACTION_CHECK_CAR_NAMES](e) {
@@ -26,6 +39,15 @@ export default class RacingGame {
 
     const carNames = toCarNameList(userInput);
     this.cars = Car.generateCars(carNames);
+  }
+
+  [ACTION_CHECK_RACING_COUNT](e) {
+    e.preventDefault();
+
+    const userInput = this.racingCountForm.getInputValue();
+    if (!isValidRacingCountInput(userInput)) return;
+
+    this.racingCount = Number(userInput);
   }
 
   onClick(event) {
