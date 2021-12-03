af5db8fcf8e2b89de621d5319cb90bd047b22d14
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-12-04 00:00:03 +0900

    feat: 자동차 이름이 겹쳤을 때 알림 구현

--- src/index.js
+++ src/index.js
@@ -15,10 +15,22 @@ class RacingCarGame {
     event.preventDefault();
     const carNames = $('#car-names-input').value;
     const splittedCarNames = carNames.split(',');
+    if (this.checkCarNameDuplicated(splittedCarNames)) {
+      alert('자동차 이름이 겹쳤습니다.');
+      return;
+    }
     splittedCarNames.forEach(carName => {
       this.cars.push(new Car(carName));
     });
     console.log(this.cars);
+  }
+
+  checkCarNameDuplicated(carNames) {
+    const set = new Set();
+    carNames.forEach(carName => {
+      set.add(carName);
+    });
+    return set.size !== carNames.length;
   }
 }
 
