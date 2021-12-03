2aca13f4687d2d52b87fead2f668b45645da5711
leegwae <leegwae@gmail.com>
2021-12-04 02:03:18 +0900

    feat: add const to save id selector

---
+++ src/dom/const.js
@@ -0,0 +1,20 @@
+export const ID_APP = 'app';
+
+const ID_INPUT_CAR_NAMES = 'car-names-input';
+const ID_BUTTON_SUBMIT_CAR_NAMES = 'car-names-submit';
+export const FORM_CAR_NAMES = {
+  inputId: ID_INPUT_CAR_NAMES,
+  buttonId: ID_BUTTON_SUBMIT_CAR_NAMES,
+};
+
+const ID_INPUT_RACING_COUNT = 'racing-count-input';
+const ID_BUTTON_SUBMIT_RACING_COUNT = 'racing-count-submit';
+export const FORM_RACING_COUNT = {
+  inputId: ID_INPUT_RACING_COUNT,
+  buttonId: ID_BUTTON_SUBMIT_RACING_COUNT,
+};
+
+export const ID_RACING_WINNERS = 'racing-winners';
+
+export const ACTION_CHECK_CAR_NAMES = 'checkCarNames';
+export const ACTION_CHECK_RACING_COUNT = 'checkRacingCount';
