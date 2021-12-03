b258335ec3be8b66e17ffad5c26cbd760bac9d0e
kevin7498 <kevin7498@naver.com>
2021-12-03 23:27:06 +0900

    feat: 자동차 이름 수만큼 자동차를 생성하는 함수 'makeCars' 구현

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,10 +1,23 @@
+import Car from "./car.js";
 export default class RacingGame {
     constructor(carNames, racingCount) {
         this.carNames = carNames;
         this.racingCount = racingCount;
+        this.cars = [];
 
-        console.log("게임 실행");
-        console.log(this.carNames);
-        console.log(this.racingCount);
+        this.makeCars(this.carNames, this.racingCount);
+
+        // console.log("게임 실행");
+        // console.log(this.carNames);
+        // console.log(this.racingCount);
     }
+
+    makeCars(carNames, racingCount) {
+        for(let i = 0; i < carNames.length; i++) {
+            const car = new Car(carNames[i], racingCount);
+            this.cars.push(car);
+        }
+    }
+
+
 }
