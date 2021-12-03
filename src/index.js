2253a97a81f2d1e9cee11576267738c9568cd321
2019015414 <2019015414@hanyang.ac.kr>
2021-12-04 03:59:36 +0900

    feat: add check function about count input
    index.js
    + onCountInputSubmit()
      : check count input when button is clicked

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ export default class RacingCarGame {
   constructor() {
     document.getElementById('car-names-submit').onclick =
       this.onCarNameInputSubmit.bind(this);
+    document.getElementById('racing-count-submit').onclick =
+      this.onCountInputSubmit.bind(this);
     this.formTag = document.getElementsByTagName('form');
     this.titleTag = document.getElementsByTagName('h4');
     this.makeHiddenCountAndResultSection();
@@ -25,10 +27,21 @@ export default class RacingCarGame {
     const inputCarNames = document.getElementById('car-names-input').value;
     this.carName = inputCarNames.split(',');
     if (!checkCarName(this.carName)) {
-      alert('올바르지 않은 입력입니다. 다시 입력해주세요.');
+      alert('자동차 이름 입력이 올바르지 않습니다. 다시 입력해주세요.');
       this.makeHiddenCountAndResultSection();
     } else {
       this.makeVisibleCountSection();
+    }
+  }
+
+  onCountInputSubmit(e) {
+    e.preventDefault();
+    const inputCountString =
+      document.getElementById('racing-count-input').value;
+    if (inputCountString.length === 0) {
+      alert('시도할 횟수 입력이 올바르지 않습니다. 숫자로 입력해주세요.');
+    } else {
+      const inputCount = Number(inputCountString);
     }
   }
 }
