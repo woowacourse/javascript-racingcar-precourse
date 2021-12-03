1e305db2e47ed3c8b42aa5d901ede75c18d310e9
byhhh2 <rlaalsdl111@gmail.com>
2021-12-03 21:40:33 +0900

    feat: add function getting car names array with exceptions handling

--- src/user.js
+++ src/user.js
@@ -1,3 +1,10 @@
+import {
+  userInputTypeException,
+  userInputLengthException,
+  userInputDuplicatedException,
+  carNameIsVacuumException,
+} from './exception.js';
+
 export const getUserInputForCarNames = () => {
   return document.getElementById('car-names-input').value.toString();
 };
@@ -5,6 +12,23 @@ export const getUserInputForCarNames = () => {
 export const userInputStringToArrayConverter = (userInput) => {
   const userInputNoVacuum = userInput.replaceAll(' ', '');
   const userInputArray = userInputNoVacuum.split(',');
+
+  return userInputArray;
+};
+
+export const getUserInputArray = (userInput) => {
+  const userInputArray = userInputStringToArrayConverter(userInput);
+  const hasExceptionArray = [];
+
+  if (userInputTypeException(userInput)) {
+    return [];
+  } else if (userInputLengthException(userInputArray)) {
+    return [];
+  } else if (userInputDuplicatedException(userInputArray)) {
+    return [];
+  } else if (carNameIsVacuumException(userInputArray)) {
+    return [];
+  }
 
   return userInputArray;
 };
