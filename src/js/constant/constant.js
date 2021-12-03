96f673bd4f3b3c98caf23de681c6bfc994b0b5c3
JihyeokUm <75592315+Jihyeok-Um@users.noreply.github.com>
2021-12-03 23:41:46 +0900

    feat: 자동차 이름이 잘못 입력 될 경우 alert 띄우기 구현

---
+++ src/js/constant/constant.js
@@ -0,0 +1,3 @@
+export const MIN_CARS_NUM = 2;
+export const MAX_CAR_NAMES_NUM = 5;
+export const MIN_CAR_NAMES_NUM = 1;
--- src/js/getCarName.js
+++ src/js/getCarName.js
@@ -1,6 +1,36 @@
 import { $ } from './util/dom.js';
+// prettier-ignore
+import { MAX_CAR_NAMES_NUM, MIN_CAR_NAMES_NUM, MIN_CARS_NUM } from './constant/constant.js';
 
 export const getCarNameInput = e => {
   e.preventDefault();
   const carNamesInput = $('#car-names-input').value.split(',');
+  if (
+    !checkCarNum(carNamesInput) ||
+    !checkCarNameLength(carNamesInput) ||
+    !checkOverlapCarName(carNamesInput)
+  ) {
+    window.alert('잘못된 값을 입력하셨습니다.');
+  } else {
+    $('#racing-count-title').style.display = '';
+    $('#racing-count-form').style.display = '';
+  }
+};
+
+const checkCarNum = carNamesInput => {
+  return carNamesInput.length >= MIN_CARS_NUM;
+};
+
+const checkCarNameLength = carNamesInput => {
+  let wrongCarNames = carNamesInput.filter(
+    x => x.length < MIN_CAR_NAMES_NUM || x.length > MAX_CAR_NAMES_NUM,
+  );
+  return wrongCarNames.length === 0;
+};
+
+const checkOverlapCarName = carNamesInput => {
+  const isOverlap = carNamesInput.some(function (x) {
+    return !(carNamesInput.indexOf(x) !== carNamesInput.lastIndexOf(x));
+  });
+  return isOverlap;
 };
