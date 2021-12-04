3017d9585f27fa03a77b66a9448b9a110ab5e8fc
DomMorello <zuzudnf@gmail.com>
2021-12-04 23:08:28 +0900

    feat: 반복횟수만큼 차 전진여부 결정 후 결과저장

--- src/game/playGame.js
+++ src/game/playGame.js
@@ -2,13 +2,20 @@ import Car from './Car.js';
 
 function getCars(userInput) {
   const cars = [];
-
   userInput.getCarNames().forEach((name) => {
     cars.push(new Car(name));
   });
   return cars;
 }
 
+function goOnce(cars) {
+  cars.forEach((car) => car.go());
+}
+
 export default function playGame(userInput) {
   const cars = getCars(userInput);
+
+  for (let index = 0; index < userInput.getRepeatCount(); index += 1) {
+    goOnce(cars);
+  }
 }
