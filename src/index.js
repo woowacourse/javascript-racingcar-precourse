6e3c403ef1b2e4d027d4aaeef100bc4ac4e54bf8
2doo <2doo@kakao.com>
2021-12-04 17:42:09 +0900

    feat(index): 우승자들 결과 출력 메소드 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import { ELEMENT_ID } from './constants/index.js';
+import DOMElement from './views/domElement.js';
+
+const $carNamesInput = DOMElement.createById(ELEMENT_ID.CAR_NAMES_INPUT);
+const $carNamesSubmit = DOMElement.createById(ELEMENT_ID.CAR_NAMES_SUBMIT);
+const $racingCountInput = DOMElement.createById(ELEMENT_ID.RACING_COUNT_INPUT);
+const $racingCountSubmit = DOMElement.createById(
+  ELEMENT_ID.RACING_COUNT_SUBMIT
+);
+const $racingResult = DOMElement.createById(ELEMENT_ID.RACING_RESULT);
+
+const renderWinnerNames = (winnerNames) => {
+  const $winnerWrapper = DOMElement.createByTagName('div');
+  $winnerWrapper.setText('최종 우승자: ');
+
+  const $winner = DOMElement.createByTagName('span');
+  $winner.setId(ELEMENT_ID.RACING_WINNERS);
+  $winner.setText(winnerNames.join(','));
+
+  $winnerWrapper.appendChild($winner);
+  $racingResult.appendChild($winnerWrapper);
+};
+
 const main = () => {
   // TO BE IMPLEMENTED
 };
