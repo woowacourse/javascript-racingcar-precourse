af6b13e25ddfa4a29ffb8d7f161b3e1a7c706deb
moonheekim0118 <hellomooneekim@gmail.com>
2021-12-04 19:36:10 +0900

    refactor: 자동차 이름 검증 함수를 분리한다.

--- src/utils/input.js
+++ src/utils/input.js
@@ -1,16 +1,17 @@
+import { RULE } from './constants.js';
+
 export const parseCarNames = names => names.split(',').map(name => name.trim());
 
 export const hasDuplicatedName = names => new Set(names).size < names.length;
 
-export const isValidCarNames = names => {
-  if (names.length === 0) return false;
-  if (hasDuplicatedName(names)) return false;
-  for (const name of names) {
+export const hasValidNamesLength = names =>
+  names.filter(name => {
     const { length } = name;
-    if (length === 0 || length > 5) return false;
-  }
-  return true;
-};
+    return length >= RULE.MIN_NAME_LENGTH && length <= RULE.MAX_NAME_LENGTH;
+  }).length === names.length;
+
+export const isValidCarNames = names =>
+  names.length > 0 && !hasDuplicatedName(names) && hasValidNamesLength(names);
 
 export const parseRacingCount = count => +count.trim();
 
