5f70dec4e545f3b795d023fc932f3ce07f8e48a6
InKyoJeong <viapolar@naver.com>
2021-12-04 16:13:51 +0900

    feat: add alert to count input

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,4 +16,5 @@ export const ERROR = {
   NAME_LENGTH_IS_LONG: `${NUM.MAX_NAME_LENGTH}자를 초과한 이름이 존재합니다.`,
   NAME_IS_DUPLICATED: '중복된 이름이 존재합니다.',
   NAME_IS_EMPTY: '빈값인 이름이 존재합니다.',
+  COUNT_IS_NOT_CORRECT: '시도 횟수를 올바르게 입력해주세요.',
 };
