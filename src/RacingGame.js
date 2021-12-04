f808e04f46e5b243d416e469ca434f9dfcc84c3f
SoYi Jeon <soyijeon47@gmail.com>
2021-12-04 20:15:37 +0900

    refactor: RacingGame 일부 메소드 이름 변경 및 작성 순서 변경

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -17,41 +17,9 @@ export default class RacingGame {
     this.count = count;
   }
 
-  resetResult() {
-    this.render.resetResult();
-  }
-
-  isReadyForGame() {
-    return this.carNames.length !== 0 && this.count !== 0;
-  }
-
-  play() {
-    this.cars = this.carNames.map((name) => new Car(name));
-
-    this.render.showResultTitle();
-    for (let round = 0; round < this.count; round += 1) {
-      this.racing(round);
-    }
-
-    const winnerNames = this.getWinnerNames();
-    this.render.winners(winnerNames);
-  }
-
-  racing(round) {
-    this.cars.forEach((car) => car.play());
-    const states = this.cars.map((car) => car.state());
-    this.render.racingState(round, states);
-  }
-
-  getWinnerNames() {
-    const maxStep = Math.max(...this.cars.map((car) => car.step));
-    const winners = this.cars.filter((car) => car.step === maxStep);
-    return winners.map((winner) => winner.name);
-  }
-
   onSubmitCarNames(input) {
     const carNames = input.value.split(',');
-    this.resetResult();
+    this.render.resetResult();
 
     if (!isValidCarNames(carNames)) {
       this.setCarNames([]);
@@ -64,7 +32,7 @@ export default class RacingGame {
 
   onSubmitRacingCount(input) {
     const racingCount = Number(input.value);
-    this.resetResult();
+    this.render.resetResult();
 
     if (!isValidRacingCount(racingCount)) {
       this.setCount(0);
@@ -73,5 +41,33 @@ export default class RacingGame {
 
     this.setCount(racingCount);
     if (this.isReadyForGame()) this.play();
+  }
+
+  isReadyForGame() {
+    return this.carNames.length !== 0 && this.count !== 0;
+  }
+
+  play() {
+    this.cars = this.carNames.map((name) => new Car(name));
+
+    this.render.showResultTitle();
+    for (let round = 0; round < this.count; round += 1) {
+      this.runRound(round);
+    }
+
+    const winnerNames = this.getWinnerNames();
+    this.render.winners(winnerNames);
+  }
+
+  runRound(round) {
+    this.cars.forEach((car) => car.play());
+    const states = this.cars.map((car) => car.state());
+    this.render.racingState(round, states);
+  }
+
+  getWinnerNames() {
+    const maxStep = Math.max(...this.cars.map((car) => car.step));
+    const winners = this.cars.filter((car) => car.step === maxStep);
+    return winners.map((winner) => winner.name);
   }
 }
