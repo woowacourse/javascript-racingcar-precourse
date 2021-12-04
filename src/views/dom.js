34925a3c856728e682835ada6cf3c1bbdaa24458
2doo <2doo@kakao.com>
2021-12-04 17:04:03 +0900

    fix(index.html): HTML에 racing-result id를 추가

--- src/views/dom.js
+++ src/views/dom.js
@@ -2,4 +2,5 @@ const $carNamesInput = document.getElementById('car-names-input');
 const $carNamesSubmit = document.getElementById('car-names-submit');
 const $racingCountInput = document.getElementById('racing-count-input');
 const $racingCountSubmit = document.getElementById('racing-count-submit');
+const $racingResult = document.getElementById('racing-result');
 const $racingWinners = document.getElementById('racing-winners');
