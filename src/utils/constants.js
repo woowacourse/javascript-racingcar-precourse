187e071f3f7d4631e6dba04ccccf5929022d3ab5
moonyerim2 <moonyerim2@gmail.com>
2021-12-05 02:13:18 +0900

    feat: 랜덤 숫자를 생성하는 유틸 함수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,14 @@
 export const ERR_MESSAGE = {
   EMPTY_INPUT: '자동차 이름을 입력해 주세요.',
-  LESS_INPUT: '자동차 이름은 2개 이상 입력해주세요.',
-  OVER_MAX_LENGTH: '자동자 이름은 5자 이하로 입력해주세요.',
+  LESS_INPUT: '자동차 이름은 2개 이상 입력해 주세요.',
+  OVER_MAX_LENGTH: '자동자 이름은 5자 이하로 입력해 주세요.',
   HAS_EMPTY_NAME: '공백은 자동차 이름으로 사용할 수 없습니다.',
-  DUPLICATED_NAME: '중복된 이름은 사용할 수 없습니다.'
+  DUPLICATED_NAME: '중복된 이름은 사용할 수 없습니다.',
 };
 
 export const CONSTRAINTS = {
   MAX_NAME_LENGTH: 5,
   MIN_CAR_NUMBER: 2,
-}
+  MIN_OF_RANGE: 0,
+  MAX_OF_RANGH: 9,
+};
---
+++ src/utils/getRandomNum.js
@@ -0,0 +1,3 @@
+export const getRandomNum = (min, max) => {
+  return MissionUtils.Random.pickNumberInRange(min, max);
+};
