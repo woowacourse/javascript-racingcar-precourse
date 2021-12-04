3a42e153ac1c4c79388180cb32119f2eddb59e3e
InKyoJeong <viapolar@naver.com>
2021-12-04 15:59:24 +0900

    feat: add tag id

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const ID = {
+  CAR_NAMES_INPUT: 'car-names-input',
+  CAR_NAMES_SUBMIT: 'car-names-submit',
+  RACING_COUNT_INPUT: 'racing-count-input',
+  RACING_COUNT_SUBMIT: 'racing-count-submit',
+  RACING_WINNERS: 'racing-winners',
+};
