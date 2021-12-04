2dd7492f44d0b474049a02d6e5972eb2a5c80f97
InKyoJeong <viapolar@naver.com>
2021-12-04 16:30:50 +0900

    feat: print advance results

--- src/components/Result.js
+++ src/components/Result.js
@@ -11,7 +11,15 @@ class GameResult {
   printResults(count) {
     while (count) {
       const advanceResults = this.game.advanceCar();
-      console.log(advanceResults);
+      advanceResults.forEach(({ name, distance }) => {
+        const carContainer = document.createElement('div');
+        const carText = document.createElement('span');
+        carText.innerText = `${name}: ${distance}`;
+        carContainer.append(carText);
+        this.$resultContainer.append(carContainer);
+      });
+      const br = document.createElement('br');
+      this.$resultContainer.append(br);
       count--;
     }
   }
