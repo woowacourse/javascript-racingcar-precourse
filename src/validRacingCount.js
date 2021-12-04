a29402cefbcdb9f736a41eed91ed0c8d643789d8
prefer2 <sunho6200@gmail.com>
2021-12-04 22:30:58 +0900

    fix: 레이싱 횟수 유효 조건 추가(정수값)

--- src/validRacingCount.js
+++ src/validRacingCount.js
@@ -3,9 +3,14 @@ import { NOT_VALID_RACING_INPUT } from './constants/errorMessage.js';
 
 const racingCountStartZero = racingCount => racingCount[0] === '0';
 const racingCountUnderOne = racingCount => +racingCount < 1;
+const racingCountInteger = racingCount => Number.isInteger(+racingCount);
 
 export default function validRacingCount(racingCount) {
-  if (racingCountStartZero(racingCount) || racingCountUnderOne(racingCount)) {
+  if (
+    racingCountStartZero(racingCount) ||
+    racingCountUnderOne(racingCount) ||
+    !racingCountInteger(racingCount)
+  ) {
     alert(NOT_VALID_RACING_INPUT);
     $racingCountInput.value = '';
     $racingCountInput.focus();
