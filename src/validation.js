f7fb24a1b90cba2c04dd153d51c63d887a6242d0
ellynhan <wown252@naver.com>
2021-12-04 14:57:24 +0900

    feat: 자동차 이름 중복되거나 빈문자일 경우 예외처리

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,6 @@
-const checkValidNameLength = (names) => {
-  return names.every(
-    (name) => name.length <= 5
-  );
-}
-
-export const checkValidation = (names) => {
-  return checkValidNameLength(names);
-}
+const checkValidNameLength = (names) => names.every((name) => name.length <= 5);
+const checkBlankName = (names) => names.every((name) => name !== '');
+const checkDuplicatedName = (names) => {
+  const set = new Set(names);
+  return set.size === names.length;
+};
