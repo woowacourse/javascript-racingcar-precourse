d17c8df7c3db1bc2bc43355818d6170581d0d040
SoYi Jeon <soyijeon47@gmail.com>
2021-12-04 19:57:36 +0900

    refactor: 에러 메시지 템플릿 리터럴로 변경

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,13 +10,13 @@ const MAX_LENGTH_OF_CAR_NAME = 5;
 const MIN_OF_RACING_COUNT = 1;
 
 const ERROR_MESSAGE = {
-  NUMBER_OF_CAR: '자동차는 2대 이상이어야 합니다.',
+  NUMBER_OF_CAR: `자동차는 ${MIN_NUMBER_OF_CARS}대 이상이어야 합니다.`,
   DUPLICATE_CAR_NAME: '중복되는 자동차 이름이 있습니다.',
-  LENGTH_OF_CAR_NAME: '자동차 이름은 1자 이상, 5자 이하만 가능합니다.',
+  LENGTH_OF_CAR_NAME: `자동차 이름은 ${MIN_LENGTH_OF_CAR_NAME}자 이상, ${MAX_LENGTH_OF_CAR_NAME}자 이하만 가능합니다.`,
   WHITESPACE_IN_CAR_NAME: '자동차 이름에는 공백이 포함될 수 없습니다.',
 
   RACING_COUNT_MUST_BE_INTEGER: '레이싱 횟수 입력값은 정수여야 합니다.',
-  MIN_OF_RACING_COUNT: '레이싱 횟수는 1번 이상이어야 합니다.',
+  MIN_OF_RACING_COUNT: `레이싱 횟수는 ${MIN_OF_RACING_COUNT}번 이상이어야 합니다.`,
 };
 
 export {
