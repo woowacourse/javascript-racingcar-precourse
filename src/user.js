9f4c4f3873fa0b194bdd95d160ea0f60529e938b
byhhh2 <rlaalsdl111@gmail.com>
2021-12-04 18:08:25 +0900

    refeator: delete useless export statement

--- src/user.js
+++ src/user.js
@@ -9,7 +9,7 @@ export const getUserInputForCarNames = () => {
   return document.getElementById('car-names-input').value.toString();
 };
 
-export const userInputStringToArrayConverter = (userInput) => {
+const userInputStringToArrayConverter = (userInput) => {
   const userInputNoVacuum = userInput.replaceAll(' ', '');
   const userInputArray = userInputNoVacuum.split(',');
 
