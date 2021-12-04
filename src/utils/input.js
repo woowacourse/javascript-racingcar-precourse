50d4f16538df2f4eeadc4cd3a627cebabe426745
moonheekim0118 <hellomooneekim@gmail.com>
2021-12-04 19:37:26 +0900

    refactor: 불필요한 export를 제거한다.

--- src/utils/input.js
+++ src/utils/input.js
@@ -2,9 +2,9 @@ import { RULE } from './constants.js';
 
 export const parseCarNames = names => names.split(',').map(name => name.trim());
 
-export const hasDuplicatedName = names => new Set(names).size < names.length;
+const hasDuplicatedName = names => new Set(names).size < names.length;
 
-export const hasValidNamesLength = names =>
+const hasValidNamesLength = names =>
   names.filter(name => {
     const { length } = name;
     return length >= RULE.MIN_NAME_LENGTH && length <= RULE.MAX_NAME_LENGTH;
