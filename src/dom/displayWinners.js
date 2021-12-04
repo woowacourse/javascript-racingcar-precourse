09f718ab9da6521dd4830a94e88228dc6723fa43
leegwae <leegwae@gmail.com>
2021-12-04 09:42:40 +0900

    fix: 최종 우승자 출력 결과 수정

--- src/dom/displayWinners.js
+++ src/dom/displayWinners.js
@@ -2,11 +2,15 @@ import { ID_RACING_WINNERS, TEXT_WINNERS } from './const.js';
 import { DIVIDER } from '../game/const.js';
 
 function displayWinners(parent, winners) {
+  const div = document.createElement('div');
+  const text = document.createTextNode(`${TEXT_WINNERS}: `);
   const span = document.createElement('span');
   span.id = ID_RACING_WINNERS;
-  span.innerText = `${TEXT_WINNERS}: ${winners.join(DIVIDER)}`;
+  span.innerText = winners.join(DIVIDER);
 
-  parent.appendChild(span);
+  div.append(text);
+  div.append(span);
+  parent.appendChild(div);
 }
 
 export default displayWinners;
