a18420a0d01371a103c05f84fe6658ebd8726871
DomMorello <zuzudnf@gmail.com>
2021-12-04 23:19:19 +0900

    feat: 게임 재시작 대비 입력 들어오면 이전 결과 삭제

--- src/view/initView.js
+++ src/view/initView.js
@@ -1,7 +1,17 @@
+function resetView($appDiv) {
+  const $resultContainer = document.querySelector('#result-container');
+
+  if ($resultContainer) {
+    $appDiv.removeChild($resultContainer);
+  }
+}
+
 export default function initView() {
   const $newResultContainer = document.createElement('div');
   const $appDiv = document.querySelector('#app');
+  resetView($appDiv);
 
+  $newResultContainer.innerHTML = '';
   $newResultContainer.id = 'result-container';
   $appDiv.append($newResultContainer);
 }
