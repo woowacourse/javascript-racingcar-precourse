3f0d37bec02c5aae9da6ba0d96a8b70d9a067c6f
2doo <2doo@kakao.com>
2021-12-04 22:22:29 +0900

    docs(README): carName에 trim 적용해서 공백 제거

--- src/controllers/inputParser.js
+++ src/controllers/inputParser.js
@@ -1,6 +1,6 @@
 import Car from '../models/car.js';
 
 export const parseCars = (text) =>
-  text.split(',').map((carName) => new Car(carName));
+  text.split(',').map((carName) => new Car(carName.trim()));
 
 export const parseRacingCount = (text) => Number(text);
