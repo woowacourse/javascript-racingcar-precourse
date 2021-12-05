eca16513878488a0fbc8cc79d6df205225a3ed44
kimsanglok <evertree6031@gmail.com>
2021-12-05 23:37:11 +0900

    Feat: 자동차이름 입력값 - 5자 이하인지 검사하는 함수 추가

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,9 +2,18 @@ const isEmpty = (carNamesInput) => {
   return carNamesInput === '';
 };
 
+const isLongerThanFiveLetters = (carNamesInput) => {
+  return carNamesInput.split(',').some((carName) => carName.length > 5);
+};
+
 const isValidate = (carNamesInput) => {
   if (isEmpty(carNamesInput)) {
     alert('자동차 이름을 입력해주세요.  올바른 예) east,west,south,north');
+    return false;
+  }
+
+  if (isLongerThanFiveLetters(carNamesInput)) {
+    alert('자동차 이름은 5자 이하로 입력해주세요.  올바른 예) east,west,south,north');
     return false;
   }
 
