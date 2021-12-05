a484989d3f2c7e6a0d0ae4ccbf1ae4f0a37db6fe
sanaandmomo <qajjang46@naver.com>
2021-12-05 09:25:32 +0900

    chore: jest 설정 추가해라

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
         browser: true,
         es6: true,
         node: true,
+        jest: true,
     },
     extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
     rules: {
