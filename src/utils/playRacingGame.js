776d850b5f1514a00538c8df3de81f94cc9baeb6
sohyeonyun <wja06018@naver.com>
2021-12-06 02:58:00 +0900

    feat: 게임 진행

--- src/utils/playRacingGame.js
+++ src/utils/playRacingGame.js
@@ -1,3 +1,36 @@
+import { $ } from './selector.js';
+
 export const playRacingGame = (cars, racingCount) => {
-  console.log(cars, racingCount);
+  for (let i = 0; i < racingCount; i++) {
+    cars = startRound(cars);
+    showGameResult(cars);
+  }
+  getWinner(cars);
 };
+
+const startRound = cars => {
+  cars.forEach(car => {
+    if (isMove()) {
+      car.point++;
+    }
+  });
+  return cars;
+};
+
+const isMove = () => {
+  const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+  if (randomNumber >= 4) {
+    return true;
+  }
+  return false;
+};
+
+const showGameResult = cars => {
+  cars.forEach(car => {
+    const item = `${car.name}: ${'-'.repeat(car.point)} <br/>`;
+    $('#racing-round-result').innerHTML += item;
+  });
+  $('#racing-round-result').innerHTML += '<br/>';
+};
+
+const getWinner = cars => {};
