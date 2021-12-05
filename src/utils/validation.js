b790fb3c95bea3b48b111de919c3252096e1e750
ziho <ziho@kakao.com>
2021-12-05 22:19:00 +0900

    feat: input 검증 함수 구현

---
+++ src/utils/validation.js
@@ -0,0 +1,31 @@
+import {ERROR_MESSAGE, NUM_RULE} from './constants.js';
+import {showErrorMessage} from './domUtil.js';
+
+const isEmptyInput = (nameArr) => nameArr.length < 1;
+
+const isValidCarNameLength = (nameArr) => {
+  return nameArr.every((name) => name.length <= NUM_RULE.NAME_LIMIT);
+};
+
+const isDuplicatedNames = (nameArr) => {
+  const nameSet = new Set(nameArr);
+
+  return nameArr.length !== nameSet.size;
+};
+
+export const isVaildNamesInput = ($target, names) => {
+  if (isEmptyInput(names)) {
+    showErrorMessage($target, ERROR_MESSAGE.EMPTY_INPUT);
+    return false;
+  }
+  if (isDuplicatedNames(names)) {
+    showErrorMessage($target, ERROR_MESSAGE.DUPLICATED_NAME);
+    return false;
+  }
+  if (!isValidCarNameLength(names)) {
+    showErrorMessage($target, ERROR_MESSAGE.OVERLENGTH_NAME);
+    return false;
+  }
+
+  return true;
+};
