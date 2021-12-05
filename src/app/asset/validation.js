d0b0c13f766a7daf146141a9df4cc5736d10ec0b
sanaandmomo <qajjang46@naver.com>
2021-12-05 11:31:49 +0900

    fix: 자동차 이름값 전체 검사 버그 수정해라

--- src/app/asset/validation.js
+++ src/app/asset/validation.js
@@ -1,18 +1,18 @@
 // import { CAR_NAMES_INPUT_MSG } from './constant.js';
-const CAR_NAMES_INPUT_MSG = require('./constant');
+// const CAR_NAMES_INPUT_MSG = require('./constant');
 
 const checkCarNameFunctions = [
     (carNames) => {
-        if (carNames.every((carName) => carName.length < 1 || carName.length > 5)) {
-            alert(CAR_NAMES_INPUT_MSG.length);
+        if (carNames.some((carName) => carName.length < 1 || carName.length > 5)) {
+            // alert(CAR_NAMES_INPUT_MSG.length);
             return false;
         }
 
         return true;
     },
     (carNames) => {
-        if (carNames.every((carName) => carName.length !== carName.replace(/\s/g, '').length)) {
-            alert(CAR_NAMES_INPUT_MSG.space);
+        if (carNames.some((carName) => carName.length !== carName.replace(/\s/g, '').length)) {
+            // alert(CAR_NAMES_INPUT_MSG.space);
             return false;
         }
 
