abb3d281c4ec0da908161ae22c52fcfa3b9837cf
KIM YEJI <rladpwl0512@gmail.com>
2021-12-05 23:02:42 +0900

    refactor: alertCarInputError 함수에서 if-else if문 삭제

--- src/js/handlers/handleCarNameInput.js
+++ src/js/handlers/handleCarNameInput.js
@@ -1,13 +1,37 @@
 import $ from '../utils/dom.js';
 import renderCountInput from '../views/renderCountInput.js';
-import { isOverFive, isDuplicate, alertCarInputError } from '../views/alertInputError.js';
+import { alertCarInputError } from '../views/alertInputError.js';
 import { resetCarInput } from '../views/resetInput.js';
 import { disableCarInput } from '../views/disableInput.js';
-import { CAR } from '../utils/constants.js';
+import { CAR, ERROR } from '../utils/constants.js';
+
+export const isOverFive = carInput => {
+  carInput = carInput.split(',').map(car => car.trim());
+  if (carInput.some(car => car.length > CAR.MAX_LENGTH)) {
+    return true;
+  }
+  return false;
+};
+
+export const isDuplicate = carInput => {
+  carInput = carInput.split(',').map(car => car.trim());
+  if (carInput.length !== new Set(carInput).size) {
+    return true;
+  }
+  return false;
+};
 
 const isValidCarInput = carInput => {
-  if (carInput.length < CAR.MIN_INPUT || isOverFive(carInput) || isDuplicate(carInput)) {
-    alertCarInputError(carInput);
+  if (carInput.length < CAR.MIN_INPUT) {
+    alertCarInputError(ERROR.INPUT_BLANK);
+    return false;
+  }
+  if (isOverFive(carInput)) {
+    alertCarInputError(ERROR.OVER_MAX_LENGTH);
+    return false;
+  }
+  if (isDuplicate(carInput)) {
+    alertCarInputError(ERROR.NAME_DUPLICATE);
     return false;
   }
   return true;
--- src/js/views/alertInputError.js
+++ src/js/views/alertInputError.js
@@ -1,29 +1,7 @@
-import { CAR, COUNT, ERROR } from '../utils/constants.js';
+import { COUNT, ERROR } from '../utils/constants.js';
 
-export const isOverFive = carInput => {
-  carInput = carInput.split(',').map(car => car.trim());
-  if (carInput.some(car => car.length > CAR.MAX_LENGTH)) {
-    return true;
-  }
-  return false;
-};
-
-export const isDuplicate = carInput => {
-  carInput = carInput.split(',').map(car => car.trim());
-  if (carInput.length !== new Set(carInput).size) {
-    return true;
-  }
-  return false;
-};
-
-export const alertCarInputError = carInput => {
-  if (carInput.length < CAR.MIN_INPUT) {
-    alert(ERROR.INPUT_BLANK);
-  } else if (isOverFive(carInput)) {
-    alert(ERROR.OVER_MAX_LENGTH);
-  } else if (isDuplicate(carInput)) {
-    alert(ERROR.NAME_DUPLICATE);
-  }
+export const alertCarInputError = ERROR_MSG => {
+  alert(ERROR_MSG);
 };
 
 export const alertCountInputError = countInput => {
