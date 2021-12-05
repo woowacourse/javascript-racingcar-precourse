7c842852e585de373464026b512a7ef8240f9d0e
ziho <ziho@kakao.com>
2021-12-05 17:34:32 +0900

    chore: eslint rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
   extends: ['eslint-config-tui', 'plugin:prettier/recommended'],
   rules: {
     indent: [2, 2, {SwitchCase: 1, ignoreComments: false, ImportDeclaration: 1}],
-    semi: 2
+    semi: 2,
+    'no-new': 0,
+    'padding-line-between-statements': 0,
+    'newline-before-return': 0
   }
 };
