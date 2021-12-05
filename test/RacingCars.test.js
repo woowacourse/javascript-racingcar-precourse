2e9f8c2457f734abb335715ecd598f2162d30102
sanaandmomo <qajjang46@naver.com>
2021-12-05 19:08:02 +0900

    test: 게임 결과 데이터 바뀐 형식 테스트해라

--- test/RacingCars.test.js
+++ test/RacingCars.test.js
@@ -18,11 +18,11 @@ describe('RacingCars', () => {
     test('getGameResult', () => {
         const gameResult = racingCars.getGameResult(5);
         expect(gameResult.length).toBe(5);
-        expect(gameResult.every((game) => game.trackLogs.length <= 5)).toBeTruthy();
-        expect(gameResult[0].carName).toBe('a');
-        expect(gameResult[1].carName).toBe('b');
-        expect(gameResult[2].carName).toBe('c');
-        expect(gameResult[3].carName).toBe('d');
-        expect(gameResult[4].carName).toBe('e');
+        expect(gameResult.every((game) => game.length <= 5)).toBeTruthy();
+        expect(gameResult[0][0].carName).toBe('a');
+        expect(gameResult[0][1].carName).toBe('b');
+        expect(gameResult[0][2].carName).toBe('c');
+        expect(gameResult[0][3].carName).toBe('d');
+        expect(gameResult[0][4].carName).toBe('e');
     });
 });
