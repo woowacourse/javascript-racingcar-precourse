20dc033b38ee8b6236c06b433cb5fd82dafa2b11
KIM YEJI <rladpwl0512@gmail.com>
2021-12-05 16:39:27 +0900

    refactor: calculateRandomNum 로직을 Car 클래스 내부 메소드로 삽입

--- src/js/handlers/startGame.js
+++ src/js/handlers/startGame.js
@@ -1,22 +1,27 @@
 import $ from '../utils/dom.js';
 import printGameProcess from '../views/printGameProcess.js';
 import printWinner from '../views/printWinner.js';
-import calculateRandomNum from '../models/calculateRandomNum.js';
 import getCarArr from '../models/getCarArr.js';
 
 const startGame = () => {
-  const carArr = getCarArr();
+  const cars = getCarArr(); // 객체들이 모여있는 배열의 형식
+  console.log(cars);
   const resultDiv = document.createElement('div');
   resultDiv.setAttribute('id', 'result-wrapper');
 
   $('#app').appendChild(resultDiv);
 
   let countInput = $('#racing-count-input').value;
+
   while (countInput) {
-    printGameProcess(calculateRandomNum(carArr));
+    // 정지할지 말지 결정
+    cars.forEach(car => {
+      car.setMove();
+    });
+    printGameProcess(cars);
     countInput -= 1;
   }
-  printWinner(carArr);
+  printWinner(cars);
 };
 
 export default startGame;
--- src/js/models/Car.js
+++ src/js/models/Car.js
@@ -1,4 +1,13 @@
+import getRandomNumber from '../utils/getRandomNumber.js';
+
 export default function Car(name) {
   this.name = name;
   this.move = 0;
+
+  this.setMove = () => {
+    const randomNum = getRandomNumber();
+    if (randomNum >= 4) {
+      this.move += 1;
+    }
+  };
 }
