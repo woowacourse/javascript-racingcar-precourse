0057ae5eb3e7faa9a91a6ade28d4b56335e85b19
parksil0 <parksil00@naver.com>
2021-12-06 00:40:33 +0900

    refactor : index에 있는 코드 Main 클래스로 생성

---
+++ src/main.js
@@ -0,0 +1,83 @@
+import Car from './car.js';
+import CarRacing from './carRacing.js';
+import {
+  APP,
+  CAR_NAMES_INPUT,
+  CAR_NAMES_SUBMIT_BUTTON,
+  ERROR_MESSAGE,
+  RACING_COUNT,
+  RACING_COUNT_INPUT,
+  RACING_COUNT_SUBMIT_BUTTON,
+  RACING_RESULT,
+  STYLE_DISPLAY,
+} from './constants.js';
+import { on, qs, setStyleDisplay, checkOverFiveLetters } from './utils.js';
+
+const { BLOCK, INLINE_BLOCK, NONE } = STYLE_DISPLAY;
+const { MORE_THAN_FIVE_LETTERS, NO_MORE_THAN_ZERO } = ERROR_MESSAGE;
+
+export default class Main {
+  constructor() {
+    this.initializeHTML();
+    this.handleCarNamesSubmitButton();
+    this.handleRacingCountSubmitButton();
+  }
+
+  initializeHTML() {
+    this.app = qs(APP);
+    this.carNamesInput = qs(CAR_NAMES_INPUT);
+    this.carNamesSubmitButton = qs(CAR_NAMES_SUBMIT_BUTTON);
+    this.racingCountInput = qs(RACING_COUNT_INPUT);
+    this.racingCountSubmitButton = qs(RACING_COUNT_SUBMIT_BUTTON);
+    this.racingCount = qs(RACING_COUNT);
+    this.racingResult = qs(RACING_RESULT);
+
+    setStyleDisplay(this.racingCount, NONE);
+    setStyleDisplay(this.racingCountInput, NONE);
+    setStyleDisplay(this.racingCountSubmitButton, NONE);
+    setStyleDisplay(this.racingResult, NONE);
+  }
+
+  showRacingCountForm() {
+    setStyleDisplay(this.racingCount, BLOCK);
+    setStyleDisplay(this.racingCountInput, INLINE_BLOCK);
+    setStyleDisplay(this.racingCountSubmitButton, INLINE_BLOCK);
+  }
+
+  handleCarNamesSubmitButton() {
+    on(this.carNamesSubmitButton, 'click', (event) => {
+      event.preventDefault();
+
+      const carNamesArr = this.carNamesInput.value.split(',');
+      if (!checkOverFiveLetters(carNamesArr)) {
+        alert(MORE_THAN_FIVE_LETTERS);
+        return;
+      }
+
+      carNamesArr.forEach((name) => cars.push(new Car(name)));
+      this.showRacingCountForm();
+    });
+  }
+
+  showRacingResult(resultTemplate) {
+    setStyleDisplay(this.racingResult, BLOCK);
+    const racingEachResults = document.createElement('div');
+    racingEachResults.innerHTML = resultTemplate;
+    this.app.append(racingEachResults);
+  }
+
+  handleRacingCountSubmitButton() {
+    on(this.racingCountSubmitButton, 'click', (event) => {
+      event.preventDefault();
+      const racingCountInputValue = this.racingCountInput.value;
+      if (racingCountInputValue <= 0) {
+        alert(NO_MORE_THAN_ZERO);
+        return;
+      }
+
+      const carRacing = new CarRacing(cars);
+      const resultTemplate = carRacing.play(racingCountInputValue);
+      showRacingResult(resultTemplate);
+    });
+  }
+}
