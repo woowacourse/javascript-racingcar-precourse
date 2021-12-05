9f5bf2475a68e4101d690daacf1845f496da7991
sohyeonyun <wja06018@naver.com>
2021-12-06 03:55:48 +0900

    feat: 우승자 발표

--- src/utils/playRacingGame.js
+++ src/utils/playRacingGame.js
@@ -5,7 +5,7 @@ export const playRacingGame = (cars, racingCount) => {
     cars = startRound(cars);
     showGameResult(cars);
   }
-  getWinner(cars);
+  showWinner(cars);
 };
 
 const startRound = cars => {
@@ -26,11 +26,37 @@ const isMove = () => {
 };
 
 const showGameResult = cars => {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+
   cars.forEach(car => {
     const item = `${car.name}: ${'-'.repeat(car.point)} <br/>`;
-    $('#racing-round-result').innerHTML += item;
+    div.innerHTML += item;
   });
-  $('#racing-round-result').innerHTML += '<br/>';
+  div.innerHTML += '<br/>';
 };
 
-const getWinner = cars => {};
+const showWinner = cars => {
+  const winners = getWinner(cars);
+
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+
+  div.innerHTML = `최종 우승자: <span id="racing-winners">${winners.join(
+    ','
+  )}</span>`;
+};
+
+const getWinner = cars => {
+  let winners = [];
+  const maxValue = Math.max.apply(
+    Math,
+    cars.map(car => car.point)
+  );
+  cars.forEach(car => {
+    if (maxValue === car.point) {
+      winners.push(car.name);
+    }
+  });
+  return winners;
+};
