95872944a6a209f5a751efd71c72f3e592f67557
sohyeonyun <wja06018@naver.com>
2021-12-06 04:27:07 +0900

    refactor: 메소드 분리

--- src/utils/playRacingGame.js
+++ src/utils/playRacingGame.js
@@ -1,4 +1,4 @@
-import { $ } from './selector.js';
+import { showGameResult, showWinner } from './showResults.js';
 
 export const playRacingGame = (cars, racingCount) => {
   for (let i = 0; i < racingCount; i++) {
@@ -23,40 +23,4 @@ const isMove = () => {
     return true;
   }
   return false;
-};
-
-const showGameResult = cars => {
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-
-  cars.forEach(car => {
-    const item = `${car.name}: ${'-'.repeat(car.point)} <br/>`;
-    div.innerHTML += item;
-  });
-  div.innerHTML += '<br/>';
-};
-
-const showWinner = cars => {
-  const winners = getWinner(cars);
-
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-
-  div.innerHTML = `최종 우승자: <span id="racing-winners">${winners.join(
-    ','
-  )}</span>`;
-};
-
-const getWinner = cars => {
-  let winners = [];
-  const maxValue = Math.max.apply(
-    Math,
-    cars.map(car => car.point)
-  );
-  cars.forEach(car => {
-    if (maxValue === car.point) {
-      winners.push(car.name);
-    }
-  });
-  return winners;
 };
---
+++ src/utils/showResults.js
@@ -0,0 +1,34 @@
+export const showGameResult = cars => {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+
+  cars.forEach(car => {
+    const item = `${car.name}: ${'-'.repeat(car.point)} <br/>`;
+    div.innerHTML += item;
+  });
+  div.innerHTML += '<br/>';
+};
+
+export const showWinner = cars => {
+  const winners = getWinner(cars);
+
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  div.innerHTML = `
+    최종 우승자: <span id="racing-winners">${winners.join(',')}</span>
+  `;
+};
+
+const getWinner = cars => {
+  let winners = [];
+  const maxValue = Math.max.apply(
+    Math,
+    cars.map(car => car.point)
+  );
+  cars.forEach(car => {
+    if (maxValue === car.point) {
+      winners.push(car.name);
+    }
+  });
+  return winners;
+};
