448fc0589450aaf9c220f3e3c8c019565fe82d1f
Leejin-Yang <ejin1996@gmail.com>
2021-12-05 22:34:30 +0900

    refactor: 이동거리 number -> string

--- src/car.js
+++ src/car.js
@@ -1,14 +1,14 @@
 export default class Car {
   constructor(name) {
     this.name = name;
-    this.distance = 0;
+    this.distance = "";
   }
 
   move() {
     const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
 
     if (randomNumber >= 4) {
-      this.distance += 1;
+      this.distance += "-";
     }
   }
 }
