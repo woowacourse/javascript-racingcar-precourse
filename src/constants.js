9a1e13c7a75c18e5ffd9a4cc1e80b9a732ec95d3
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-05 22:35:47 +0900

    fix: readme 조건 변경 및 상수 수정

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 export const CARRACING = {
   ALERT_WRONG_NAME: '조건에 맞게 자동차 이름을 입력해주세요.',
-  ALERT_WRONG_NUM: '숫자만 입력해 주세요.',
+  ALERT_WRONG_NUM: '1 이상의 수를 입력해주세요.',
   WINNER: '최종 우승자',
-  MIN_NUM: '0',
-  RANGE_MIN_NUM: '4',
-  MAX_NUM: '9',
+  MIN_NUM: 0,
+  RANGE_MIN_NUM: 4,
+  MAX_NUM: 9,
 };
