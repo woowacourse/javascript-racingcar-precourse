bd26dc31e7847354d723bcea1302ebf4d2e7768f
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-12-05 22:15:33 +0900

    refactor: CarNameValidator 변경
    - 메소드안에서 alert를 하는 것이 아닌 boolean값만 리턴하도록 변경

--- src/validators/CarNameValidator.js
+++ src/validators/CarNameValidator.js
@@ -1,26 +1,15 @@
 export default {
   checkAtLeastOneCar(splittedCarNames) {
-    if (!splittedCarNames[0]) {
-      alert('자동차를 최소한 하나 이상 입력해주세요');
-      return false;
-    }
-    return true;
+    return splittedCarNames[0];
   },
   checkCarNameDuplicated(splittedCarNames) {
     const set = new Set();
     splittedCarNames.forEach(carName => set.add(carName));
-    if (set.size !== splittedCarNames.length) {
-      alert('자동차 이름이 겹쳤습니다.');
-      return false;
-    }
-    return true;
+    return set.size === splittedCarNames.length;
   },
   checkCarNameUnderFiveLetter(splittedCarNames) {
     for (let i = 0; i < splittedCarNames.length; i += 1) {
-      if (splittedCarNames[i].length > 5) {
-        alert('자동차 이름은 5자 이하만 가능합니다.');
-        return false;
-      }
+      if (splittedCarNames[i].length > 5) return false;
     }
     return true;
   },
