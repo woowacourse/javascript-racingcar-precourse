1e69e427db418972837ee6ab82d57fbc1716258e
parksil0 <parksil00@naver.com>
2021-12-05 20:02:22 +0900

    style : eslint 규칙 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,18 @@ module.exports = {
   },
   rules: {
     'import/extensions': ['error', 'always'],
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'no-alert': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'max-classes-per-file': 'off',
+    'no-use-before-define': 'off',
+    'no-loop-func': 'off',
+    'no-undef': 'off',
   },
 };
