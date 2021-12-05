9efae76686b25f21aafea559cd2ea9d88e3eda26
kkojae <jaejeung3210@gmail.com>
2021-12-05 21:35:40 +0900

    refactor: RacingResult 관련 파일 분리
    - Controller에서 경주 결과, 최종 우승자 관리 -> RacingResult.js 로 분리하여 관리

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -2,59 +2,30 @@ import { CAUTION_MESSAGE, DOM } from '../utils/constant.js';
 import CarNamesEvent from './CarNamesEvent.js';
 import RacingCountEvent from './RacingCountEvent.js';
 import Game from '../model/Game.js';
-import Render from '../view/Render.js';
-import Winners from '../model/Winners.js';
+import ResultRacing from './RacingResult.js';
 
 export default class Controller {
   constructor() {
     this.carNamesEvent = new CarNamesEvent();
     this.racingCountEvent = new RacingCountEvent();
-    this.render = new Render();
+
     this.$racingCountSubmit = DOM.$RACING_COUNT_SUBMIT;
     this.$racingCountInput = DOM.$RACING_COUNT_INPUT;
     this.$carNamesSubmit = DOM.$CAR_NAMES_SUBMIT;
     this.$app = DOM.$APP;
-    this.$div = DOM.$DIV;
-    this.$span = DOM.$SPAN;
-    this.game = null;
+
     this.carNames = [];
-    this.resultInformations = [];
-    this.resultWinners = [];
     this.racingCount = 0;
-    this.resultTemplate = '';
-    this.resultWinnersTemplate = '';
+
     this.main();
   }
-
-  racingCountInputFocus = () => {
-    this.$racingCountInput.addEventListener('focus', () => {
-      if (this.racingCountEvent.isCarNamesBlank()) {
-        this.racingCountEvent.alertCautionMessage(CAUTION_MESSAGE.FIRST_CAR_NAMES);
-      }
-    });
-  };
-
-  renderResultTemplate = () => this.render.template(this.$div, this.$app, this.resultTemplate);
-
-  renderWinnersTemplate = () => this.render.template(this.$span, this.$app, this.resultWinnersTemplate);
-
-  getRacingResult = () => {
-    this.resultTemplate = this.game.getResultTemplate();
-    this.resultInformations = this.game.getResultInformations();
-  };
 
   readyGame = () => {
     this.racingCount = this.racingCountEvent.getInput();
 
-    this.game = new Game(this.racingCount, this.carNames);
-    this.game.start();
-    this.getRacingResult();
-    this.renderResultTemplate();
-
-    const winners = new Winners(this.resultInformations);
-    this.resultWinners = winners.getWinners();
-    this.resultWinnersTemplate = winners.getWinnersTemplate();
-    this.renderWinnersTemplate();
+    const game = new Game(this.racingCount, this.carNames);
+    game.start();
+    new ResultRacing(game);
   };
 
   isCarNamesSubmit = () => {
@@ -106,6 +77,14 @@ export default class Controller {
 
       if (event.target === this.$racingCountSubmit) {
         this.racingCountSubmit();
+      }
+    });
+  };
+
+  racingCountInputFocus = () => {
+    this.$racingCountInput.addEventListener('focus', () => {
+      if (this.racingCountEvent.isCarNamesBlank()) {
+        this.racingCountEvent.alertCautionMessage(CAUTION_MESSAGE.FIRST_CAR_NAMES);
       }
     });
   };
---
+++ src/controller/RacingResult.js
@@ -0,0 +1,40 @@
+import Winners from '../model/Winners.js';
+import { DOM } from '../utils/constant.js';
+import Render from '../view/Render.js';
+
+export default class ResultRacing {
+  constructor(game) {
+    this.game = game;
+    this.render = new Render();
+
+    this.$app = DOM.$APP;
+    this.$div = DOM.$DIV;
+    this.$span = DOM.$SPAN;
+
+    this.resultInformations = [];
+    this.resultWinners = [];
+    this.resultWinnersTemplate = '';
+    this.resultTemplate = '';
+
+    this.main();
+  }
+
+  renderWinnersTemplate = () => this.render.template(this.$span, this.$app, this.resultWinnersTemplate);
+
+  renderResultTemplate = () => this.render.template(this.$div, this.$app, this.resultTemplate);
+
+  getRacingResult = () => {
+    this.resultTemplate = this.game.getResultTemplate();
+    this.resultInformations = this.game.getResultInformations();
+  };
+
+  main = () => {
+    this.getRacingResult();
+    this.renderResultTemplate();
+
+    const winners = new Winners(this.resultInformations);
+    this.resultWinners = winners.getWinners();
+    this.resultWinnersTemplate = winners.getWinnersTemplate();
+    this.renderWinnersTemplate();
+  };
+}
