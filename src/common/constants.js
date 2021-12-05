21d5733cc762849186d157c099450123a42d4ae2
jhy979 <jhy979@gmail.com>
2021-12-05 23:57:05 +0900

    feat: carName 5자 이하 확인 함수 구현

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,5 +3,12 @@ const $carNameSubmit = document.getElementById("car-names-submit");
 const $racingCount = document.getElementById("racing-count-input");
 const $racingCountSubmit = document.getElementById("racing-count-submit");
 const $winner = document.getElementById("racing-winners");
-
-export { $carName, $carNameSubmit, $racingCount, $racingCountSubmit, $winner };
+const NAME_MAX_LENGTH = 5;
+export {
+  $carName,
+  $carNameSubmit,
+  $racingCount,
+  $racingCountSubmit,
+  $winner,
+  NAME_MAX_LENGTH,
+};
--- src/input/validateCarName.js
+++ src/input/validateCarName.js
@@ -1,10 +1,22 @@
-import { $carName } from "../common/constants.js";
+import { $carName, NAME_MAX_LENGTH } from "../common/constants.js";
 
 const splitCarName = () => $carName.value.split(",");
 
 const trimCarName = (carArr) => carArr.map((el) => el.trim());
 
+const isCarNameLength = (carArr) => {
+  let valid = true;
+  carArr.map((el) => {
+    if (el.length > NAME_MAX_LENGTH) {
+      valid = false;
+      return false;
+    }
+  });
+
+  return valid;
+};
+
 export default function validateCarName() {
-  let carArr = splitCarName();
-  carArr = trimCarName(carArr);
+  const carArr = trimCarName(splitCarName());
+  console.log(isCarNameLength(carArr));
 }
