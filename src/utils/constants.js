2249d63cf0f7d0e8b148f4b59a8800110f5e388c
ziho <ziho@kakao.com>
2021-12-05 23:05:12 +0900

    feat: html 헤더 id 선택자 설정

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,11 @@ export const DOM_ID = Object.freeze({
   CAR_NAMES_CONTAINER: 'car-names-container',
   CAR_NAMES_INPUT: 'car-names-input',
   CAR_NAMES_SUBMIT: 'car-names-submit',
+  RACING_COUNT_TITLE: 'raicing-count-title',
   RACING_COUNT_CONTAINER: 'raicing-count-container',
   RACING_COUNT_INPUT: 'racing-count-input',
   RACING_COUNT_SUBMIT: 'racing-count-submit',
+  RESULT_TITLE: 'result-title',
   RACING_WINNERS: 'racing-winners'
 });
 
@@ -21,4 +23,10 @@ export const ERROR_MESSAGE = Object.freeze({
   EMPTY_INPUT: '값을 입력해주세요.',
   DUPLICATED_NAME: '중복된 이름이 존재합니다.',
   NEGATIVE_NUMBER: '게임 횟수는 양수를 입력해주세요.'
+});
+
+export const PROGRESS = Object.freeze({
+  INPUT_CAR_NAME: 0,
+  INPUT_RACING_COUNT: 1,
+  PLAY: 2
 });
