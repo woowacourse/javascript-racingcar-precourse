3838f9ac53aa1c154f73e8c70902eb514ae828d5
sanaandmomo <qajjang46@naver.com>
2021-12-05 18:31:12 +0900

    test: 테스트 진행을 위한 setup 설정 추가해라

--- test/setup/index.js
+++ test/setup/index.js
@@ -1,1 +1,7 @@
 global.alert = () => {};
+global.MissionUtils = {
+    Random: {
+        pickNumberInRange: (startInclusive, endInclusive) =>
+            Math.floor(Math.random() * (endInclusive + 1 - startInclusive)) + startInclusive,
+    },
+};
