be48444cb08fd37ccdf2161b296c42ea5079d1eb
jhy979 <jhy979@gmail.com>
2021-12-05 22:16:43 +0900

    chore: eslint, prettier 설정 적용

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,13 +1,7 @@
-const $carName = document.getElementById('car-names-input');
-const $carNameSubmit = document.getElementById('car-names-submit');
-const $racingCount = document.getElementById('racing-count-input');
-const $racingCountSubmit = document.getElementById('racing-count-submit');
-const $winner = document.getElementById('racing-winners');
+const $carName = document.getElementById("car-names-input");
+const $carNameSubmit = document.getElementById("car-names-submit");
+const $racingCount = document.getElementById("racing-count-input");
+const $racingCountSubmit = document.getElementById("racing-count-submit");
+const $winner = document.getElementById("racing-winners");
 
-export {
-  $carName,
-  $carNameSubmit,
-  $racingCount,
-  $racingCountSubmit,
-  $winner
-}
+export { $carName, $carNameSubmit, $racingCount, $racingCountSubmit, $winner };
