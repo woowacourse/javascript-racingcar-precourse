467c3225e238ee88337d271b870d504770ae1a48
Leejin-Yang <ejin1996@gmail.com>
2021-12-05 20:24:58 +0900

    feat: 자동차 전진 기능 구현

--- src/car.js
+++ src/car.js
@@ -3,4 +3,12 @@ export default class Car {
     this.name = name;
     this.distance = 0;
   }
+
+  move() {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+
+    if (randomNumber >= 4) {
+      this.distance += 1;
+    }
+  }
 }
