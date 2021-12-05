6b82954e5f92573e48ba8a4f3dd25df4567bab9a
kimsanglok <evertree6031@gmail.com>
2021-12-05 23:43:10 +0900

    Feat: 자동차이름 입력값 - 중복 검사하는 함수 추가

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,6 +6,11 @@ const isLongerThanFiveLetters = (carNamesInput) => {
   return carNamesInput.split(',').some((carName) => carName.length > 5);
 };
 
+const isDuplicate = (carNamesInput) => {
+  const carNamesArray = carNamesInput.split(',');
+  return carNamesArray.length !== [...new Set(carNamesArray)].length;
+};
+
 const isValidate = (carNamesInput) => {
   if (isEmpty(carNamesInput)) {
     alert('자동차 이름을 입력해주세요.  올바른 예) east,west,south,north');
@@ -14,6 +19,11 @@ const isValidate = (carNamesInput) => {
 
   if (isLongerThanFiveLetters(carNamesInput)) {
     alert('자동차 이름은 5자 이하로 입력해주세요.  올바른 예) east,west,south,north');
+    return false;
+  }
+
+  if (isDuplicate(carNamesInput)) {
+    alert('자동차 이름은 중복 없이 입력해주세요.  올바른 예) east,west,south,north');
     return false;
   }
 
