4f0f2f763ea61f78a318edd1d544e5886df53a60
sanaandmomo <qajjang46@naver.com>
2021-12-05 09:37:12 +0900

    feat: 자동차 이름 공백 있는지 체크해라

--- src/app/asset/validation.js
+++ src/app/asset/validation.js
@@ -1,5 +1,6 @@
 const checkCarNameFunctions = [
-    (carNames) => carNames.every((carName) => carName.length >= 1 && carName.length <= 5),
+    (names) => names.every((name) => name.length >= 1 && name.length <= 5),
+    (names) => names.every((name) => name.length === name.replace(/\s/g, '')),
 ];
 
 module.exports = checkCarNameFunctions;
