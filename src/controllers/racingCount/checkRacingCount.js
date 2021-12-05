4ce5e7ab4122293de83cff291c84005b88323df2
LAH1203 <lah1203@naver.com>
2021-12-05 14:31:19 +0900

    fix: 횟수 입력 시 숫자가 아니거나 실수인 경우를 예외로 판단하도록 수정

--- src/controllers/racingCount/checkRacingCount.js
+++ src/controllers/racingCount/checkRacingCount.js
@@ -1,7 +1,19 @@
+const isInteger = input => {
+  let check = true;
+
+  if (isNaN(input)) {
+    check = false;
+  } else if (Number(input) % 1 !== 0) {
+    check = false;
+  }
+
+  return check;
+};
+
 const isValidRacingCount = racingCountStr => {
   let check = true;
 
-  if (racingCountStr === "") {
+  if (!isInteger(racingCountStr)) {
     check = false;
   } else {
     const racingCount = parseInt(racingCountStr, 10);
