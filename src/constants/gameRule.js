e7fc30d30ea1a02193d81061f7ec97a462485859
Leejin-Yang <ejin1996@gmail.com>
2021-12-06 03:11:08 +0900

    feat: 게임 요소 및 에러 메시지 상수화

---
+++ src/constants/gameRule.js
@@ -0,0 +1,18 @@
+export const CAR_NUMBER_MIN = 2;
+export const RACING_COUNT_MIN = 1;
+
+export const RANDOM = {
+  MIN: 0,
+  MAX: 9,
+};
+
+export const NAME_LENGTH = {
+  MIN: 1,
+  MAX: 5,
+};
+
+export const GAME = {
+  MOVE_CONDITION: 4,
+  DISTANCE_MARKER: "-",
+  WINNER: "최종 우승자",
+};
