1aef1bd97e37a40e3fcae8e1970d2d43577707ee
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-05 22:47:03 +0900

    feat: 자동차 이름, 카운트 수 받아오는 함수 추가

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ const carNameSubmit = document.getElementById('car-names-submit');
 const countInput = document.getElementById('racing-count-input');
 const countSubmit = document.getElementById('racing-count-submit');
 
-export default function racingGame() {}
+export default function racingGame() {
+  carNameSubmit.addEventListener('click', getCarName);
+  countSubmit.addEventListener('click', getCount);
+}
 
 function Car(name) {
   this.name = name;
+}
+
+function getCarName() {
+  let carNameList = [];
+  carNameList = carNameInput.value.split(',');
+  return carNameList;
+}
+
+function getCount() {
+  let count = 0;
+  count = countInput.value;
+  return count;
 }
 
 racingGame();
