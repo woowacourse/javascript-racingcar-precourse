9a2b1483fd7902a660edffce7b2d800c3fd1ba27
sanaandmomo <qajjang46@naver.com>
2021-12-05 09:42:31 +0900

    test: 자동차 이름 공백값 체크 잘하는지 테스트해라

--- test/validtaion.test.js
+++ test/validtaion.test.js
@@ -1,9 +1,14 @@
 const checkCarNameFunctions = require('../src/app/asset/validation');
 
-const [checkCarLength] = checkCarNameFunctions;
+const [checkCarLength, checkSpace] = checkCarNameFunctions;
 
 test('checkCarLength', () => {
     expect(checkCarLength(['abc', 'ced', 'efg'])).toBeTruthy();
     expect(checkCarLength(['moreThanFive', 'abc'])).toBeFalsy();
     expect(checkCarLength(['', 'abc'])).toBeFalsy();
+});
+
+test('checkSpace', () => {
+    expect(checkSpace(['abc', 'ced', 'efg'])).toBeTruthy();
+    expect(checkSpace(['ab c', 'ced', 'efg'])).toBeFalsy();
 });
