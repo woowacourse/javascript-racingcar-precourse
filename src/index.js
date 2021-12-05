30e767ad8a3751e87c69de7c57054eba92a2d5a6
junzzi <jjh01027717502@gmail.com>
2021-12-06 01:59:59 +0900

    feat: add function to play game (input simulation count)
     '시도할 횟수'를 입력하여 자동차 게임을 실행할 수 있다.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import { DOM, PLAIN_STRING } from './lib/constants.js';
-import { isEveryStringLessOrEqualsFive } from './lib/utils.js.js';
+import {
+  DOM,
+  FORWARD_NUMBER_SIGN,
+  MINUS_SIGN,
+  PLAIN_STRING,
+} from './lib/constants.js';
+import { getRandomNumber } from './lib/utils.js';
+import { isEveryStringLessOrEqualsFive } from './lib/utils.js';
 class Car {
   constructor(name, id) {
     this.id = id;
     this.name = name;
     this.countTemplate = PLAIN_STRING;
     this.count = 0;
+  }
+
+  setCountTemplatePlusOne() {
+    this.countTemplate = `${this.countTemplate}${MINUS_SIGN}`;
+  }
+
+  setCountPlusOne() {
+    this.count = this.count + 1;
   }
 }
 class CarGameUtil {
@@ -20,24 +34,50 @@ class CarGameUtil {
     return isEveryStringLessOrEqualsFive(namesArray);
   }
 }
+class CarGameLogic {
+  constructor() {}
 
-class CarGame {
+  makeTemplatePerSimulate() {
+    let template = PLAIN_STRING;
+
+    return template;
+  }
+
+  simulatePerNumberOfTimes() {
+    this.cars.forEach(car => {
+      // simulate
+    });
+  }
+
+  makeResultTemplate(number) {
+    let template = PLAIN_STRING;
+    for (let i = 0; i < number; i = i + 1) {
+      this.simulatePerNumberOfTimes();
+      // simulate 이후 자동차를 기준으로 템플릿을 만들어 변수에 합친다.
+      template = `${template}${this.makeTemplatePerSimulate()}`;
+    }
+
+    return template;
+  }
+}
+class CarGame extends CarGameLogic {
   constructor() {
+    super();
     this.cars = null;
   }
 
   init() {
     this.initDOM();
     this.initHandler();
-
     this.formCars.addEventListener('submit', this.onFormCarsSubmit);
+    this.formCount.addEventListener('submit', this.onFormCountSubmit);
   }
 
   initDOM() {
     this.app = document.querySelector(DOM.APP);
 
-    this.formCars = document.querySelector(DOM.FORM_CARS_ID);
-    this.formCount = document.querySelector(DOM.FORM_COUNT_ID);
+    this.formCars = document.querySelector(DOM.CAR_NAMES);
+    this.formCount = document.querySelector(DOM.RANCING_COUNT);
 
     this.result = document.createElement('div');
     this.result.setAttribute('id', 'result');
@@ -47,9 +87,7 @@ class CarGame {
   initHandler() {
     this.onFormCarsSubmit = e => {
       e.preventDefault();
-
       const [{ value: carsString }] = e.target;
-
       try {
         this.cars = CarGameUtil.makeCars(carsString.split(','));
       } catch (error) {
@@ -57,6 +95,17 @@ class CarGame {
         alert(error);
       }
     };
+    this.onFormCountSubmit = e => {
+      e.preventDefault();
+      const [{ value: stringNumber }] = e.target;
+      try {
+        this.result.innerHTML = this.makeResultTemplate(Number(stringNumber));
+      } catch (error) {
+        this.result.innerHTML = PLAIN_STRING;
+        alert(error);
+      }
+    };
   }
 }
+
 new CarGame().init();
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,6 @@
 export const isEveryStringLessOrEqualsFive = namesArray => {
   return namesArray.every(name => name.length <= 5);
 };
+export const getRandomNumber = () => {
+  return window.MissionUtils.Random.pickNumberInRange(0, 9);
+};
