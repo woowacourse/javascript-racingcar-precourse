1f32bd9fee7e2cc02f974e66571b3eb9556ba2f4
jhy979 <jhy979@gmail.com>
2021-12-05 22:56:58 +0900

    feat: DOM 선택자 지정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,6 @@
-const $carName = document.getElementById("car-names-input");
+const $carName = document.getElementById("car-names-input").value;
 const $carNameSubmit = document.getElementById("car-names-submit");
-const $racingCount = document.getElementById("racing-count-input");
+const $racingCount = document.getElementById("racing-count-input").value;
 const $racingCountSubmit = document.getElementById("racing-count-submit");
 const $winner = document.getElementById("racing-winners");
 
