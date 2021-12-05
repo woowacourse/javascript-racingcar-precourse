dc0b4d4cb02962c265cfba2b310a2424ac9eae5a
jhy979 <jhy979@gmail.com>
2021-12-06 00:15:31 +0900

    feat: race 현재 상황 status DOM 선택자 상수화

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,9 +2,12 @@ const $carName = document.getElementById("car-names-input");
 const $carNameSubmit = document.getElementById("car-names-submit");
 const $racingCount = document.getElementById("racing-count-input");
 const $racingCountSubmit = document.getElementById("racing-count-submit");
+const $status = document.getElementById("racing-status");
 const $winner = document.getElementById("racing-winners");
+
 const NAME_MAX_LENGTH = 5;
 const ERROR_CAR_NAME = "자동차의 이름을 형식에 맞춰서 작성해주세요.";
+
 export {
   $carName,
   $carNameSubmit,
@@ -13,4 +16,5 @@ export {
   $winner,
   NAME_MAX_LENGTH,
   ERROR_CAR_NAME,
+  $status,
 };
