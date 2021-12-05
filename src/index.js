24bbdef46d1cd58c6026c817c063681b3f7a20c5
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-05 22:43:19 +0900

    feat: input, submit 요소 불러오기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-class Car {
-  constructor(name) {
-    this.name = name;
-  }
+const carNameInput = document.getElementById('car-names-input');
+const carNameSubmit = document.getElementById('car-names-submit');
+const countInput = document.getElementById('racing-count-input');
+const countSubmit = document.getElementById('racing-count-submit');
+
+export default function racingGame() {}
+
+function Car(name) {
+  this.name = name;
 }
 
-Car();
+racingGame();
