372af8eb922921cce3cdd8c5ad2e45498f8215e4
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-05 23:17:53 +0900

    feat: 자동차 이름 조건 검사 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { CARRACING } from './constants.js';
 const carNameInput = document.getElementById('car-names-input');
 const carNameSubmit = document.getElementById('car-names-submit');
 const countInput = document.getElementById('racing-count-input');
@@ -12,9 +13,55 @@ function Car(name) {
   this.name = name;
 }
 
+function checkCarNameDuplicate(carNameList) {
+  for (let name of carNameList) {
+    let count = carNameList.filter(element => name === element).length;
+
+    if (count >= 2) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkCarNameLen(carNameList) {
+  for (let name of carNameList) {
+    if (name.length >= 6) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkCarNameComma(carNameList) {
+  if (carNameList.length === 1) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkCarName(carNameList) {
+  if (
+    checkCarNameDuplicate(carNameList) &&
+    checkCarNameLen(carNameList) &&
+    checkCarNameComma(carNameList)
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
 function getCarName() {
   let carNameList = [];
   carNameList = carNameInput.value.split(',');
+  if (!checkCarName(carNameList)) {
+    alert(CARRACING.ALERT_WRONG_NAME);
+    return false;
+  }
   return carNameList;
 }
 
