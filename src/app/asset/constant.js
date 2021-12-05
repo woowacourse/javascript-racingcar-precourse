c582c5a0d6154ebc709a7c46f090bbe5c978e56b
sanaandmomo <qajjang46@naver.com>
2021-12-05 09:58:29 +0900

    feat: 자동차 이름값 유효성 판단 만족하지 못할 때 alert 메세지 띄워라

---
+++ src/app/asset/constant.js
@@ -0,0 +1,4 @@
+export const CAR_NAMES_INPUT_MSG = {
+    length: '자동차 이름을 1 ~ 5자 사이로 입력해주세요.',
+    space: '자동차 이름을 공백없이 입력해주세요.',
+};
--- src/app/asset/validation.js
+++ src/app/asset/validation.js
@@ -1,6 +1,22 @@
+import { CAR_NAMES_INPUT_MSG } from './constant';
+
 const checkCarNameFunctions = [
-    (names) => names.every((name) => name.length >= 1 && name.length <= 5),
-    (names) => names.every((name) => name.length === name.replace(/\s/g, '').length),
+    (names) => {
+        if (names.every((name) => name.length < 1 || name.length > 5)) {
+            alert(CAR_NAMES_INPUT_MSG.length);
+            return false;
+        }
+
+        return true;
+    },
+    (names) => {
+        if (names.every((name) => name.length !== name.replace(/\s/g, '').length)) {
+            alert(CAR_NAMES_INPUT_MSG.space);
+            return false;
+        }
+
+        return true;
+    },
 ];
 
-module.exports = checkCarNameFunctions;
+const checkCarName = (names) => checkCarNameFunctions.every(names);
