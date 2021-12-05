e4e17b217fe2b40ac4a07f4de48d729d49e1f64e
On <cks3066@naver.com>
2021-12-05 20:01:09 +0900

    feat: 입력 예외처리 (alert 사용)

--- src/controllers/userInput.js
+++ src/controllers/userInput.js
@@ -1,9 +1,17 @@
-import { makeStringToArrayByComma } from "./utils.js";
+import {
+  makeStringToArrayByComma,
+  IsElementsOverFiveWords,
+  IsElementsExsistSpace,
+} from "./utils.js";
 
 export function carNamesInput(e) {
   e.preventDefault();
   const $carNamesInput = document.getElementById("car-names-input");
-
   const cars = makeStringToArrayByComma($carNamesInput.value);
-  console.log(cars);
+
+  if (IsElementsOverFiveWords(cars))
+    alert("자동차 이름을 5자 이하로 입력해주세요.");
+  else if (IsElementsExsistSpace(cars))
+    alert("자동차 이름에 공백을 제거해주세요.");
+  else console.log(cars);
 }
--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,3 +1,18 @@
 export function makeStringToArrayByComma(string) {
   return string.split(",");
 }
+
+export function IsElementsOverFiveWords(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length > 5) return true;
+  }
+  return false;
+}
+
+export function IsElementsExsistSpace(array) {
+  const pattern = /\s/g;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].match(pattern)) return true;
+  }
+  return false;
+}
