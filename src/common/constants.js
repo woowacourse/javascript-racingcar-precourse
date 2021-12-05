6aad76c119b0342e731b4d235f08aa9d794632ea
jhy979 <jhy979@gmail.com>
2021-12-05 21:02:04 +0900

    feat: html dom selector 상수화

---
+++ src/common/constants.js
@@ -0,0 +1,13 @@
+const $carName = document.getElementById('car-names-input');
+const $carNameSubmit = document.getElementById('car-names-submit');
+const $racingCount = document.getElementById('racing-count-input');
+const $racingCountSubmit = document.getElementById('racing-count-submit');
+const $winner = document.getElementById('racing-winners');
+
+export {
+  $carName,
+  $carNameSubmit,
+  $racingCount,
+  $racingCountSubmit,
+  $winner
+}
