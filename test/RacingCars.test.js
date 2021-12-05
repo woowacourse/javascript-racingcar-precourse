459bd22b7be5b458a5dbae6783bfdd63b2740969
sanaandmomo <qajjang46@naver.com>
2021-12-05 18:31:35 +0900

    test: 레이싱 횟수만큼 진행한 데이터가 올바른지 테스트해라

--- test/RacingCars.test.js
+++ test/RacingCars.test.js
@@ -5,13 +5,24 @@ describe('RacingCars', () => {
 
     beforeEach(() => {
         racingCars = new RacingCars();
+        racingCars.generateCars(['a', 'b', 'c', 'd', 'e']);
     });
 
     test('getWinners', () => {
-        racingCars.generateCars(['a', 'b', 'c', 'd', 'e']);
         racingCars.cars[0].go();
         racingCars.cars[1].go();
         racingCars.cars[2].go();
         expect(racingCars.getWinners()).toEqual(['a', 'b', 'c']);
+    });
+
+    test('getGameResult', () => {
+        const gameResult = racingCars.getGameResult(5);
+        expect(gameResult.length).toBe(5);
+        expect(gameResult.every((game) => game.trackLogs.length <= 5)).toBeTruthy();
+        expect(gameResult[0].name).toBe('a');
+        expect(gameResult[1].name).toBe('b');
+        expect(gameResult[2].name).toBe('c');
+        expect(gameResult[3].name).toBe('d');
+        expect(gameResult[4].name).toBe('e');
     });
 });
