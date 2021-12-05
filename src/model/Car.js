38de839f85bb44fefa0ddbe1bb5c85933644ac1e
kimsanglok <evertree6031@gmail.com>
2021-12-06 01:59:21 +0900

    Feat: Car 클래스에 get 메서드 추가

--- src/model/Car.js
+++ src/model/Car.js
@@ -3,6 +3,14 @@ class Car {
     this.name = name;
     this.distance = 0;
   }
+
+  getName() {
+    return this.name;
+  }
+
+  getDistance() {
+    return this.distance;
+  }
 }
 
 export default Car;
