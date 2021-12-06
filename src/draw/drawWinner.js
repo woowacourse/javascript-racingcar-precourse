087e6c41762b94a5976915c80bdf074b5db9cba9
NaRae-tech <narae.dev@gmail.com>
2021-12-06 19:16:38 +0900

    modify error about winner printing

--- src/draw/drawWinner.js
+++ src/draw/drawWinner.js
@@ -5,6 +5,7 @@ export default function DrawWinner(carList) {
   let winners = ComputeWinner(carList);
   let winnerString = winners.join(',');
   const winner = document.createElement('p');
-  winner.innerHTML=`최종우승자 : ${winnerString}`;
+  winner.id = "racing-winners";
+  winner.innerHTML=`${winnerString}`;
   $('#racing-result-container').append(winner);
 }
