8a01e57ffc92291764034b9fc1fed6345a1361ee
연훈 배 <sbaru1997@naver.com>
2021-12-07 00:50:47 +0900

    feat: 레이싱 횟수 입력 기능 구현
    userInput.js => carNamesInput, racingCountInput

--- src/user/userInput.js
+++ src/user/userInput.js
@@ -2,6 +2,8 @@ import { getValidateNames } from './validateInput.js';
 
 const $carNamesInput = document.querySelector('#car-names-input');
 const $carNamesSubmit = document.querySelector('#car-names-submit');
+const $racingCountInput = document.querySelector('#racing-count-input');
+const $racingCountSubmit = document.querySelector('#racing-count-submit');
 
 function Car(name) {
   this.name = name;
@@ -14,7 +16,18 @@ function carNamesInput(event) {
 
   if (getValidateNames(carNames)) {
     const cars = carNames.map(name => new Car(name));
+    racingCountInput(cars);
   }
+}
+
+function racingCountInput(cars) {
+  cars.forEach(car => (car.count = 0));
+  $racingCountSubmit.addEventListener('click', event => {
+    event.preventDefault();
+
+    const racingCount = $racingCountInput.value;
+    console.log(racingCount);
+  });
 }
 
 export default function userInput() {
