bd0330b19c208869d10e9f727347d232d3037ddd
airman5573 <rpf5573@kakao.com>
2021-12-06 21:45:25 +0900

    chore: add linting using eslint based on airbnb-base

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-alert': 'off',
+    'import/extensions': ['error', 'always'],
+    'max-depth': ['error', 2],
+  },
+};
