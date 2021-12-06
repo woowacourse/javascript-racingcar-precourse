a674d04c6ac74cea111c3ba35a021f487282b799
nan-noo <uni613@naver.com>
2021-12-07 01:26:17 +0900

    refactor: separate duplicated code
    Added in event-handler.js
    - popAlertMessage

--- src/event-handler.js
+++ src/event-handler.js
@@ -21,30 +21,26 @@ export default class EventHandler {
     
     onCarNameSubmit() {
         if(!this.isCorrectCarName()) {
-            alert("잘못된 입력 형식입니다. 쉼표로 구분하여 각각 5자 이하로 입력해주세요.");
-            this.carNameInput.value = '';
-            this.carNameInput.focus();
+            this.popAlertMessage("잘못된 입력 형식입니다. 쉼표로 구분하여 각각 5자 이하로 입력해주세요.", this.carNameInput);
         }
     }
     
     onRacingCountSubmit() {
         if(this.isCorrectCount()) {
-            if(!this.isCorrectCarName()) {
-                alert("자동차 이름을 형식에 맞춰 입력해 주세요.");
-                this.carNameInput.value = '';
-                this.carNameInput.focus();
-            }
-            else {
+            if(this.isCorrectCarName()) {
                 const carNameArray = this.carNameInput.value.split(',');
                 const racingCount = Number(this.countInput.value);
                 const newGame = new RacingCarGame(carNameArray, racingCount, this.app);
                 newGame.gameStart();
             }
+            else this.popAlertMessage("자동차 이름을 형식에 맞춰 입력해 주세요.", this.carNameInput);
         }
-        else {
-            alert("잘못된 입력 형식입니다. 1 이상의 정수를 입력해주세요.");
-            this.countInput.value = '';
-            this.countInput.focus();
-        }
+        else this.popAlertMessage("잘못된 입력 형식입니다. 1 이상의 정수를 입력해주세요.", this.countInput);
+    }
+
+    popAlertMessage(message, input) {
+        alert(message);
+        input.value = '';
+        input.focus();
     }
 };
