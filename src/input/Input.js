9d481a132f42949a9610f3828bb0a722def7ad3d
DomMorello <zuzudnf@gmail.com>
2021-12-06 15:36:46 +0900

    fix: Input 클래스 static 함수를 클래스 외부로 이동

--- src/input/Input.js
+++ src/input/Input.js
@@ -1,40 +1,40 @@
+function isLongerThanFive(names) {
+  return names.find((name) => name.length > 5);
+}
+
+function isEmptyName(names) {
+  return names.find((name) => name === '');
+}
+
+function isIncludeSpaceInName(names) {
+  return names.find((name) => name.split('').includes(' '));
+}
+
+function isValidCarNames(carNames) {
+  if (isEmptyName(carNames) === '') {
+    return false;
+  }
+  if (isLongerThanFive(carNames)) {
+    return false;
+  }
+  if (isIncludeSpaceInName(carNames)) {
+    return false;
+  }
+  return true;
+}
+
+function isAboveZero(repeatCount) {
+  return Number(repeatCount) > 0;
+}
+
 export default class Input {
   constructor() {
     this.carNames = null;
     this.repeatCount = 0;
   }
 
-  static isLongerThanFive(names) {
-    return names.find((name) => name.length > 5);
-  }
-
-  static isEmptyName(names) {
-    return names.find((name) => name === '');
-  }
-
-  static isIncludeSpaceInName(names) {
-    return names.find((name) => name.split('').includes(' '));
-  }
-
-  static isValidCarNames(carNames) {
-    if (Input.isEmptyName(carNames) === '') {
-      return false;
-    }
-    if (Input.isLongerThanFive(carNames)) {
-      return false;
-    }
-    if (Input.isIncludeSpaceInName(carNames)) {
-      return false;
-    }
-    return true;
-  }
-
-  static isAboveZero(repeatCount) {
-    return Number(repeatCount) > 0;
-  }
-
   setCarNames(carNames) {
-    if (Input.isValidCarNames(carNames)) {
+    if (isValidCarNames(carNames)) {
       this.carNames = carNames;
       return;
     }
@@ -42,7 +42,7 @@ export default class Input {
   }
 
   setRepeatCount(repeatCount) {
-    if (Input.isAboveZero(repeatCount)) {
+    if (isAboveZero(repeatCount)) {
       this.repeatCount = repeatCount;
       return;
     }
