d4cc78d3ec04d7aa5c05e2b9e93354fcfede3ca9
nan-noo <uni613@naver.com>
2021-12-06 16:31:42 +0900

    feat: check car name input value
    Added function
    - isCorrectCarName

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+// functions
+function isCorrectCarName(carNameInputValue) {
+    const wrongNames = carNameInputValue
+        .split(',')
+        .filter(name => name.length > 5);
+    if(!wrongNames.length) return true;
+    else return false;
+}
+
 // DOM
 const app = document.querySelector("#app");
 const carNameInput = document.querySelector("#car-names-input");
@@ -7,7 +16,7 @@ app.addEventListener("click", function(e) {
     e.preventDefault();
     
     if(e.target.id === "car-names-submit") {
-        console.log(carNameInput.value);
+        if(isCorrectCarName(carNameInput.value)) console.log(carNameInput.value);
     }
     else if(e.target.id === "racing-count-submit") {
         console.log(countInput.value);
