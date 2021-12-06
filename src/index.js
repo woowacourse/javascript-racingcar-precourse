145f638c56f0050f2f7515aeb36e43af3d3fa3f3
nan-noo <uni613@naver.com>
2021-12-06 20:57:27 +0900

    feat: add wrong input alert

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 function isCorrectCarName(carNameInputValue) {
     const wrongNames = carNameInputValue
         .split(',')
-        .filter(name => name.length > 5);
+        .filter(name => name.length > 5 || name.length < 1);
     if(!wrongNames.length) return true;
     else return false;
 }
@@ -20,11 +20,20 @@ app.addEventListener("click", function(e) {
     e.preventDefault();
     
     if(e.target.id === "car-names-submit") {
-        if(isCorrectCarName(carNameInput.value)) console.log(carNameInput.value);
+        if(!isCorrectCarName(carNameInput.value)) {
+            alert("잘못된 입력 형식입니다. 쉼표로 구분하여 각각 5자 이하로 입력해주세요.");
+            carNameInput.value = '';
+            carNameInput.focus();
+        }
     }
     else if(e.target.id === "racing-count-submit") {
         if(isCorrectCount(countInput.value)) {
             console.log(countInput.value);
+        }
+        else {
+            alert("잘못된 입력 형식입니다. 1 이상의 정수를 입력해주세요.");
+            countInput.value = '';
+            countInput.focus();
         }
     }
 });
