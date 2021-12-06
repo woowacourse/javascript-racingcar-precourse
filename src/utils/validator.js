d406addff5447b34efa45239635109c6d873ab25
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-12-06 11:05:40 +0900

    refactor: 유효성 체크 부분 구조 개선

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,12 +2,8 @@ const isOnlyNumbers = (value) => /^[0-9]*$/g.test(value) && value > 0;
 
 const isWithComma = (value) => value.indexOf(',') > -1;
 
-const isLengthLimit = (values, min, max) => {
-  const validList = values.filter(
-    (item) => item.length >= min && item.length <= max
-  );
-  return validList.length === values.length;
-};
+const isLengthLimit = (values, min, max) =>
+  values.every((item) => item.length >= min && item.length <= max);
 
 const isUniqueWord = (values) => values.length === new Set(values).size;
 
