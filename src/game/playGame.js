444a75e07b13d17054130d7891bcf56ff7f3d58d
DomMorello <zuzudnf@gmail.com>
2021-12-06 18:03:01 +0900

    style: playGame.js 공백 제거

--- src/game/playGame.js
+++ src/game/playGame.js
@@ -2,7 +2,7 @@ import GameController from './GameController.js';
 
 export default function playGame(userInput, view) {
   const controller = new GameController(userInput);
-  
+
   view.initView();
   controller.repeatRace();
   controller.decideWinners();
