2b40f5396536c96fb296494cab10f1af94424555
leezer94 <2kunhee94@gmail.com>
2021-12-06 22:31:27 +0900

    feat: Input, Button관련 기능 추가 1. 유효성 검사 실패시 유저 입력창 클리어 해주는 기능 생성
    2. 유효성 검사 성공시 유저 입력창과 버튼 비활성화 기능 생성

--- test/src/utils/get-user-input.js
+++ test/src/utils/get-user-input.js
@@ -11,3 +11,31 @@ export function getRacingCountInput() {
 
   return racingCountInputValue;
 }
+
+export function clearCarNamesInput() {
+  const carNamesInput = document.querySelector('#car-names-input');
+
+  carNamesInput.value = '';
+}
+
+export function clearRacingCountInput() {
+  const racingCountInput = document.querySelector('#racing-count-input');
+
+  racingCountInput.value = '';
+}
+
+export const carNamesDisabled = function () {
+  const carNamesButton = document.querySelector('#car-names-submit');
+  const carNamesInput = document.querySelector('#car-names-input');
+
+  carNamesInput.disabled = true;
+  carNamesButton.disabled = true;
+};
+
+export const racingCountDisabled = function () {
+  const racingCountButton = document.querySelector('#racing-count-submit');
+  const racingCountInput = document.querySelector('#racing-count-input');
+
+  racingCountButton.disabled = true;
+  racingCountInput.disabled = true;
+};
