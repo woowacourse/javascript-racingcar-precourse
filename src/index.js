737b59b52cdc831418e9f427098b2968f63a838c
nan-noo <uni613@naver.com>
2021-12-06 22:02:53 +0900

    refactor: addEventListener
    Separate functions
    - onCarNameSubmit
    - onRacingCountSubmit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // functions
 function isCorrectCarName(carNameInputValue) {
     const wrongNames = carNameInputValue
@@ -15,6 +17,32 @@ function gameStart(carNameArray, racingCount) {
     console.log(carNameArray, racingCount);
 }
 
+function onCarNameSubmit() {
+    if(!isCorrectCarName(carNameInput.value)) {
+        alert("잘못된 입력 형식입니다. 쉼표로 구분하여 각각 5자 이하로 입력해주세요.");
+        carNameInput.value = '';
+        carNameInput.focus();
+    }
+}
+
+function onRacingCountSubmit() {
+    if(isCorrectCount(countInput.value)) {
+        if(!isCorrectCarName(carNameInput.value)) {
+            alert("자동차 이름을 형식에 맞춰 입력해 주세요.");
+            carNameInput.value = '';
+            carNameInput.focus();
+        }
+        else {
+            gameStart(carNameInput.value.split(','), Number(countInput.value));
+        }
+    }
+    else {
+        alert("잘못된 입력 형식입니다. 1 이상의 정수를 입력해주세요.");
+        countInput.value = '';
+        countInput.focus();
+    }
+}
+
 // DOM
 const app = document.querySelector("#app");
 const carNameInput = document.querySelector("#car-names-input");
@@ -22,29 +50,10 @@ const countInput = document.querySelector("#racing-count-input");
 
 app.addEventListener("click", function(e) {
     e.preventDefault();
-    
-    if(e.target.id === "car-names-submit") {
-        if(!isCorrectCarName(carNameInput.value)) {
-            alert("잘못된 입력 형식입니다. 쉼표로 구분하여 각각 5자 이하로 입력해주세요.");
-            carNameInput.value = '';
-            carNameInput.focus();
-        }
-    }
-    else if(e.target.id === "racing-count-submit") {
-        if(isCorrectCount(countInput.value)) {
-            if(!isCorrectCarName(carNameInput.value)) {
-                alert("자동차 이름을 형식에 맞춰 입력해 주세요.");
-                carNameInput.value = '';
-                carNameInput.focus();
-            }
-            else {
-                gameStart(carNameInput.value.split(','), Number(countInput.value));
-            }
-        }
-        else {
-            alert("잘못된 입력 형식입니다. 1 이상의 정수를 입력해주세요.");
-            countInput.value = '';
-            countInput.focus();
-        }
-    }
+
+    const handleFunctions = {
+        "car-names-submit"() { onCarNameSubmit(); },
+        "racing-count-submit"() { onRacingCountSubmit(); },
+    };
+    if(Object.keys(handleFunctions).includes(e.target.id)) handleFunctions[e.target.id]();
 });
