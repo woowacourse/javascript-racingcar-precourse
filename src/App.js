4416c2e0885a57ad479dc0e426c7eb9f1494f13f
woose28 <dldntmdsla@naver.com>
2021-12-06 18:16:32 +0900

    feat: '게임에 필요한 DOM 엘리먼트 삽입하기' 구현
    1. id가 app인 div 엘리먼트의 자식 노드로 게임에 필요한 DOM 엘리먼트를 삽입
    삽입한 DOM 엘리먼트 목록
    - 실행 결과를 보여줄 div 엘리먼트(id = result)
    - '최종 우승자' 텍스트를 보여줄 span 엘리먼트(id = racing-winners-title)
    - 최종 우승자를 출력하는 span 엘리먼트(id = racing-winners)

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ import {
 } from './utils/validation.js';
 
 import {
+  ID_APP,
   ID_CAR_NAMES_INPUT,
   ID_CAR_NAMES_SUBMIT,
   ID_RACING_COUNT_INPUT,
   ID_RACING_COUNT_SUBMIT,
+  ID_RSULT,
+  ID_RACING_WINNERS_TITLE,
+  ID_RACING_WINNERS,
 } from './constant/selector.js';
 import { INPUT_NAME_ERROR, INPUT_RACING_COUNT_ERROR } from './constant/error.js';
 
@@ -20,6 +24,7 @@ export default class App {
   constructor() {
     this.cars = [];
     this.racingCount = null;
+    this.$app = document.getElementById(ID_APP);
     this.$carNamesInput = document.getElementById(ID_CAR_NAMES_INPUT);
     this.$carNamesSubmit = document.getElementById(ID_CAR_NAMES_SUBMIT);
     this.$racingCountInput = document.getElementById(ID_RACING_COUNT_INPUT);
@@ -27,7 +32,42 @@ export default class App {
   }
 
   init() {
+    this.addElement();
     this.addEvent();
+  }
+
+  addElement() {
+    this.addResultElement();
+    this.addRacingWinnersTitleElement();
+    this.addRacingWinnersElement();
+  }
+
+  addResultElement() {
+    const $result = document.createElement('div');
+    $result.setAttribute('id', ID_RSULT);
+    $result.setAttribute('hidden', true);
+    
+    this.$app.appendChild($result);
+
+    this.$result = $result;
+  }
+
+  addRacingWinnersTitleElement() {
+    const $racingWinnersTitle = document.createElement('span');
+    $racingWinnersTitle.setAttribute('id', ID_RACING_WINNERS_TITLE,);
+    $racingWinnersTitle.setAttribute('hidden', true);
+    this.$app.appendChild($racingWinnersTitle);
+
+    this.$racingWinnersTitle = $racingWinnersTitle
+  }
+
+  addRacingWinnersElement() {
+    const $racingWinners = document.createElement('span');
+    $racingWinners.setAttribute('id', ID_RACING_WINNERS);
+    $racingWinners.setAttribute('hidden', true);
+    this.$app.appendChild($racingWinners);
+
+    this.$racingWinners = $racingWinners;
   }
 
   addEvent() {
--- src/constant/selector.js
+++ src/constant/selector.js
@@ -1,12 +1,22 @@
+const ID_APP = 'app';
+
 const ID_CAR_NAMES_INPUT = 'car-names-input';
 const ID_CAR_NAMES_SUBMIT = 'car-names-submit';
 
 const ID_RACING_COUNT_INPUT = 'racing-count-input';
 const ID_RACING_COUNT_SUBMIT = 'racing-count-submit';
 
+const ID_RSULT = 'result';
+const ID_RACING_WINNERS_TITLE = 'racing-winners-title';
+const ID_RACING_WINNERS = 'racing-winners';
+
 export { 
+  ID_APP,
   ID_CAR_NAMES_INPUT,
   ID_CAR_NAMES_SUBMIT,
   ID_RACING_COUNT_INPUT,
   ID_RACING_COUNT_SUBMIT,
+  ID_RSULT,
+  ID_RACING_WINNERS_TITLE,
+  ID_RACING_WINNERS,
 };
