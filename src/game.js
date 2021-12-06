89b75b85e56a0eb16cc976cfc2b1db8350831c1a
dugazada <dugazada@gmail.com>
2021-12-07 06:25:36 +0900

    feat: 무작위 값 4 이상인 경우 이동 기능 구현

--- src/game.js
+++ src/game.js
@@ -1,5 +1,14 @@
 import Car from './car.js';
 
+export function isStepValid() {
+  const step = makeRandomNum();
+  let validStep = 0;
+  if (step >= 4) {
+    validStep = 1;
+  }
+  return validStep;
+}
+
 export function getCarObjList(...cars) {
   const carObjList = [];
   cars.forEach((car) => {
