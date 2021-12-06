4b48e9b63e088e8c6d0acbfa2ec6d4f5575c68d9
nan-noo <uni613@naver.com>
2021-12-06 16:13:32 +0900

    feat: get count input

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // DOM
 const app = document.querySelector("#app");
 const carNameInput = document.querySelector("#car-names-input");
+const countInput = document.querySelector("#racing-count-input");
 
 app.addEventListener("click", function(e) {
     e.preventDefault();
     
     if(e.target.id === "car-names-submit") {
         console.log(carNameInput.value);
+    }
+    else if(e.target.id === "racing-count-submit") {
+        console.log(countInput.value);
     }
 });
