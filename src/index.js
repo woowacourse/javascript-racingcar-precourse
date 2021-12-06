3c1ae878f246c7c6d6ed739e934eee2688ad22ec
NaRae-tech <narae.dev@gmail.com>
2021-12-06 19:05:28 +0900

    modify function to class

--- src/index.js
+++ src/index.js
@@ -5,46 +5,47 @@ import InputCount from './userInput/inputCount.js';
 import drawResultContainer from './draw/drawResultContainer.js';
 import Play from './play.js';
 
-export default function CarRacing() {
-  this.gameObject = {
-    gameCount : 0,
-    carNameList: [],
-    carList:[]
+export class CarRacing {
+  constructor() {
+    this.gameObject = {
+      gameCount: 0,
+      carNameList: [],
+      carList: []
+    };
+    this.start = () => {
+      drawResultContainer();
+      addCarNamesEventListener();
+    };
+    const addCarNamesEventListener = () => {
+      const carNamesContainer = $('#car-names-form');
+      const carNamesSubmitBtn = $('#car-names-submit');
+      carNamesContainer.addEventListener('submit', (e) => {
+        e.preventDefault();
+      });
+      carNamesSubmitBtn.addEventListener('click', () => {
+        this.gameObject.carNameList = InputCars();
+        if (this.gameObject.carNameList) {
+          this.gameObject.carList = makeCarList(this.gameObject.carNameList);
+          carNamesSubmitBtn.disabled = true;
+          addCountEventListener();
+        }
+      });
+    };
+    const addCountEventListener = () => {
+      const racingCountSubmitContainer = $('#racing-count-form');
+      const racingCountSubmitBtn = $('#racing-count-submit');
+      racingCountSubmitContainer.addEventListener('submit', e => {
+        e.preventDefault();
+      });
+      racingCountSubmitBtn.addEventListener('click', () => {
+        this.gameObject.gameCount = InputCount();
+        if (this.gameObject.gameCount) {
+          racingCountSubmitBtn.disabled = true;
+          Play(this.gameObject);
+        }
+      });
+    };
   }
-  this.start = () => {
-    drawResultContainer();
-    addCarNamesEventListener();
-  }
-  const addCarNamesEventListener = () => {
-    const carNamesContainer = $('#car-names-form');
-    const carNamesSubmitBtn = $('#car-names-submit');
-    carNamesContainer.addEventListener('submit', (e) => {
-      e.preventDefault();
-    })
-    carNamesSubmitBtn.addEventListener('click', () => {
-      this.gameObject.carNameList = InputCars();
-      if (this.gameObject.carNameList) {
-        this.gameObject.carList = makeCarList(this.gameObject.carNameList);
-        carNamesSubmitBtn.disabled = true;
-        addCountEventListener();
-      } 
-    });
-  }
-  const addCountEventListener = () => {
-    const racingCountSubmitContainer = $('#racing-count-form');
-    const racingCountSubmitBtn = $('#racing-count-submit');
-    racingCountSubmitContainer.addEventListener('submit', e => {
-      e.preventDefault();
-    })
-    racingCountSubmitBtn.addEventListener('click', () => {
-      this.gameObject.gameCount = InputCount();
-      if (this.gameObject.gameCount) {
-        racingCountSubmitBtn.disabled = true;
-        Play(this.gameObject);
-      }
-    });
-  }
-  
 }
 const newgame = new CarRacing();
 newgame.start();
