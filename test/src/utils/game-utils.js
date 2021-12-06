f352ee90f6eccd21eec1046e913906c4e3f869e5
leezer94 <2kunhee94@gmail.com>
2021-12-06 22:51:11 +0900

    feat: 랜덤넘버 관련 함수 구현
    1. 라이브러리 이용 랜덤한 숫자 생성
    
    2. 생성한 랜덤한 숫자 유저 입력 레이싱카운트 숫자만큼 position에 push

--- test/src/utils/game-utils.js
+++ test/src/utils/game-utils.js
@@ -1,5 +1,23 @@
+import { MAX_NUMBER, MIN_NUMBER } from '../constants/constants.js';
 import { state } from '../index.js';
 
 export const makeNewCars = function (carNamesArray) {
   carNamesArray.forEach((name) => state.carArray.push(new carNamesArray(name)));
+};
+
+export const generateRandomNumber = function () {
+  const randomNumber = MissionUtils.Random.pickNumberInRange(
+    MIN_NUMBER,
+    MAX_NUMBER
+  );
+
+  return randomNumber;
+};
+
+export const addRandomPosition = function (racingCount) {
+  for (let i = 0; i < racingCount; i++) {
+    for (let j = 0; j < state.carArray.length; j++) {
+      state.carArray[j].position.push(getRandomNumber());
+    }
+  }
 };
