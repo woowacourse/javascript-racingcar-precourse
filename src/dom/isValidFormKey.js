087b66ca70ababf785e29f55e28df4fdf4e925a8
leegwae <leegwae@gmail.com>
2021-12-06 20:32:56 +0900

    fix: form 관리 키 유효성 검사 함수 수정

--- src/dom/isValidFormKey.js
+++ src/dom/isValidFormKey.js
@@ -1,7 +1,7 @@
 import { KEY_FORM_CAR_NAMES, KEY_FORM_RACING_COUNT } from './const.js';
 
 function isValidFormKey(formKey) {
-  return formKey !== KEY_FORM_CAR_NAMES || formKey !== KEY_FORM_RACING_COUNT;
+  return formKey === KEY_FORM_CAR_NAMES || formKey === KEY_FORM_RACING_COUNT;
 }
 
 export default isValidFormKey;
