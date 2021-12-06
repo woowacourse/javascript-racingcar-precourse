09837899f3fcad25c1a0cfc90d10a0e5ed4ed47f
leezer94 <2kunhee94@gmail.com>
2021-12-06 22:27:03 +0900

    feat: 유효성검사에 필요한 상수 추가

---
+++ test/src/constants/constants.js
@@ -0,0 +1,15 @@
+export const MAX_NUMBER = 9;
+
+export const MIN_NUMBER = 1;
+
+export const POSITION_CONDITION_NUMBER = 4;
+
+export const MAX_CARNAME_LENGTH = 5;
+
+export const MIN_CARNAME_LENGTH = 1;
+
+export const MIN_NUMBER_OF_CAR = 2;
+
+export const MIN_NUMBER_OF_RACINGCOUNT = 1;
+
+export const EMPTY = 0;
