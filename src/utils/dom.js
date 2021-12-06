395d3ccde812001f837217f789762dee6281a6c1
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-12-06 17:24:16 +0900

    refactor: createElement 함수화
    - document.element()함수를 사용하여 안에 text를 넣는 경우가 많아 이를
      함수화 하여서 적용시켰다

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,42 +1,5 @@
 import { STYLE, COMMENT, SELECTOR } from '../constants/constant.js';
 
-export const hideNode = ($node) => {
-  $node.style.display = 'none';
-};
-export const showNode = ($node) => {
-  $node.style.display = 'block';
-};
-
-export const createProgressNode = (name, position) => {
-  const $node = document.createElement('span');
-  const $brNode = document.createElement('br');
-  let paragraph = name + ': ';
-  while (position--) {
-    paragraph += '-';
-  }
-
-  const textNode = document.createTextNode(paragraph);
-  $node.appendChild(textNode);
-  $node.appendChild($brNode);
-  return $node;
-};
-
-export const createResultNode = (cars) => {
-  const $resultNode = document.createElement('div');
-  cars.forEach((car) => {
-    $resultNode.appendChild(car.positionElement);
-  });
-
-  $resultNode.style.margin =
-    STYLE.RESULT_MARGIN_VERTICAL + ' ' + STYLE.RESULT_MARGIN_HORIZONTAL;
-
-  return $resultNode;
-};
-
-export const disableForm = ($form) => {
-  $form.childNodes.forEach((node) => (node.disabled = true));
-};
-
 const createElement = (tag, text, id) => {
   const $container = document.createElement(tag);
 
@@ -50,6 +13,41 @@ const createElement = (tag, text, id) => {
   }
 
   return $container;
+};
+
+export const hideNode = ($node) => {
+  $node.style.display = 'none';
+};
+export const showNode = ($node) => {
+  $node.style.display = 'block';
+};
+
+export const createProgressNode = (name, position) => {
+  let paragraph = name + ': ';
+  while (position--) {
+    paragraph += '-';
+  }
+
+  const $node = createElement('span', paragraph);
+  const $brNode = createElement('br');
+  $node.appendChild($brNode);
+  return $node;
+};
+
+export const createResultNode = (cars) => {
+  const $resultNode = createElement('div');
+  cars.forEach((car) => {
+    $resultNode.appendChild(car.positionElement);
+  });
+
+  $resultNode.style.margin =
+    STYLE.RESULT_MARGIN_VERTICAL + ' ' + STYLE.RESULT_MARGIN_HORIZONTAL;
+
+  return $resultNode;
+};
+
+export const disableForm = ($form) => {
+  $form.childNodes.forEach((node) => (node.disabled = true));
 };
 
 export class Winners {
