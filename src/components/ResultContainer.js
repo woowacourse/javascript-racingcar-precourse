5e530cdc1631ab77a34aa7deec158b99d7376d6c
ziho <ziho@kakao.com>
2021-12-07 00:09:53 +0900

    feat: 매 회마다 게임 결과 출력 구현

--- src/components/ResultContainer.js
+++ src/components/ResultContainer.js
@@ -1,4 +1,4 @@
-import {NUM_RULE} from '../utils/constants.js';
+import {DOM_ID, NUM_RULE} from '../utils/constants.js';
 import {generateRandomNumber, showDisplayContainer} from '../utils/util.js';
 import Component from './Component.js';
 
@@ -26,6 +26,24 @@ export default class ResultContainer extends Component {
   raicing() {
     for (let i = 0; i < this.$props.count; i = i + 1) {
       this.moveCar();
+      this.$props.cars.forEach(({name, position}) => {
+        this.printOneBlock(name, position);
+      });
+
+      const br = document.createElement('br');
+      this.$target.appendChild(br);
     }
+  }
+
+  printOneBlock(name, position) {
+    const carContainer = document.createElement('div');
+    const carText = document.createElement('span');
+    let dash = '';
+    for (let i = 0; i < position; i = i + 1) {
+      dash = dash + '-';
+    }
+    carText.innerText = `${name}: ${dash}`;
+    carContainer.appendChild(carText);
+    this.$target.appendChild(carContainer);
   }
 }
