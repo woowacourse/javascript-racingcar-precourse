9cd7494553cbae2509220186348c3996614ee181
dugazada <dugazada@gmail.com>
2021-12-07 01:15:28 +0900

    feat: 경주 횟수 입력 기능 구현

--- src/index.js
+++ src/index.js
@@ -6,19 +6,26 @@ export default class RacingcarGame {
 
   getInput() {
     this.$carListInput = document.getElementById('car-names-input');
-    this.$raceCount = document.getElementById('racing-count-input');
+    this.$raceCountInput = document.getElementById('racing-count-input');
   }
 
   onSubmit() {
     const $carListSubmit = document.getElementById('car-names-submit');
     const $raceCountSubmit = document.getElementById('racing-count-submit');
     $carListSubmit.addEventListener('click', this.getCarList.bind(this));
+    $raceCountSubmit.addEventListener('click', this.getRaceCount.bind(this));
   }
 
   getCarList(e) {
     e.preventDefault();
     const carList = this.$carListInput.value.split(',');
-    console.log(carList);
+    return carList;
+  }
+
+  getRaceCount(e) {
+    e.preventDefault();
+    const raceCount = this.$raceCountInput.value;
+    return raceCount;
   }
 }
 
