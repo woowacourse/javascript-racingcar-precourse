be247b407847704d1a760e80294a11d16607b9b9
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-06 17:43:25 +0900

    restore: html 내 id 선택자 js로 추가

--- src/components/init.js
+++ src/components/init.js
@@ -1,9 +1,5 @@
 export default function init() {
-  const [countTitle, resultTitle] = document.querySelectorAll('h4');
-
-  countTitle.setAttribute('id', 'count-title');
-  resultTitle.setAttribute('id', 'result-title');
-
+  setElement();
   createResultDiv();
 }
 
@@ -11,4 +7,17 @@ function createResultDiv() {
   const resultDiv = document.createElement('div');
   resultDiv.setAttribute('id', 'result-div');
   document.querySelector('#app').appendChild(resultDiv);
+}
+
+function setElement() {
+  const [countTitle, resultTitle] = document.querySelectorAll('h4');
+  const [carNameInput, countInput] = document.querySelectorAll('input');
+  const [carNameSubmit, countSubmit] = document.querySelectorAll('button');
+
+  countTitle.setAttribute('id', 'count-title');
+  resultTitle.setAttribute('id', 'result-title');
+  carNameInput.setAttribute('id', 'car-names-input');
+  countInput.setAttribute('id', 'racing-count-input');
+  carNameSubmit.setAttribute('id', 'car-names-submit');
+  countSubmit.setAttribute('id', 'racing-count-submit');
 }
