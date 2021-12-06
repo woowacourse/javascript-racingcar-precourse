07cef726da48fe5ab5234a37fdf7bb1d8e727a09
yongrae <kyr9389@naver.com>
2021-12-06 23:55:05 +0900

    feat: 사용자가 잘못된 입력 값을 작성하면 경고하기
    - isValidate()를 이용하여 사용자의 입력
      값을 검증
    - 자동차의 이름이 5자를 초과하는 경우
    - 입력값이 1보다 작을 경우

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import CarList from "./car/CarList.js";
+import isValidate from "./utils/isValidate.js";
 
 const formTag = document.querySelectorAll("form");
 const carNameButton = document.getElementById("car-names-submit");
@@ -22,6 +23,9 @@ carNameButton.addEventListener('click', event => {
 racingCountButton.addEventListener('click', event => {
     const racingCountInput = document.getElementById("racing-count-input").value;
 
-    carList.game(racingCountInput);
-    console.log(carList);
+    if(!isValidate("count", racingCountInput)) alert("잘못된 숫자를 입력하셨습니다.");
+    else {
+        carList.game(racingCountInput);
+        console.log(carList);
+    }
 });
