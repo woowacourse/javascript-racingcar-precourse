fc53e0ff2e79eb1624802491aafbd7faed8501c0
leezer94 <2kunhee94@gmail.com>
2021-12-06 22:13:46 +0900

    chore: 의존성 설치 1. ESLinter, Prettier, Airbnb config 설치 2. HTML Element 선택자 추가

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:cypress/recommended',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'class-methods-use-this': 'off',
+    'no-alert': 'off',
+    'consistent-return': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 100,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
