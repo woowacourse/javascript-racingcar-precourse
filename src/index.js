25b6ac5fdbd53278a187a783d0615c90fd1f8264
nan-noo <uni613@naver.com>
2021-12-06 20:50:23 +0900

    feat: check racing count value
    Added function
    - isCorrectCount

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ function isCorrectCarName(carNameInputValue) {
     else return false;
 }
 
+function isCorrectCount(countInputValue) {
+    return Number.isInteger(Number(countInputValue)) && countInputValue >= 1;
+}
+
 // DOM
 const app = document.querySelector("#app");
 const carNameInput = document.querySelector("#car-names-input");
@@ -19,6 +23,8 @@ app.addEventListener("click", function(e) {
         if(isCorrectCarName(carNameInput.value)) console.log(carNameInput.value);
     }
     else if(e.target.id === "racing-count-submit") {
-        console.log(countInput.value);
+        if(isCorrectCount(countInput.value)) {
+            console.log(countInput.value);
+        }
     }
 });
