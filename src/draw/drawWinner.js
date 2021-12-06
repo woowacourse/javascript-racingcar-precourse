90ea6c1000552686b6a9b3851a4386815bf04dca
NaRae-tech <narae.dev@gmail.com>
2021-12-06 19:20:19 +0900

    add winner title

--- src/draw/drawWinner.js
+++ src/draw/drawWinner.js
@@ -4,8 +4,11 @@ import { $ } from '../dom/dom.js';
 export default function DrawWinner(carList) {
   let winners = ComputeWinner(carList);
   let winnerString = winners.join(',');
-  const winner = document.createElement('p');
+  const winnerTitle = document.createElement('span');
+  winnerTitle.innerHTML = '최종 우승자 : ';
+  const winner = document.createElement('span');
   winner.id = "racing-winners";
   winner.innerHTML=`${winnerString}`;
+  $('#racing-result-container').append(winnerTitle);
   $('#racing-result-container').append(winner);
 }
