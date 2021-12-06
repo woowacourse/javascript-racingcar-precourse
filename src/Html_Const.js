509a7fe22e4c920717187be4e7c4fab70ab97e78
mieumje <66072832+mieumje@users.noreply.github.com>
2021-12-06 20:53:18 +0900

    Feat : 시도 횟수 입력 숨기기
    자동차 이름 입력이 올바르게 전달되기 전까지 시도 횟수 입력 숨김

--- src/Html_Const.js
+++ src/Html_Const.js
@@ -3,3 +3,7 @@ export const $Car_names_submit = document.querySelector('#car-names-submit');
 export const $Racing_count_input = document.querySelector('#racing-count-input');
 export const $Racing_count_submit = document.querySelector('#racing-count-submit');
 export const $Racing_winners = document.querySelector('#racing-winners');
+
+
+$Racing_count_submit.style.display = 'none';
+$Racing_count_input.style.display = 'none';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { Car } from "./Car.js";
 import { $Car_names_input, $Car_names_submit, $Racing_count_input, $Racing_count_submit } from "./Html_Const.js";
 import { splice_names } from "./Check_CarNames.js";
 import { check_numbers } from "./Check_TryNumbers.js";
@@ -14,6 +13,8 @@ export default function RacingCarGame() {
         //const arr = splice_names(userInputCarNames);
         carArray = splice_names(userInputCarNames);
         console.log(carArray);
+        $Racing_count_submit.style.display = '';
+        $Racing_count_input.style.display = '';
     });
 
     $Racing_count_submit.addEventListener('click', (e) => {
