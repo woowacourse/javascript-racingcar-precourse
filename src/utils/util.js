91e77643f4338a2421cf31f5cfd54d9acc01c010
ziho <ziho@kakao.com>
2021-12-06 23:23:13 +0900

    feat: 노드리스트 hide, 컨테이너 show 유틸 함수 추가

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,5 @@
+import {NUM_RULE} from './constants.js';
+
 export const showErrorMessage = ($target, errorMessage) => {
   alert(errorMessage);
   $target.value = '';
@@ -10,4 +12,20 @@ export const showElement = ($target) => {
 
 export const hideElement = ($target) => {
   $target.style.display = 'none';
+};
+
+export const hideAllElements = (nodeList) => {
+  nodeList.forEach((node) => {
+    hideElement(node);
+  });
+};
+
+export const showDisplayContainer = ($target, $title) => {
+  showElement($target);
+  showElement($title);
+};
+
+export const generateRandomNumber = () => {
+  // eslint-disable-next-line no-undef
+  return MissionUtils.Random.pickNumberInRange(NUM_RULE.MIN_NUM, NUM_RULE.MAX_NUM);
 };
