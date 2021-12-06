8af66ac68b9602dd47efade3b7cf14c21c78c423
Leejin-Yang <ejin1996@gmail.com>
2021-12-07 03:40:03 +0900

    feat: 자동차 경주 게임 구현

---
+++ src/racingCarGame.js
@@ -0,0 +1,48 @@
+import CarNamesForm from './components/carNamesForm.js';
+import RacingCountForm from './components/racingCountForm.js';
+import RacingResult from './components/racingResult.js';
+
+export default function RacingCarGame(app) {
+  this.state = { cars: [], count: 0 };
+
+  const setCars = (racingCars) => {
+    this.state.cars = racingCars;
+    console.log(this.state.cars);
+  };
+
+  const setCount = (racingCount) => {
+    this.state.count = racingCount;
+    console.log(this.state.count);
+  };
+
+  const racing = () => {
+    for (let i = 0; i < this.state.count; i++) {
+      this.state.cars.forEach((car) => car.move());
+
+      const racingProcess = this.state.cars
+        .map((car) => `<span>${car.name}: ${car.distance}</span>`)
+        .join('');
+
+      this.racingResult.printProcess(racingProcess);
+    }
+
+    this.racingResult.printWinner(getWinner());
+  };
+
+  const getWinner = () => {
+    const carsDistance = this.state.cars.map((car) => car.distance.length);
+    const maxDistance = Math.max(...carsDistance);
+    const winner = this.state.cars
+      .filter((car) => car.distance.length === maxDistance)
+      .map((car) => car.name)
+      .join(', ');
+
+    return winner;
+  };
+
+  this.render = () => {
+    this.carNamesForm = CarNamesForm(setCars);
+    this.racingCountForm = RacingCountForm(setCount, racing);
+    this.racingResult = new RacingResult(app);
+  };
+}
