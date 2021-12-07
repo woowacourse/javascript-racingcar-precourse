8ff6da37298b6fe54726390d2d8b107bf9b14c21
gxxrxn <kkr1226@naver.com>
2021-12-07 23:33:01 +0900

    refactor(Render): 필요한 template 메소드만 남기고 제거

--- src/view/Render.js
+++ src/view/Render.js
@@ -3,12 +3,4 @@ export default class Render {
     $child.innerHTML = template;
     $parent.appendChild($child);
   };
-
-  hidden = $target => {
-    $target.style.visibility = 'hidden';
-  };
-
-  visible = $target => {
-    $target.style.visibility = 'visible';
-  };
 }
