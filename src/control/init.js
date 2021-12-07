7cd4b7ce0c9f570fbeafdfcb0fca979e6b5e6b51
simyeen <simyeen@naver.com>
2021-12-07 17:08:51 +0900

    Refact : 불필요한 함수 제거

--- src/control/init.js
+++ src/control/init.js
@@ -1,10 +1,6 @@
 import { SELECTOR_NUMBERS } from "../common/const.js";
 import startGame from "./startGame.js";
 
-function setDocumentId(seletor, id) {
-  seletor.id = `${id}`;
-}
-
 function addEventHandler(seletor) {
   seletor.addEventListener("click", (event) => {
     event.preventDefault();
@@ -21,10 +17,10 @@ export default function init() {
   const $racingCountInput = domInputArray[SELECTOR_NUMBERS.RACING_NUMBER];
   const $racingCountSubmit = domButtonArray[SELECTOR_NUMBERS.RACING_NUMBER];
 
-  setDocumentId($carNamesInput, "car-names-input");
-  setDocumentId($carNamesSubmit, "car-names-submit");
-  setDocumentId($racingCountInput, "racing-count-input");
-  setDocumentId($racingCountSubmit, "racing-count-submit");
+  $carNamesInput.id = "car-names-input";
+  $carNamesSubmit.id = "car-names-submit";
+  $racingCountInput.id = "racing-count-input";
+  $racingCountSubmit.id = "racing-count-submit";
 
   addEventHandler($carNamesSubmit);
   addEventHandler($racingCountSubmit);
