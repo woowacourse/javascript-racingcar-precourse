0387526d615261ea76ab932ac4acbf7da72ba58b
sangminsong <ssmin0606@hanmail.net>
2021-12-07 17:06:24 +0900

    refactor: 에러 메시지 내부 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,9 +1,10 @@
+import NUMBER from './number.js';
+
 const MESSAGE = {
   ERROR: {
-    NOT_MEET_RACING_COUNT_CONDITION: '1이상의 숫자를 입력해주세요',
-    NOT_MEET_CAR_NAME_CONDITION:
-      '5자이하의 이름을 중복되지 않게 입력해주세요! (공백은 이름으로 설정 불가능합니다)',
-    NOT_MEET_CARS_NUM_CONDITION: '2대이상의 차 이름을 넣어주세요',
+    NOT_MEET_RACING_COUNT_CONDITION: `${NUMBER.MIN_RACING_COUNT}이상의 양의 정수를 입력해주세요`,
+    NOT_MEET_CAR_NAME_CONDITION: `${NUMBER.MAX_CAR_NAME_LENGTH}자이하의 이름을 중복되지 않게 입력해주세요! (공백은 이름으로 설정 불가능합니다)`,
+    NOT_MEET_CARS_NUM_CONDITION: `${NUMBER.MIN_CARS_LENGTH}대 이상의 차 이름을 넣어주세요`,
   },
 };
 
