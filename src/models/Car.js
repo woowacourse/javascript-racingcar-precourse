2121c2c7dbb291f9d1a212646dfc6d4da722a015
최광희 <polyckh@gmail.com>
2021-12-07 14:09:40 +0900

    refactor: Car 객체 constants.js import

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,8 +1,13 @@
+import { NUMBER_RULE } from '../utils/constants';
+
 export default function Car(name) {
   this.name = name;
   this.forwardCount = 0;
   this.race = () => {
-    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-    this.forwardCount += randomNumber >= 4;
+    const randomNumber = MissionUtils.Random.pickNumberInRange(
+      NUMBER_RULE.MIN_NUMBER,
+      NUMBER_RULE.MAX_NUMBER,
+    );
+    this.forwardCount += randomNumber >= NUMBER_RULE.FORWARD_NUMBER;
   };
 }
