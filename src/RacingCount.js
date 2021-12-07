28d82c5f4a5d76e551607325c8485fa3065709d3
Daye Lee <leeda1801@gmail.com>
2021-12-07 15:21:47 +0900

    feat: add exception for invalid racing count submit

--- src/RacingCount.js
+++ src/RacingCount.js
@@ -2,9 +2,18 @@ import { racingCountInput } from './elements.js';
 
 import { checkRacingCountInputValid } from './InputValidation.js';
 
+function onInvalidRacingCountInput() {
+  alert('잘못 입력하셨습니다. 양의 정수를 입력해 주세요.');
+  racingCountInput.value = '';
+  racingCountInput.focus();
+}
+
 export default function onRacingCountSubmit(event) {
   event.preventDefault();
   const racingCount = racingCountInput.value;
   const racingCountInputValid = checkRacingCountInputValid(racingCount);
-  console.log(racingCountInputValid);
+
+  if (!racingCountInputValid) {
+    onInvalidRacingCountInput();
+  }
 }
