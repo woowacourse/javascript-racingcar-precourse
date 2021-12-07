b954e42aa3dc39e32200170b606acb2c5be4ed16
simyeen <simyeen@naver.com>
2021-12-07 17:05:37 +0900

    Add: 게임마다 새로운 결과창 보여주기

--- src/control/startGame.js
+++ src/control/startGame.js
@@ -1,19 +1,16 @@
 import getInputCarNames from "../model/getInputCarNames.js";
 import getInputRunTime from "../model/getInputRunTime.js";
+import resetView from "../view/resetView.js";
 import racingCars from "./racingCars.js";
 
 export default function startGame() {
   let carNames = getInputCarNames();
   let runTime = getInputRunTime();
 
-  const $app = document.getElementById("app");
-  let newDIV = document.createElement("div");
-  newDIV.id = "result";
-  $app.appendChild(newDIV);
-
   console.log("조건 확인", carNames, runTime);
   if (carNames && runTime) {
     console.log("게임 시작");
+    resetView();
     racingCars(carNames, runTime);
   } else {
     console.log("게임 시작 조건 불층분");
