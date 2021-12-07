5770b8d6c5c8ccca8658911031cfbe0e239fdb86
최광희 <polyckh@gmail.com>
2021-12-07 13:32:08 +0900

    feat: 레이싱 횟수 입력 받음

---
+++ src/views/CountFormView.js
@@ -0,0 +1,36 @@
+import { ELEMENT_ID } from '../utils/constants';
+
+export default function NameFormView(el) {
+  this.el = el;
+  this.inputEl = this.el.querySelector(ELEMENT_ID.racingCountInput);
+  this.submitEl = this.el.querySelector(ELEMENT_ID.racingCountSubmit);
+  this.init = () => {
+    this.bindEvent();
+    return this;
+  };
+
+  this.bindEvent = () => {
+    this.submitEl.addEventListener('click', (e) => this.onClickSubmit(e));
+  };
+
+  this.onClickSubmit = (e) => {
+    e.preventDefault();
+    const racingCountInput = this.inputEl.value;
+  };
+
+  this.on = (event, handler) => {
+    this.el.addEventListener(event, handler);
+    return this;
+  };
+
+  this.emit = (event, data) => {
+    const e = new CustomEvent(event, { detail: data });
+    this.el.dispatchEvent(e);
+    return this;
+  };
+
+  this.clearInput = () => {
+    this.inputEl.value = '';
+    this.inputEl.focus();
+  };
+}
