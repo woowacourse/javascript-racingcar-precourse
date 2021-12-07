f6264611638f5ac6c79bdd5d1b9db60c17865afa
hozzijeong <ghwls0905@naver.com>
2021-12-07 21:18:05 +0900

    Feat: hide,show 함수 추가 및 element에 id 추가

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import { checkInputValidate } from "./input.js";
 import { CheckCarName } from "./car.js";
 import { PlayGame } from "./game.js";
+import { showElement, hideElement } from "./view.js";
+
 export default function SetGame() {
     const $racing_count_input = document.getElementById("racing-count-input");
     const $racing_count_btn = document.getElementById("racing-count-submit");
+    const $racing_count_form = document.getElementById("racing-count-form");
+    const $racing_count_h4 = document.getElementById("racing-count-h4");
 
     const $car_names_input = document.getElementById("car-names-input");
     const $car_names_submit = document.getElementById("car-names-submit");
+
+    const $racing_result_h4 = document.getElementById("racing-result-h4");
 
     this.game_information = {
         racing_count: 0,
         car_names: [],
     };
     this.init = () => {
+        hideElement($racing_count_form);
+        hideElement($racing_result_h4);
+        hideElement($racing_count_h4);
         $racing_count_btn.addEventListener("click", function (e) {
             e.preventDefault();
             const input_val = $racing_count_input.value;
-            if (checkInputValidate(input_val)) setRacingCount(input_val);
-            else alert("올바른 값을 입력해주세요");
+            if (checkInputValidate(input_val)) {
+                setRacingCount(input_val);
+                showElement($racing_result_h4);
+            } else {
+                alert("올바른 값을 입력해주세요");
+                $racing_count_input.value = "";
+            }
 
             new PlayGame(getGameInformation());
         });
@@ -25,8 +39,14 @@ export default function SetGame() {
         $car_names_submit.addEventListener("click", function (e) {
             e.preventDefault();
             const names = $car_names_input.value.split(",");
-            if (CheckCarName(names)) setCarNames(names);
-            else alert("올바른 값을 입력해주세요");
+            if (CheckCarName(names)) {
+                setCarNames(names);
+                showElement($racing_count_h4);
+                showElement($racing_count_form);
+            } else {
+                alert("올바른 값을 입력해주세요");
+                $car_names_input.value = "";
+            }
         });
     };
 
