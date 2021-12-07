e7b6650bf1d4aa7916b91b5fe5f615f164b418a8
junzzi <jjh01027717502@gmail.com>
2021-12-07 13:59:46 +0900

    refactor: apply Object.freeze to ERROR_MESSAGE constant

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -11,12 +11,12 @@ export const DOM = Object.freeze({
   RESULT: 'result',
   RESULT_TITLE: 'result-title',
 });
-export const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = Object.freeze({
   CAR_NAME_INCLUDE_SPACE: '자동차 이름에 공백은 포함될 수 없습니다.',
   CAR_NAME_LENGTH_GREATHER_THAN_FIVE: '자동차 이름은 5자 이하입니다.',
   CAR_NAME_DUPLICATED: '자동차 이름은 5자 이하입니다.',
   COUNT_NEGATIVE_NUMBER_OR_ZERO: '0보다 큰 수를 입력해주세요.',
-};
+});
 export const PLAIN_STRING = '';
 export const MINUS_SIGN = '-';
 export const FORWARD_NUMBER_SIGN = 4;
