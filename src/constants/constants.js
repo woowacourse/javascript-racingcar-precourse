a48f911be14a97db76fd3fc21938d85743727b7a
ire4564 <ire4564@gmail.com>
2021-12-07 16:46:23 +0900

    feat: Add Constatns in Constatns.js

---
+++ src/constants/constants.js
@@ -0,0 +1,14 @@
+export const TEXT = {
+  ALERT_IS_WRONG_LENGTH: "자동차 이름은 5글자 내로 정해주세요.",
+  ALERT_IS_WRONG_DEPULICATE: "자동차 이름은 중복될 수 없습니다.",
+};
+
+export const $carNameInput = document.querySelector('#car-names-input');
+
+export const $racingCountInput = document.querySelector('#racing-count-input');
+
+export const $carNameSubmit = document.querySelector('#car-names-submit');
+
+export const $racingCountSubmit = document.querySelector('#racing-count-submit');
+
+
