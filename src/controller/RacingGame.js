5d64f21f148ed399d0a4d33e6c56ad4268661e23
gxxrxn <kkr1226@naver.com>
2021-12-07 23:30:30 +0900

    feat(RacingGame): 자동차 경주게임을 진행하는 RacingGame 구현
    - 자동차 배열과 시도 횟수를 전달받아 인스턴스 생성
    - 라운드별로 시도 횟수만큼 게임 진행 후 결과 저장
    - 최종 우승자 판단 후 배열에 저장

---
+++ src/controller/RacingGame.js
@@ -0,0 +1,61 @@
+import { MAX_SIZE } from '../validation/checker.js';
+
+const { pickNumberInRange } = MissionUtils.Random;
+
+export default class RacingGame {
+  constructor(cars, racingCount) {
+    this._cars = cars;
+    this._racingCount = racingCount;
+    this._roundResultData = [];
+    this._winnerNames = [];
+    this.startGame();
+    this.judgeWinner();
+  }
+
+  getResult = () => {
+    return this._roundResultData;
+  };
+
+  getWinners = () => {
+    return this._winnerNames;
+  };
+
+  startGame = () => {
+    for (let i = 0; i < this._racingCount; i++) {
+      this._roundResultData.push(this.startRound());
+    }
+  };
+
+  startRound = () => {
+    let roundResult = [];
+
+    for (const car of this._cars) {
+      const ramdomValue = pickNumberInRange(1, 9);
+      car.go(ramdomValue);
+      roundResult.push(car.getData());
+    }
+
+    return roundResult;
+  };
+
+  getMaxDistance = datas => {
+    let distances = [];
+    for (const data of datas) {
+      distances.push(data.distance);
+    }
+
+    return Math.max(...distances);
+  };
+
+  judgeWinner = () => {
+    const length = this._roundResultData.length;
+    const finalResultData = this._roundResultData[length - 1];
+    const winDistance = this.getMaxDistance(finalResultData);
+
+    for (const data of finalResultData) {
+      if (data.distance == winDistance) {
+        this._winnerNames.push(data.name);
+      }
+    }
+  };
+}
