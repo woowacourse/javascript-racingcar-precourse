a0910eb86b6331b26805371261d29b2842df4e2f
최광희 <polyckh@gmail.com>
2021-12-07 13:52:08 +0900

    refactor: 숫자 상수, event_Type 상수, alert_message 상수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,20 @@ export const ELEMENT_ID = Object.freeze({
   racingCountSubmit: '#racing-count-submit',
   app: '#app',
 });
+
+export const NUMBER_RULE = Object.freeze({
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 9,
+  FORWARD_NUMBER: 4,
+  MAX_CAR_NAME_LENGTH: 5,
+});
+
+export const ALERT_MESSAGE = Object.freeze({
+  WRONG_COUNT_NUMBER: '시도할 횟수 입력이 올바르지 않습니다.',
+  CAR_NOT_EXIT: '자동차 이름을 입력하고 확인 버튼을 눌러주세요.',
+  WRONG_CAR_NAME_LENGTH: '자동차 이름은 5글자 이하로 입력해 주세요.',
+});
+
+export const EVENT_TYPE = Object.freeze({
+  SUBMIT: '@submit',
+});
