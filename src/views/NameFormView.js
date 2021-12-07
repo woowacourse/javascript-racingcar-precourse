888c7c3531cd0a4dfd7caaeac683d12336c74a40
최광희 <polyckh@gmail.com>
2021-12-07 13:14:57 +0900

    feat: 빈 자동차 이름 입력 시 alert

--- src/views/NameFormView.js
+++ src/views/NameFormView.js
@@ -13,9 +13,13 @@ export default function NameFormView(el) {
 
   this.onClickSubmit = (e) => {
     e.preventDefault();
-    const carNamesInput = this.inputEl.value.split(',');
-    if (!carNamesInput.every((name) => name.length <= 5)) {
+    const carNamesInput = this.inputEl.value;
+    if (!carNamesInput.split(',').every((name) => name.length <= 5)) {
       alert('자동차 이름은 5자 이하입니다.');
+      return;
+    }
+    if (!carNamesInput.length) {
+      alert('자동차 이름이 없습니다.');
       return;
     }
     this.emit('@submit', { input: carNamesInput });
