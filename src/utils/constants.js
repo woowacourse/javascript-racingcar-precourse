31a105b4ef35b3788d3de9b938dd98fe1b31c8a1
최광희 <polyckh@gmail.com>
2021-12-07 14:02:46 +0900

    refactor: validation.js에서 상수들 import해서 사용

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,6 +19,8 @@ export const ALERT_MESSAGE = Object.freeze({
   WRONG_COUNT_NUMBER: '시도할 횟수 입력이 올바르지 않습니다.',
   CAR_NOT_EXIT: '자동차 이름을 입력하고 확인 버튼을 눌러주세요.',
   WRONG_CAR_NAME_LENGTH: '자동차 이름은 5글자 이하로 입력해 주세요.',
+  CAR_INPUT_NOT_EXIT: '빈 자동차 이름 입력입니다.',
+  DUPLICATED_CAR_NAME: '중복된 자동차 이름이 있습니다.',
 });
 
 export const EVENT_TYPE = Object.freeze({
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,18 +1,39 @@
+import { ALERT_MESSAGE, NUMBER_RULE } from './constants';
+
 export default {
   checkValidCarName(carNamesInput) {
     const splittedCarNames = carNamesInput.split(',');
     if (!carNamesInput.length) {
-      alert('자동차 이름이 없습니다.');
+      alert(ALERT_MESSAGE.CAR_INPUT_NOT_EXIT);
       return false;
     }
-    if (!splittedCarNames.every((name) => name.length <= 5)) {
-      alert('자동차 이름은 5자 이하입니다.');
+    if (!isValidCarNamesLength(splittedCarNames)) {
+      alert(ALERT_MESSAGE.WRONG_COUNT_NUMBER);
       return false;
     }
-    if (splittedCarNames.length !== new Set(splittedCarNames).size) {
-      alert('중복된 이름이 있습니다.');
+    if (!isDuplicatedCarNames(splittedCarNames)) {
+      alert(ALERT_MESSAGE.DUPLICATED_CAR_NAME);
+      return false;
+    }
+    return true;
+  },
+  checkValidRacingCount(count) {
+    if (!isPositiveNumber(count)) {
+      alert(ALERT_MESSAGE.WRONG_COUNT_NUMBER);
+      return false;
+    }
+    if (!isCarExist()) {
+      alert(ALERT_MESSAGE.CAR_NOT_EXIT);
       return false;
     }
     return true;
   },
 };
+const isPositiveNumber = (number) => number > 0;
+const isValidCarNamesLength = (carNamesArray) =>
+  carNamesArray.every(
+    (carName) => carName.length <= NUMBER_RULE.MAX_CAR_NAME_LENGTH,
+  );
+const isDuplicatedCarNames = (carNames) =>
+  carNames.length !== new Set(carNames).size;
+const isCarExist = () => CarsModel.getCars().length;
