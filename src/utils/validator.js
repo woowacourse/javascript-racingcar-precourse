075b0f90574fc4511af27edcdf180aace9f2dba2
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-12-07 23:00:46 +0900

    refactor: 유효성 검증 유틸 구조 개선

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,8 +2,10 @@ const isOnlyNumbers = (value) => /^[0-9]*$/g.test(value) && value > 0;
 
 const isWithComma = (value) => value.indexOf(',') > -1;
 
-const isLengthLimit = (values, min, max) =>
-  values.every((item) => item.length >= min && item.length <= max);
+const isLengthLimit = (values, min, max) => {
+  if (values.length === 0) return false;
+  return values.every((item) => item.length >= min && item.length <= max);
+};
 
 const isUniqueWord = (values) => values.length === new Set(values).size;
 
