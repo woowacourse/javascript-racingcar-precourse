eabdce96996381ad2da575eb35d66f7b2775f784
unknown <lsj8249@naver.com>
2021-12-07 22:30:18 +0900

    feat : 자동차 n대 입력받기

---
+++ src/car/CarList.js
@@ -0,0 +1,19 @@
+import Car from "./index.js";
+
+class CarList {
+  constructor() {
+    this.array = [];
+  }
+
+  push(item) {
+    this.array.push(item);
+  }
+
+  init(carNameArray) {
+    for (let i = 0; i < carNameArray.length; i++) {
+      this.push(new Car(carNameArray[i]));
+    }
+  }
+}
+
+export default CarList;
