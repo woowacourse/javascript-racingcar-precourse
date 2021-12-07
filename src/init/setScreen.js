152b83c4ed2302e5c71c260c66f5a8000e519cea
Ahn <jin7969@gmail.com>
2021-12-07 17:51:06 +0900

    refactor: setScreen.js 파일 dom 선택자 중복 제거

--- src/init/setScreen.js
+++ src/init/setScreen.js
@@ -1,20 +1,16 @@
 import { hideElement, showElement } from '../utils/setAttribute.js';
 
+const $racingCountTitle = document.querySelector('#racing-count-title');
+const $racingCountForm = document.querySelector('#racing-count-form');
 const $racingResultTitle = document.querySelector('#racing-result-title');
 
 export function hideCountInputAndResult() {
-  const $racingCountTitle = document.querySelector('#racing-count-title');
-  const $racingCountForm = document.querySelector('#racing-count-form');
-
   hideElement($racingCountTitle);
   hideElement($racingCountForm);
   hideElement($racingResultTitle);
 }
 
 export function showCountInput() {
-  const $racingCountTitle = document.querySelector('#racing-count-title');
-  const $racingCountForm = document.querySelector('#racing-count-form');
-
   showElement($racingCountTitle);
   showElement($racingCountForm);
 }
