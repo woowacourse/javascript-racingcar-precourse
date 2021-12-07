2eb61d2d4f1f403039f71a32f4c1bc057f177b24
Daye Lee <leeda1801@gmail.com>
2021-12-07 14:43:40 +0900

    feat: add exception for invalid car names submit

--- src/Car.js
+++ src/Car.js
@@ -2,9 +2,17 @@ import { carNamesInput } from './elements.js';
 
 import checkCarNamesInputValid from './InputValidation.js';
 
+function onInvalidCarNamesInput() {
+  alert('잘못 입력하셨습니다. 자동차 이름을 5자 이하로 입력해 주세요.');
+  carNamesInput.focus();
+}
+
 export default function onCarNamesSubmit(event) {
   event.preventDefault();
   const carNames = carNamesInput.value.split(',');
   const carNamesInputValid = checkCarNamesInputValid(carNames);
-  console.log(carNamesInputValid);
+
+  if (!carNamesInputValid) {
+    onInvalidCarNamesInput();
+  }
 }
