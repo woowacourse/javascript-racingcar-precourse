2f5ce534c779f92045c5aa8dc9d641a1ea548f00
simyeen <simyeen@naver.com>
2021-12-07 18:04:39 +0900

    Feat : Validation Class 생성

---
+++ src/model/validate.js
@@ -0,0 +1,64 @@
+import showAlert from "../view/showAlert.js";
+
+function checkRacingCount(racingCount) {
+  const isUnderZero = Number(racingCount) <= 0 ? true : false;
+  if (isUnderZero) {
+    showAlert("RACING_COUNT");
+    return false;
+  }
+  return true;
+}
+
+function checkNameisSpace(carNamesArray) {
+  let isExistSpace = false;
+  carNamesArray.forEach((carName) => {
+    if (carName === "") {
+      isExistSpace = true;
+    }
+  });
+
+  if (isExistSpace) {
+    showAlert("SPACE");
+    return false;
+  }
+  return true;
+}
+
+function checkTotalCar(carNamesArray) {
+  let isOverTotalMaxLength = false;
+  carNamesArray.forEach((carName) => {
+    if (carName.length <= 0 || carName.length > 5) {
+      isOverTotalMaxLength = true;
+    }
+  });
+
+  if (isOverTotalMaxLength) {
+    showAlert("LENGTH");
+    return false;
+  }
+  return true;
+}
+
+export default class Validation {
+  constructor() {
+    this.carNames = null;
+    this.racingCount = 0;
+  }
+
+  isValidateCarNames(carNamesArray) {
+    if (!checkNameisSpace(carNamesArray)) {
+      return false;
+    }
+    if (!checkTotalCar(carNamesArray)) {
+      return false;
+    }
+    return true;
+  }
+
+  isValidateRacingCount(racingCount) {
+    if (!checkRacingCount(racingCount)) {
+      return false;
+    }
+    return true;
+  }
+}
