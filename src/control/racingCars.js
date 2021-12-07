7778b1459c7bd143be2230a62c8ff973fa9f617f
simyeen <simyeen@naver.com>
2021-12-07 16:43:05 +0900

    Feat : 입력받은 racingCount만큼 게임진행

---
+++ src/control/racingCars.js
@@ -0,0 +1,38 @@
+import { showResult, appendBr, showWinners } from "../view/showResult.js";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.currentCount = 0;
+  }
+
+  go() {
+    if (MissionUtils.Random.pickNumberInRange(1, 10) >= 4) {
+      this.currentCount++;
+    }
+  }
+
+  showRunning() {
+    let courrentLine = "";
+    for (let i = 0; i < this.currentCount; i++) {
+      courrentLine += "-";
+    }
+    return courrentLine;
+  }
+}
+
+export default function racingCars(carNames, runTime) {
+  console.log("레이싱 시작!", carNames, runTime);
+
+  const Cars = carNames.map((name) => new Car(name));
+  let time = runTime;
+
+  while (time--) {
+    Cars.map((car) => {
+      car.go();
+      showResult(car);
+    });
+    appendBr();
+  }
+  showWinners(Cars);
+}
