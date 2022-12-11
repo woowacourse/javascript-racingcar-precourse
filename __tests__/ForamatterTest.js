af9eebeef46765401628dea757297efb991adff5
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-12-11 21:05:02 +0900

    :white_check_mark: test : formatter 관련 테스트 코드 추가

---
+++ __tests__/ForamatterTest.js
@@ -0,0 +1,29 @@
+const { MOVE } = require('../src/lib/constants/message');
+const { moveFormatter, nameFormatter, roundFormatter } = require('../src/lib/utils/formatter');
+
+describe('포멧터 검사', () => {
+  test('moveFormatter - 움직임 개수에 따른 반환값', () => {
+    const name = 'east';
+    const move = 3;
+
+    const result = moveFormatter(name, move);
+
+    expect(result).toEqual(`${name}: ${MOVE}${MOVE}${MOVE}`);
+  });
+
+  test('nameFormatter - 이름을 쉼표를 기준으로 배열값으로 변환한 값 - 공백포함', () => {
+    const names = 'east, west, south, north';
+
+    const result = nameFormatter(names);
+
+    expect(result).toEqual(['east', 'west', 'south', 'north']);
+  });
+
+  test('nameFormatter - 이름을 쉼표를 기준으로 배열값으로 변환한 값 - 공백 불포함', () => {
+    const names = 'east,west,south,north';
+
+    const result = nameFormatter(names);
+
+    expect(result).toEqual(['east', 'west', 'south', 'north']);
+  });
+});
