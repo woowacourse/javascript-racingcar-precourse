48b30bcecacd6764e8f1563c7ef21495660c49e5
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-12-11 19:08:26 +0900

    :sparkles: feat : 전체적인 프로세스를 담당하는 App 클래스

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+const View = require('./View');
+const RacingGame = require('./Model/RacingGame');
+
 class App {
-	play() {}
+  #racingGame;
+
+  constructor() {
+    this.view = new View();
+    this.#racingGame = new RacingGame();
+  }
+
+  play() {
+    this.readCarName();
+  }
+
+  readCarName() {
+    this.view.readCarName((input) => {
+      this.inputCheckHandler(
+        () => this.#racingGame.setCarNames(input),
+        this.readCarName.bind(this),
+      );
+      this.readCount();
+    });
+  }
+
+  readCount() {
+    this.view.readCount((input) => {
+      this.inputCheckHandler(
+        () => this.#racingGame.setCount(input),
+        this.readCount.bind(this),
+      );
+      const result = this.#racingGame.makeCar().racing().checkWinner().getResult();
+      this.view.printResult(result);
+      this.view.close();
+    });
+  }
+
+  inputCheckHandler(tryHandler, again) {
+    try {
+      tryHandler();
+    } catch (err) {
+      this.view.printError(err.message);
+      again();
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
