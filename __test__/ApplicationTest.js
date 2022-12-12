9b198dd7493a70282a78ab79648aa09323d2e0d8
ryan-dia <cjfdnjs158@naver.com>
2022-12-12 18:47:23 +0900

    test:CarName test케이스 추가

--- __test__/ApplicationTest.js
+++ __test__/ApplicationTest.js
@@ -1,6 +1,6 @@
 // 여기에 테스트 코드 작성
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -12,10 +12,10 @@ const mockQuestions = (answers) => {
 };
 
 const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  MissionUtils.Random.pickNumberInRange = jest.fn();
   numbers.reduce((acc, number) => {
     return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickUniqueNumbersInRange);
+  }, MissionUtils.Random.pickNumberInRange);
 };
 
 const getLogSpy = () => {
@@ -24,35 +24,22 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("로또 테스트", () => {
+describe("자동차 경주 테스트", () => {
   test("기능 테스트", () => {
-    mockRandoms([
-      [8, 21, 23, 41, 42, 43],
-      [3, 5, 11, 16, 32, 38],
-      [7, 11, 16, 35, 36, 44],
-      [1, 8, 11, 31, 41, 42],
-      [13, 14, 16, 38, 42, 45],
-      [7, 11, 30, 40, 42, 43],
-      [2, 13, 22, 32, 38, 45],
-      [1, 3, 5, 14, 22, 45],
-    ]);
-    mockQuestions(["8000", "1,2,3,4,5,6", "7"]);
+    mockRandoms([4, 1, 2, 5, 8, 9]);
+    mockQuestions(['철수, 민수, 영희', '2']);
+
     const logs = [
-      "8개를 구매했습니다.",
-      "[8, 21, 23, 41, 42, 43]",
-      "[3, 5, 11, 16, 32, 38]",
-      "[7, 11, 16, 35, 36, 44]",
-      "[1, 8, 11, 31, 41, 42]",
-      "[13, 14, 16, 38, 42, 45]",
-      "[7, 11, 30, 40, 42, 43]",
-      "[2, 13, 22, 32, 38, 45]",
-      "[1, 3, 5, 14, 22, 45]",
-      "3개 일치 (5,000원) - 1개",
-      "4개 일치 (50,000원) - 0개",
-      "5개 일치 (1,500,000원) - 0개",
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-      "6개 일치 (2,000,000,000원) - 0개",
-      "총 수익률은 62.5%입니다.",
+      "실행 결과",
+      "철수: ",
+      "민수: -",
+      "영희: -",
+      "",
+      "철수: -",
+      "민수: --",
+      "영희: --",
+      "",
+      "최종 우승자: 민수, 영희",
     ];
     const logSpy = getLogSpy();
     const app = new App();
---
+++ __test__/Validator/car_name.test.js
@@ -0,0 +1,21 @@
+const CarName = require('../../src/Validator/CarName');
+
+describe('CarName', () => {
+    describe('checkComma', () => {
+        it('콤마와 스페이스로 잘 구분하였을 때', () => {
+          expect(() => new CarName ("민수, 영희, 조조")).toBeTruthy()
+        });
+
+        it('[Error] 콤마로 구분하지 않았을 때', () => {
+            expect(() => new CarName ("민수.영희.조조")).toThrow()
+        });
+
+        it('[Error] 자동차 이름이 5글자를 초과하였을 때', () => {
+            expect(() => new CarName ("엄마는외계인, 맘스터치, 아몬드봉봉")).toThrow()
+        });
+
+        it('[Error] 자동차 이름끼리 중복이 되었을 때', () => {
+            expect(() => new CarName ("아몬드봉봉, 아몬드봉봉, 민트초코웩")).toThrow()
+        });
+    });
+});
