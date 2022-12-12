44955fcde4412c6e8f036230ee930d3344ed735d
ryan-dia <cjfdnjs158@naver.com>
2022-12-12 18:21:57 +0900

    refactor(constants) :  object.freeze 적용

--- src/Constants/error.js
+++ src/Constants/error.js
@@ -1,11 +1,11 @@
-const ERROR_MESSAGE = {
+const ERROR_MESSAGE = Object.freeze({
     string_and_number : "숫자 또는 문자만 입력하실 수 있습니다.",
     comma : "콤마+스페이스로 여러대의 자동차이름을 구분해야합니다.",
     length: "자동차 이름은 5자 이하이어야 합니다.",
     duplication: "중복되는 자동차 이름은 만들 수 없습니다.",
     number : "숫자만 입력하실 수 있습니다.",
     zero : "숫자가 0으로 시작할 수 없습니다."
-}
+})
 
 
 module.exports = ERROR_MESSAGE
--- src/Constants/view.js
+++ src/Constants/view.js
@@ -1,15 +1,15 @@
 const NL = '\n'
 
-const INPUT_MESSAGE = {
+const INPUT_MESSAGE = Object.freeze({
     name: "자동차 이름을 5자 이하로 입력해주세요. (여러대일 경우 콤마+스페이스로 구분)" + NL,
     try: NL + "시도할 횟수를 입력해주세요." + NL
-};
+});
 
-const OUTPUT_MESSAGE = {
+const OUTPUT_MESSAGE = Object.freeze({
     error: (message, name, cause) => `${name} : ${message}\n[CAUSE] : ${cause}`,
     result_start : "실행 결과",
     winner : (winner) => `최종 우승자 : ${winner.join(", ")}`
-};
+});
 
 
 module.exports = {INPUT_MESSAGE, OUTPUT_MESSAGE}
