eccbde437b0864d7950d403ad64f50cec63d1dc7
박겸영 <yong_drg@naver.com>
2022-12-17 00:41:51 +0900

    feat(OutputView): 매 시도마다 자동차 이름과 전진 결과 출력하는 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,10 +3,22 @@ const { Console } = require('@woowacourse/mission-utils');
 const OutputView = {
   MESSAGE: {
     start: '자동차 경주 게임을 시작합니다.',
+    track: '-',
+    winner: '최종 우승자',
   },
 
   printStart() {
     Console.print(OutputView.MESSAGE.start);
+  },
+
+  getMovingResult(movingResult) {
+    return movingResult
+      .map(({ name, moveCount }) => `${name}: ${OutputView.MESSAGE.track.repeat(moveCount)}\n`)
+      .join('');
+  },
+
+  printMovingResult(movingResult) {
+    Console.print(OutputView.getMovingResult(movingResult));
   },
 };
 
